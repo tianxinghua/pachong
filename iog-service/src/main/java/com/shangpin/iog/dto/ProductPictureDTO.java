7a141cb56182e43efb3c8e01c290d214214b3789
lizhongren <lizhongren@vansky.cn>
2015-06-08 18:16:06 +0800

    测试 临时保存

--- iog-service/src/main/java/com/shangpin/iog/dto/ProductPictureDTO.java
+++ iog-service/src/main/java/com/shangpin/iog/dto/ProductPictureDTO.java
@@ -9,14 +9,41 @@ import java.util.Date;
  * Created by loyalty on 15/6/3.
  * SPU对象
  */
-@Getter
-@Setter
 public class ProductPictureDTO {
     private String id;
     private String supplierId;
     private String skuId;
     private String picUrl;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(String supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(String skuId) {
+        this.skuId = skuId;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
 }
