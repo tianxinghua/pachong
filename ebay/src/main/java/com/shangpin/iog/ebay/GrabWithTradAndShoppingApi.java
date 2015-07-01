332e86ee6048302ac350e2e39c5700d753e9fb62
huxia <huaxia@shangpin.com>
2015-07-01 17:23:06 +0800

    拉取数据

--- ebay/src/main/java/com/shangpin/iog/ebay/GrabWithTradAndShoppingApi.java
+++ ebay/src/main/java/com/shangpin/iog/ebay/GrabWithTradAndShoppingApi.java
@@ -1,8 +1,9 @@
 /**
- * 
+ *
  */
 package com.shangpin.iog.ebay;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
@@ -18,24 +19,33 @@ import com.ebay.soap.eBLBaseComponents.GetSellerListResponseType;
 import com.ebay.soap.eBLBaseComponents.ItemType;
 import com.shangpin.ebay.shoping.GetMultipleItemsResponseType;
 import com.shangpin.ebay.shoping.SimpleItemType;
+import com.shangpin.framework.ServiceException;
+import com.shangpin.iog.dto.ProductPictureDTO;
+import com.shangpin.iog.dto.SkuDTO;
+import com.shangpin.iog.dto.SpuDTO;
 import com.shangpin.iog.ebay.convert.ShopingItemConvert;
 import com.shangpin.iog.ebay.service.GrabEbayApiService;
+import com.shangpin.iog.service.ProductFetchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
- * @description 
+ * @description
  * @author 陈小峰
  * <br/>2015年6月30日
  */
+@Component
 public class GrabWithTradAndShoppingApi {
-	
-	static int pageSize=300;
+	@Autowired
+	ProductFetchService productFetchService;
+	static int pageSize=200;
 	/**
 	 * 抓取ebay商户的数据
 	 * @param userId 商户id
 	 * @param endStart 产品的结束时间 开始日期
 	 * @param endEnd 产品的结束时间 终止日期
-	 * @return 
-	 * @throws ApiException 
+	 * @return
+	 * @throws ApiException
 	 * @throws SdkSoapException
 	 * @throws SdkException
 	 */
@@ -45,9 +55,11 @@ public class GrabWithTradAndShoppingApi {
 		boolean hasMore=false;
 		Map<String, ? extends Collection> skuSpuAndPic=null;
 		do{
-			GetSellerListResponseType resp = GrabEbayApiService.tradeSellerList(userId, 
+			GetSellerListResponseType resp = GrabEbayApiService.tradeSellerList(userId,
 					getCalendar(endStart), getCalendar(endEnd),page,pageSize);
-			if(AckCodeType.FAILURE.equals(resp.getAck())){
+            System.out.println("nihao");
+			if(!AckCodeType.FAILURE.equals(resp.getAck())){
+                System.out.println("nihaoaaaaaa");
 				hasMore=resp.isHasMoreItems();
 				ItemType[] tps = resp.getItemArray().getItem();
 				List<String> itemIds = new ArrayList<>(tps.length);//1.得到id
@@ -60,6 +72,7 @@ public class GrabWithTradAndShoppingApi {
 				SimpleItemType[] itemTypes=multResp.getItemArray();
 				//Map<String, ? extends Collection<?>> kpp=TradeItemConvert.convert2SKuAndSpu(tps,userId);
 				Map<String, ? extends Collection<?>> kpp=ShopingItemConvert.convert2kpp(itemTypes,userId);
+				System.out.println(kpp.size()+"nihao");
 				if(skuSpuAndPic==null){
 					skuSpuAndPic=kpp;
 				}else{
@@ -70,10 +83,32 @@ public class GrabWithTradAndShoppingApi {
 				page++;
 			}
 		}while(hasMore);
-		
+
 		return skuSpuAndPic;
 	}
 
+    public void FetchAndSave() throws SdkException, ServiceException {
+		Date date=Calendar.getInstance().getTime();
+		Date date2=null;
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.MONTH, 1);
+		date2=c.getTime();
+		Map<String, ? extends Collection> skuSpuAndPic=getSellerList("inzara.store",date,date2);
+		//Collection<SkuDTO>  skus= skuSpuAndPic.get("sku");
+		//System.out.println(skus.size()+"nihaoma");
+//		for(SkuDTO sku:skus) {
+//			productFetchService.saveSKU(sku);
+//		}
+//		Collection<SpuDTO> spuDTOs = skuSpuAndPic.get("spu");
+//		for(SpuDTO spu:spuDTOs){
+//			productFetchService.saveSPU(spu);
+//		}
+//		Collection<ProductPictureDTO> picUrl = skuSpuAndPic.get("pic");
+//		for(ProductPictureDTO picurl:picUrl){
+//			productFetchService.savePictureForMongo(picurl);
+//		}
+	}
 	/**
 	 * @param date
 	 * @return
@@ -83,5 +118,5 @@ public class GrabWithTradAndShoppingApi {
 		ca.setTime(date);
 		return ca;
 	}
-	
+
 }
