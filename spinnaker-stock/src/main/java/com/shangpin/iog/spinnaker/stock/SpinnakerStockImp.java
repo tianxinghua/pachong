165a5afac9dfd491a2d4db8877f88512ff9f3aff
jinweile <jinweile@shangpin.com>
2015-07-08 15:22:26 +0800

    update spinnaker

--- spinnaker-stock/src/main/java/com/shangpin/iog/spinnaker/stock/SpinnakerStockImp.java
+++ spinnaker-stock/src/main/java/com/shangpin/iog/spinnaker/stock/SpinnakerStockImp.java
@@ -1,10 +1,17 @@
 package com.shangpin.iog.spinnaker.stock;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.shangpin.framework.ServiceException;
 import com.shangpin.ice.ice.AbsUpdateProductStock;
+import com.shangpin.iog.common.utils.httpclient.HttpUtils;
+import com.shangpin.iog.spinnaker.stock.dto.Quantity;
 import org.apache.log4j.Logger;
 
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -14,8 +21,51 @@ public class SpinnakerStockImp extends AbsUpdateProductStock {
 
     private static Logger logger = Logger.getLogger("info");
 
+    private Map<String,String> barcode_map = new HashMap<>();
+
     @Override
     public Map<String, Integer> grabStock(Collection<String> skuNo) throws ServiceException, Exception {
-        return null;
+        Map<String, Integer> stock_map = new HashMap<String, Integer>();
+        Gson gson = new Gson();
+        for (String skuno : skuNo) {
+            if (barcode_map.containsKey(skuno)) {
+                continue;
+            } else {
+                barcode_map.put(skuno, null);
+            }
+
+            String barcode = skuno;
+            //根据供应商skuno获取库存，并更新我方sop库存
+            String url = "http://185.58.119.177/spinnakerapi/Myapi/Productslist/GetQuantityByBarcode?DBContext=Default&barcode=[[barcode]]&key=8IZk2x5tVN";
+            url = url.replaceAll("\\[\\[barcode\\]\\]", barcode);
+            String json = null;
+            try {
+                json = HttpUtils.get(url);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (json != null && !json.isEmpty()) {
+                try {
+                    Quantity result = gson.fromJson(json, new TypeToken<Quantity>() {
+                    }.getType());
+                    stock_map.put(skuno, Integer.valueOf(result.getResult()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return stock_map;
     }
+
+    public static void main(String[] args) throws Exception {
+        AbsUpdateProductStock grabStockImp = new SpinnakerStockImp();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        logger.info("SPINNAKER更新数据库开始");
+        grabStockImp.updateProductStock("2015050800242","2015-01-01 00:00",format.format(new Date()));
+        logger.info("SPINNAKER更新数据库结束");
+        System.exit(0);
+
+    }
+
 }
--- spinnaker/src/main/java/com/shangpin/iog/spinnaker/stock/StartSkuJob.java
+++ spinnaker/src/main/java/com/shangpin/iog/spinnaker/stock/StartSkuJob.java
@@ -2,6 +2,7 @@ package com.shangpin.iog.spinnaker.stock;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.shangpin.iog.app.AppContext;
 import com.shangpin.iog.common.utils.UUIDGenerator;
 import com.shangpin.iog.common.utils.httpclient.HttpUtils;
 import com.shangpin.iog.dto.SkuDTO;
@@ -9,6 +10,11 @@ import com.shangpin.iog.dto.SpuDTO;
 import com.shangpin.iog.product.service.ProductFetchServiceImpl;
 import com.shangpin.iog.service.ProductFetchService;
 import com.shangpin.iog.spinnaker.stock.dto.*;
+import com.shangpin.iog.spinnaker.stock.service.FetchProduct;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Date;
 import java.util.List;
@@ -17,87 +23,26 @@ import java.util.Map;
 /**
  * Created by Administrator on 2015/7/8.
  */
+
 public class StartSkuJob {
 
-    private ProductFetchService pfs = new ProductFetchServiceImpl();
+    private static Logger log = Logger.getLogger("info");
+
+    private static ApplicationContext factory;
+    private static void loadSpringContext() {
+        factory = new AnnotationConfigApplicationContext(AppContext.class);
+    }
 
     public static void main(String[] args) throws Exception {
-        //首先获取季节码  http://185.58.119.177/spinnakerapi/Myapi/Productslist/GetAllSeasonCode?DBContext=Default&key=8IZk2x5tVN
-        String season_json = HttpUtils.get("http://185.58.119.177/spinnakerapi/Myapi/Productslist/GetAllSeasonCode?DBContext=Default&key=8IZk2x5tVN");
-        Gson gson = new Gson();
-        SeasoncodeList season_list = gson.fromJson(season_json, new TypeToken<SeasoncodeList>(){}.getType());
-
-        for (Seasoncode obj : season_list.getSeasonCode()){
-            int i = 1;
-            while (true){
-                //然后根据季节码抓取sku  http://185.58.119.177/spinnakerapi/Myapi/Productslist/GetProducts?DBContext=Default&CategoryId=&BrandId=&SeasonCode=[[seasoncode]]&StartIndex=[[startindex]]&EndIndex=[[endindex]]&key=8IZk2x5tVN
-                String producturl = "http://185.58.119.177/spinnakerapi/Myapi/Productslist/GetProducts?DBContext=Default&CategoryId=&BrandId=&SeasonCode=[[seasoncode]]&StartIndex=[[startindex]]&EndIndex=[[endindex]]&key=8IZk2x5tVN";
-                String url = producturl.replaceAll("\\[\\[seasoncode\\]\\]", obj.getSeasonCode())
-                        .replaceAll("\\[\\[startindex\\]\\]", "" + i)
-                        .replaceAll("\\[\\[endindex\\]\\]","" + (i + 100));
-
-                String json = null;
-                try {
-                    json = HttpUtils.get(url);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                if (json != null && !json.isEmpty()) {
-                    Products list = null;
-                    try {
-                        list = gson.fromJson(json, new TypeToken<Products>() {}.getType());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    if (list != null && list.getProduct() != null) {
-                        for (Spu spu : list.getProduct()) {
-                            //spu入库
-                            SpuDTO spudto = new SpuDTO();
-                            spudto.setBrandName(spu.getProducer_id());
-                            spudto.setCategoryGender(spu.getType());
-                            spudto.setCategoryName(spu.getCategory());
-                            spudto.setCreateTime(new Date());
-                            spudto.setSeasonId(obj.getSeasonCode());
-                            spudto.setSupplierId("2015051300260");
-                            spudto.setSpuId(spu.getProduct_id());
-                            spudto.setId(UUIDGenerator.getUUID());
-                            spudto.setMaterial(spu.getProduct_detail());
-                            spudto.setPicUrl(spu.getUrl());
-                            //spudto.setSpuName();
-
-                            for (Sku sku : spu.getItems().getItem()) {
-                                //sku入库操作
-                                SkuDTO skudto = new SkuDTO();
-                                skudto.setCreateTime(new Date());
-                                skudto.setBarcode(sku.getBarcode());
-                                skudto.setColor(sku.getColor());
-                                skudto.setId(sku.getItem_id());
-                                //skudto.setLastTime();
-                                skudto.setProductCode(spu.getProduct_id());
-                                skudto.setProductDescription(spu.getProduct_detail());
-                                skudto.setProductName(spu.getProduct_name());
-                                skudto.setProductSize(sku.getItem_size());
-                                //skudto.setSaleCurrency();
-                                skudto.setSalePrice(spu.getSupply_price());
-                                skudto.setSkuId(sku.getItem_id());
-                                skudto.setSpuId(spu.getProduct_id());
-                                skudto.setProductCode(spu.getProducer_id());
-                                skudto.setStock(sku.getStock());
-                                skudto.setSupplierId("2015051300260");
-                            }
-                        }
-                    } else {
-                        break;
-                    }
-
-                    if (list != null && list.getProduct() != null && list.getProduct().length < 100) {
-                        break;
-                    }
-                }
-                i += 100;
-            }
-        }
-
+        //加载spring
+        log.info("----拉取spinnaker数据开始----");
+        loadSpringContext();
+        log.info("----初始SPRING成功----");
+        //拉取数据
+        FetchProduct fetchProduct =(FetchProduct)factory.getBean("spinnaker");
+        fetchProduct.fetchProductAndSave();
+        log.info("----拉取spinnaker数据完成----");
+        System.out.println("-------fetch end---------");
         System.exit(0);
     }
 
