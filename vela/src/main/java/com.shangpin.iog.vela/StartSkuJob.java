92249d6855cd298c9df3f2f4ddde63cd1ac43fd3
lizhongren <lizhongren@vansky.cn>
2015-07-16 11:48:04 +0800

    新增vela 程序

---
+++ vela/src/main/java/com.shangpin.iog.vela/StartSkuJob.java
@@ -0,0 +1,50 @@
+package com.shangpin.iog.spinnaker.stock;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.shangpin.iog.app.AppContext;
+import com.shangpin.iog.common.utils.UUIDGenerator;
+import com.shangpin.iog.common.utils.httpclient.HttpUtils;
+import com.shangpin.iog.dto.SkuDTO;
+import com.shangpin.iog.dto.SpuDTO;
+import com.shangpin.iog.product.service.ProductFetchServiceImpl;
+import com.shangpin.iog.service.ProductFetchService;
+import com.shangpin.iog.spinnaker.stock.dto.*;
+import com.shangpin.iog.spinnaker.stock.service.FetchProduct;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2015/7/8.
+ */
+
+public class StartSkuJob {
+
+    private static Logger log = Logger.getLogger("info");
+
+    private static ApplicationContext factory;
+    private static void loadSpringContext() {
+        factory = new AnnotationConfigApplicationContext(AppContext.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        //加载spring
+        loadSpringContext();
+        log.info("----初始SPRING成功----");
+        //拉取数据
+        log.info("----拉取vela数据开始----");
+
+        FetchProduct fetchProduct =(FetchProduct)factory.getBean("vela");
+        fetchProduct.fetchProductAndSave();
+        log.info("----拉取vela数据完成----");
+        System.out.println("-------fetch end---------");
+        System.exit(0);
+    }
+
+}
