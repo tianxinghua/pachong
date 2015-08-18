c79f0d5101c403e2c0673a71a14c9480edee675b
chenxiaofeng <chenxiaofeng@vansky.com>
2015-08-18 11:20:27 +0800

    修改库存更新重试、httpclient策略；抓取分页保存，抓一页保存一页；

--- ebay/src/main/java/com/shangpin/iog/ebay/V1UpdateStock.java
+++ ebay/src/main/java/com/shangpin/iog/ebay/V1UpdateStock.java
@@ -1,9 +1,18 @@
 package com.shangpin.iog.ebay;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.xmlbeans.XmlException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.shangpin.framework.ServiceException;
 import com.shangpin.ice.ice.AbsUpdateProductStock;
@@ -14,16 +23,96 @@ import com.shangpin.ice.ice.AbsUpdateProductStock;
  * <br/>2015年7月24日
  */
 public class V1UpdateStock  extends AbsUpdateProductStock{
-
+	static Logger logger = LoggerFactory.getLogger(V1UpdateStock.class);
 	V1GrabService grabSrv = new V1GrabService();
+	static ThreadLocal<Set<String>> errItemId=new ThreadLocal<Set<String>>();
 	@Override
 	public Map<String, Integer> grabStock(Collection<String> skuNo)
 			throws ServiceException, Exception {
-		Collection<String> itemIds=new HashSet<>(skuNo.size());
+		Set<String> itemIds=new HashSet<>(skuNo.size());
 		for (Iterator<String> iterator = skuNo.iterator(); iterator.hasNext();) {
 			String skuId = iterator.next();
 			itemIds.add(skuId.split("#")[0]);
 		}
-		return grabSrv.getStock(itemIds);
+		Map<String, Integer> rtn = grabItmStock(itemIds);
+		int retry=0;
+		while(getErrItemId().size()>0 && retry<10){//失败的重取，errItemId得清空
+			retry++;
+			logger.info("拉取失败的重拉第{}次，大小：{}",retry,getErrItemId().size());
+			Set<String> itms=getErrItemId();
+			errItemId.set(new HashSet<String>());
+			combine(rtn, grabItmStock(itms));
+		}
+		return rtn;
 	}
+
+	/**
+	 * @param itemIds
+	 * @param rtn
+	 * @return
+	 * @throws XmlException 
+	 */
+	private Map<String, Integer> grabItmStock(Set<String> itemIds){
+		Map<String, Integer> rtn = new HashMap<String, Integer>();
+		if(itemIds.size()>20){
+			List<String> pageItem = new ArrayList<>(20);
+			for (String id : itemIds) {
+				if(pageItem.size()==20){
+					try {
+						combine(rtn,grabSrv.getStock(pageItem));
+					} catch (XmlException e) {
+						logger.info("拉取有失败的，加入重试库a");
+						addErrorItem(pageItem);
+					}//每20个获取一遍
+					pageItem=new ArrayList<>(20);
+				}
+				pageItem.add(id);
+			}
+			if(pageItem.size()>0){
+				try {
+					combine(rtn,grabSrv.getStock(pageItem));
+				} catch (XmlException e) {
+					logger.info("拉取有失败的，加入重试库b");
+					addErrorItem(pageItem);
+				}//每20个获取一遍
+			}
+		}else{
+			try {
+				rtn=grabSrv.getStock(itemIds);
+			} catch (XmlException e) {
+				logger.info("拉取有失败的，加入重试库c");
+				addErrorItem(itemIds);
+			}
+		}
+		return rtn;
+	}
+	
+	/**
+	 * @param itemIds
+	 */
+	private void addErrorItem(Collection<String> itemIds) {
+		errItemId.get().addAll(itemIds);
+	}
+
+	/**
+	 * @param rtn
+	 * @param stock
+	 */
+	private static void combine(Map<String, Integer> rtn, Map<String, Integer> stock) {
+		Set<Entry<String, Integer>> entrySet = stock.entrySet();
+		for (Entry<String, Integer> entry : entrySet) {
+			rtn.put(entry.getKey(), entry.getValue());
+		}
+	}
+	/**
+	 * 添加获取错误的item
+	 * @param itemId
+	 */
+	public static void addErrorItem(String itemId){
+		errItemId.get().add(itemId);
+	}
+	public static Set<String> getErrItemId() {
+		return errItemId.get();
+	}
+	
 }
--- ebay/src/main/java/com/shangpin/iog/ebay/service/GrabEbayApiService.java
+++ ebay/src/main/java/com/shangpin/iog/ebay/service/GrabEbayApiService.java
@@ -158,17 +158,21 @@ public class GrabEbayApiService {
 		sb.append("&IncludeSelector="+includeSelector);
 		//System.out.println(sb.toString());
 		String xml=HttpUtil45.get(sb.toString(),null,null);
-		log.debug("url:{},结果：{}",sb.toString(),xml);
-		if(!HttpUtil45.errorResult.equals(xml)){
-			XmlOptions option= new XmlOptions();option.setLoadStripComments();
-			option.setLoadTrimTextBuffer();
-			GetMultipleItemsResponseDocument.Factory.newInstance(option);
-			GetMultipleItemsResponseDocument doc=GetMultipleItemsResponseDocument.Factory.parse(xml);
-			GetMultipleItemsResponseType rt=doc.getGetMultipleItemsResponse();
-			return rt;
-		}else{
-			return null;
+		if(HttpUtil45.errorResult.equals(xml)){
+			throw new XmlException(xml);
 		}
+		log.debug("url:{},结果：{}",sb.toString(),xml);
+		XmlOptions option= new XmlOptions();option.setLoadStripComments();
+		option.setLoadTrimTextBuffer();
+		GetMultipleItemsResponseDocument.Factory.newInstance(option);
+		GetMultipleItemsResponseDocument doc=GetMultipleItemsResponseDocument.Factory.parse(xml);
+		GetMultipleItemsResponseType rt=doc.getGetMultipleItemsResponse();
+		return rt;
+		/*if(!HttpUtil45.errorResult.equals(xml)){
+		}else{
+			log.error("http获取itm错误："+xml);
+			return null;
+		}*/
 		//try {
 		//setLoadStripComments
 		/*} catch (XmlException e) {
