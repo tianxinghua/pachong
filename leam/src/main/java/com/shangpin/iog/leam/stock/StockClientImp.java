576f2b8830d41ab3b1590200e39846ebc354e11e
qingshan <qingshan@vansky.cn>
2015-09-07 17:13:06 +0800

    gradle依赖中，ice换成sop

--- leam/src/main/java/com/shangpin/iog/leam/stock/StockClientImp.java
+++ leam/src/main/java/com/shangpin/iog/leam/stock/StockClientImp.java
@@ -2,11 +2,11 @@ package com.shangpin.iog.leam.stock;
 
 import com.google.gson.Gson;
 import com.shangpin.framework.ServiceException;
-import com.shangpin.ice.ice.AbsUpdateProductStock;
 import com.shangpin.iog.common.utils.httpclient.HttpUtil45;
 import com.shangpin.iog.common.utils.httpclient.OutTimeConfig;
 import com.shangpin.iog.leam.dto.LeamDTO;
 import com.shangpin.iog.leam.dto.TokenDTO;
+import com.shangpin.sop.AbsUpdateProductStock;
 import org.apache.log4j.Logger;
 
 import java.text.SimpleDateFormat;
@@ -17,12 +17,13 @@ import java.util.*;
  */
 public class StockClientImp  extends AbsUpdateProductStock {
     private static Logger logger = Logger.getLogger("info");
+    private  static  ResourceBundle bundle = ResourceBundle.getBundle("sop");
     String user="shamping";
     String password="PA#=k2xU^ddUc6Jm";
     String supplierId = "201508081715";
     @Override
-    public Map<String, String> grabStock(Collection<String> skuNo) throws ServiceException, Exception {
-        Map<String, String> skustock = new HashMap<>(skuNo.size());
+    public Map<String, Integer> grabStock(Collection<String> skuNo) throws ServiceException, Exception {
+        Map<String, Integer> skustock = new HashMap<>(skuNo.size());
         Map<String,String> stockMap = new HashMap<>();
         String stockUrl="http://188.226.153.91/modules/api/v2/stock/id/";
         String tokenUrl="http://188.226.153.91/modules/api/v2/getToken/";
@@ -41,7 +42,7 @@ public class StockClientImp  extends AbsUpdateProductStock {
                 String skuno=it.next();
                 result = HttpUtil45.post(stockUrl+skuno+"/?t="+token,param,outTimeConf);
                 dto=getObjectByjsonstr(result);
-                skustock.put(skuno,dto.getQty());
+                skustock.put(skuno,Integer.valueOf(dto.getQty()));
             }
         }catch (Exception e){
             e.printStackTrace();
@@ -79,9 +80,13 @@ public class StockClientImp  extends AbsUpdateProductStock {
     }
     public static void main(String[] args) throws Exception {
         StockClientImp impl = new StockClientImp();
+        String host = bundle.getString("HOST");
+        String app_key = bundle.getString("APP_KEY");
+        String app_secret= bundle.getString("APP_SECRET");
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         logger.info("LEAM更新数据库开始");
-        impl.updateProductStock("201508081715", "2015-01-01 00:00", format.format(new Date()));
+        //impl.updateProductStock("201508081715", "2015-01-01 00:00", format.format(new Date()));
+        impl.updateProductStock(host,app_key,app_secret,"2015-01-01 00:00",format.format(new Date()));
         logger.info("LEAM更新数据库结束");
         System.exit(0);
     }
