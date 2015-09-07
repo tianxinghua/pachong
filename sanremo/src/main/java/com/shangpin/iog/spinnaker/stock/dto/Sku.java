a7f2167a2167aa210549494286604f9f0561fa6a
lizhongren <lizhongren@vansky.cn>
2015-09-07 09:58:52 +0800

    commit before merge

---
+++ sanremo/src/main/java/com/shangpin/iog/spinnaker/stock/dto/Sku.java
@@ -0,0 +1,65 @@
+package com.shangpin.iog.spinnaker.stock.dto;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/5/28.
+ */
+public class Sku {
+
+    public String getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(String item_id) {
+        this.item_id = item_id;
+    }
+
+    public String getItem_size() {
+        return item_size;
+    }
+
+    public void setItem_size(String item_size) {
+        this.item_size = item_size;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public List<String> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(List<String> pictures) {
+        this.pictures = pictures;
+    }
+
+    private String item_id;
+    private String item_size;
+    private String barcode;
+    private String color;
+    private String stock;
+    private List<String> pictures;
+
+}
