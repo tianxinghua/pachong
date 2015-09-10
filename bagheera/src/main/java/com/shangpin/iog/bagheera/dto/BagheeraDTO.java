6594b87ffc50129f012ed5d7bfc3f1f65d1c41e3
qingshan <qingshan@vansky.cn>
2015-09-10 14:16:17 +0800

    bagheera模块--下载文件，读取，存储.

---
+++ bagheera/src/main/java/com/shangpin/iog/bagheera/dto/BagheeraDTO.java
@@ -0,0 +1,31 @@
+package com.shangpin.iog.bagheera.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by sunny on 2015/9/9.
+ */
+@Getter
+@Setter
+public class BagheeraDTO {
+    private String LAST_UPDATE;
+    private String SUPPLIER_CODE;
+    private String CODE;
+    private String DEPT;
+    private String ITEM_GROUP;
+    private String MATERIAL;
+    private String SIZE_AND_FIT;
+    private String MADE;
+    private String DESCRIPTION;
+    private String COLOR;
+    private String SIZE;
+    private String STOCK;
+    private String COLLECTION;
+    private String LIST_PRICE;
+    private String DISCOUNTED;
+    private String DISCOUNT;
+    private String LASO_Price;
+    private String CURRENCY;
+    private String IMAGE_URL;
+}
