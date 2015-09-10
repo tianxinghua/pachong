1a9901370e5065eb2c4a23965b7adeabda523f32
huxia <huaxia@shangpin.com>
2015-09-10 18:07:02 +0800

    commit OrderDTO

---
+++ iog-common-product/src/main/java/com/shangpin/iog/product/dao/OrderMapper.java
@@ -0,0 +1,10 @@
+package com.shangpin.iog.product.dao;
+
+import com.shangpin.iog.dao.base.IBaseDao;
+import com.shangpin.iog.dto.OrderDTO;
+
+/**
+ * Created by huxia on 2015/9/10.
+ */
+public interface OrderMapper extends IBaseDao<OrderDTO> {
+}
