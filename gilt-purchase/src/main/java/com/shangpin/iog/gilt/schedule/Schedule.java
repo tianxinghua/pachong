bc68f334d3cc8041adec3a713bf7a6d749b80195
lizhongren <lizhongren@vansky.cn>
2015-09-19 18:08:40 +0800

    merge branch SOP 改造

--- gilt-purchase/src/main/java/com/shangpin/iog/gilt/schedule/Schedule.java
+++ gilt-purchase/src/main/java/com/shangpin/iog/gilt/schedule/Schedule.java
@@ -22,7 +22,7 @@ import java.util.*;
 public class Schedule {
     Logger logger = LoggerFactory.getLogger(this.getClass());
     private static ResourceBundle bdl=null;
-    private static String url="https://api-sandbox.gilt.com/global/orders/";
+    private static String url="https://api-sandbox.gilt.com/global/orders";
     private static String supplierId;
     private static String key ;
     static {
@@ -77,12 +77,15 @@ public class Schedule {
         System.out.println("返回结果 1："+result1);*/
         OutTimeConfig timeConfig = new OutTimeConfig(1000*5,1000*5,1000*5);
         Gson gson =new Gson();
-        Map<String,String> param =new HashMap<>();
-        //String result=HttpUtil45.get(url +"1adc7865-7aa0-4afb-b22c-1e99736ab0ad", timeConfig, param, key, "");
+        Map<String,String> paraMap =new HashMap<>();
+        paraMap.put("status","cancelled");
+        String json = "{\"status\" : \"confirmed\"}";
+       // String result=HttpUtil45.get(url +"/1adc7865-7aa0-4afb-b22c-1e99736ab0ad", timeConfig, param, key, "");
         try {
-            String str = HttpUtil45.operateData("patch","json",url+"1adc7865-7aa0-4afb-b22c-1e99736ab0ad", timeConfig,param,"",key,"");
+//            String str = HttpUtil45.operateData("delete", "", url + "/1adc7865-7aa0-4afb-b22c-1e99736ab0ad", timeConfig, paraMap,"", key,"");
+            String str = HttpUtil45.get(url,timeConfig,null,key,"");
             System.out.println(str);
-        } catch (ServiceException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         //OrderDTO dto = gson.fromJson(result, OrderDTO.class);
