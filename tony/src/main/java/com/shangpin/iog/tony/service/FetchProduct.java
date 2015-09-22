edf44bacb07bbb301c0bf93ca726ed8421bd3544
wangyuzhi <wangyuzhi@shangpin.com>
2015-09-22 13:16:49 +0800

    修改

--- tony/src/main/java/com/shangpin/iog/tony/service/FetchProduct.java
+++ tony/src/main/java/com/shangpin/iog/tony/service/FetchProduct.java
@@ -78,6 +78,7 @@ public class FetchProduct {
                 sku.setStock(item.getQty());
                 sku.setBarcode(item.getBarcode());
                 sku.setProductCode(item.getCode());
+                sku.setSaleCurrency(item.getCur());
                 productFetchService.saveSKU(sku);
 
                 if(StringUtils.isNotBlank(item.getImages()[0])){
