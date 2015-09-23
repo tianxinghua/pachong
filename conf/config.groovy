f61f12c7828906b7f1210acf719079e1341d4ddb
lizhongren <lizhongren@vansky.cn>
2015-09-23 17:58:54 +0800

    修改配置

--- conf/config.groovy
+++ conf/config.groovy
@@ -37,15 +37,12 @@ environments{
 	production{
 
         db {
-//            driverClassName = 'com.mysql.jdbc.Driver'
-//            url ='jdbc:mysql://172.20.30.141:3306/iog?useUnicode=true&amp;characterEncoding=utf-8'
-//            username = 'reader'
-//            password = 'rd@sp520'
-
             driverClassName = 'com.mysql.jdbc.Driver'
-            url ='jdbc:mysql://192.168.20.82:3306/iog?useUnicode=true&amp;characterEncoding=utf-8'
-            username = 'root'
-            password = '123456'
+            url ='jdbc:mysql://172.20.30.141:3306/iog?useUnicode=true&amp;characterEncoding=utf-8'
+            username = 'reader'
+            password = 'rd@sp520'
+
+
 
             Ice.Default.Locator='SPIceGrid/Locator:default -h 172.20.10.242 -p 12000:default -h 172.20.10.246 -p 12000'
 
