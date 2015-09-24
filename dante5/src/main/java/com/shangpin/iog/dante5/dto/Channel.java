a4a3e42c027f735b9c5f635060e2ded23103ca59
zhanglei <zhanglei@vansky.cn>
2015-09-24 18:27:49 +0800

    完成dante5开发.

--- dante5/src/main/java/com/shangpin/iog/dante5/dto/Channel.java
+++ dante5/src/main/java/com/shangpin/iog/dante5/dto/Channel.java
@@ -16,6 +16,37 @@ public class Channel {
     String description;
     List<Item> item;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Item> getItem() {
+        return item;
+    }
+
+    public void setItem(List<Item> item) {
+        this.item = item;
+    }
 
     @Override
     public String toString() {
--- dante5/src/main/java/com/shangpin/iog/dante5/dto/ImageLinks.java
+++ dante5/src/main/java/com/shangpin/iog/dante5/dto/ImageLinks.java
@@ -1,9 +1,13 @@
 package com.shangpin.iog.dante5.dto;
 
 import com.google.gson.annotations.SerializedName;
+import com.google.gson.internal.LinkedTreeMap;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -13,7 +17,25 @@ import java.util.List;
 public class ImageLinks {
 
     @SerializedName("g:image_link")
-    List<ImageLink> links;
+    List links;
+
+    public List<String> getLinks() {
+        List<String> pics = new ArrayList<>();
+        if (links != null && links.size() > 0) {
+            for (Object picObj : links) {
+                LinkedTreeMap picMap = (LinkedTreeMap)picObj;
+                Object urlObj = picMap.get("$");
+                if (urlObj != null) {
+                    pics.add(urlObj.toString());
+                }
+            }
+        }
+        return pics;
+    }
+
+    public void setLinks(List links) {
+        this.links = links;
+    }
 
     @Override
     public String toString() {
