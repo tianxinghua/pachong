0c3ac40523d8b082c3a5d1f9b2512a73cc7dacb3
lizhongren <lizhongren@vansky.cn>
2015-09-30 20:25:03 +0800

    giglio发布部署 更新

--- giglio/src/main/java/com/shangpin/iog/giglio/service/FetchProduct.java
+++ giglio/src/main/java/com/shangpin/iog/giglio/service/FetchProduct.java
@@ -50,9 +50,8 @@ public class FetchProduct {
 
         try {
             Map<String,String> mongMap = new HashMap<>();
-            OutTimeConfig timeConfig = OutTimeConfig.defaultOutTimeConfig();
-            timeConfig.confRequestOutTime(600000);
-            timeConfig.confSocketOutTime(600000);
+            OutTimeConfig timeConfig =new OutTimeConfig(1000*60,1000*60,1000*60);
+
             String result = HttpUtil45.get(url, timeConfig, null);
 
 //            mongMap.put("supplierId", supplierId);
