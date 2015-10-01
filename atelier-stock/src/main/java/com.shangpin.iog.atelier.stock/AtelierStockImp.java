444adcea5199b828255399d48112f9d927a5b38e
wangyuzhi <wangyuzhi@shangpin.com>
2015-10-01 10:13:07 +0800

    新建项目

---
+++ atelier-stock/src/main/java/com.shangpin.iog.atelier.stock/AtelierStockImp.java
@@ -0,0 +1,59 @@
+package com.shangpin.iog.atelier.stock;
+
+import com.shangpin.framework.ServiceException;
+import com.shangpin.ice.ice.AbsUpdateProductStock;
+import com.shangpin.iog.atelier.common.MyStringUtil;
+import com.shangpin.iog.atelier.common.WS_Sito_P15;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Created by wangyuzhi on 2015/9/14.
+ */
+public class AtelierStockImp  extends AbsUpdateProductStock {
+    private static Logger logger = Logger.getLogger("info");
+    private static ResourceBundle bdl=null;
+    private static String supplierId;
+    static {
+        if(null==bdl)
+            bdl=ResourceBundle.getBundle("conf");
+        supplierId = bdl.getString("supplierId");
+    }
+    @Override
+    public Map<String,String> grabStock(Collection<String> skuNo) throws ServiceException, Exception {
+        String stocks = new WS_Sito_P15().getAllAvailabilityMarketplace();
+        //定义三方
+        Map returnMap = new HashMap();
+        String itemId = "";
+        Iterator<String> iterator=skuNo.iterator();
+        //为供应商循环赋值
+        logger.info("为供应商循环赋值");
+        while (iterator.hasNext()){
+            itemId = iterator.next();
+            returnMap.put(itemId, MyStringUtil.getStockBySkuId(stocks.substring(stocks.indexOf(itemId), stocks.indexOf(itemId) + 20)));
+        }
+        return returnMap;
+    }
+
+    public static void main(String[] args) throws Exception {
+        AtelierStockImp impl = new AtelierStockImp();
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        logger.info("ATELIER更新数据库开始");
+        impl.updateProductStock(supplierId, "2015-01-01 00:00", format.format(new Date()));
+        logger.info("ATELIER更新数据库结束");
+        System.exit(0);
+
+
+/*        List<String> skuNo = new ArrayList<>();
+        skuNo.add("1986242872_10");
+        Map returnMap = impl.grabStock(skuNo);
+        System.out.println("test return size is "+returnMap.keySet().size());
+        System.out.println("test return value is "+returnMap.get("1986242872_10"));;
+        System.out.println("test return value is ");*/
+
+    }
+}
