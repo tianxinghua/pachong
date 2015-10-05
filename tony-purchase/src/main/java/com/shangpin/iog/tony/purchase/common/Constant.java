170066a80a2c36c75ed5fab7598a5eb6c5da85da
wangyuzhi <wangyuzhi@shangpin.com>
2015-10-05 09:56:59 +0800

    New changelist

---
+++ tony-purchase/src/main/java/com/shangpin/iog/tony/purchase/common/Constant.java
@@ -0,0 +1,24 @@
+package com.shangpin.iog.tony.purchase.common;
+
+import java.util.ResourceBundle;
+
+/**
+ * Created by wangyuzhi on 2015/9/11.
+ */
+public class Constant {
+    public static ResourceBundle bundle = ResourceBundle.getBundle("conf");
+    //供应商ID
+    public static String SUPPLIER_ID = bundle.getString("supplierId");
+    //供应商ID
+    public static String MERCHANT_ID = bundle.getString("merchantId");
+    //供应商ID
+    public static String TOKEN = bundle.getString("token");
+
+    /**
+     * test
+     * @param args
+     */
+    public static void main(String[] args) {
+         System.out.println("Constant.SUPPLIER_ID=" + Constant.SUPPLIER_ID);
+    }
+}
