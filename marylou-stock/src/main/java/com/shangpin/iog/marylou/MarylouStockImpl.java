a184a662d5e016283228434e6d05c6e3281dd059
lizhongren <lizhongren@vansky.cn>
2015-10-06 16:38:49 +0800

    更改maryloug更新库存方式

--- marylou-stock/src/main/java/com/shangpin/iog/marylou/MarylouStockImpl.java
+++ marylou-stock/src/main/java/com/shangpin/iog/marylou/MarylouStockImpl.java
@@ -1,13 +1,15 @@
 package com.shangpin.iog.marylou;
 
 import com.shangpin.framework.ServiceException;
-import com.shangpin.ice.ice.AbsUpdateProductStock;
 import com.shangpin.iog.onsite.base.common.HTTPClient;
 import com.shangpin.iog.onsite.base.constance.Constant;
 import com.shangpin.iog.onsite.base.utils.MapUtil;
 import com.shangpin.iog.onsite.base.utils.StringUtil;
+import com.shangpin.sop.AbsUpdateProductStock;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -15,27 +17,43 @@ import java.util.*;
  */
 public class MarylouStockImpl  extends AbsUpdateProductStock {
     private static Logger logger = Logger.getLogger("info");
+
+    private  static  ResourceBundle bundle = ResourceBundle.getBundle("sop");
     @Override
-    public Map<String,String> grabStock(Collection<String> skuNo) throws ServiceException, Exception {
-        //��ȡ��Ʒ��Ϣ
+    public Map<String,Integer> grabStock(Collection<String> skuNo) throws ServiceException, Exception {
         String json = new HTTPClient(Constant.URL_MARYLOU).fetchProductJson();
-        //������Ҫ���µ���Ϣ
-        return MapUtil.grabStock(skuNo,json);
+        Map<String ,String> tmpMap =  MapUtil.grabStock(skuNo,json);
+        Map<String,Integer> stockMap= new HashMap<>();
+        String key ="";
+        for(Iterator iterator= tmpMap.keySet().iterator();iterator.hasNext();){
+            key = (String)iterator.next();
+            stockMap.put(key,Integer.valueOf(tmpMap.get(key)));
+        }
+        return stockMap;
     }
 
     public static void main(String[] args) throws Exception {
-        MarylouStockImpl impl = new MarylouStockImpl();
-/*        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        logger.info("TESSABIT更新数据库开始");
-        impl.updateProductStock("2015091501331", "2015-01-01 00:00", format.format(new Date()));
-        logger.info("TESSABIT更新数据库结束");
-        System.exit(0);*/
 
-        List<String> skuNo = new ArrayList<>();
-        skuNo.add("1986242872_10");
-        Map returnMap = impl.grabStock(skuNo);
-        System.out.println("test return size is "+returnMap.keySet().size());
-        System.out.println("test return value is "+returnMap.get("1986242872_10"));
+        String host = bundle.getString("HOST");
+        String app_key = bundle.getString("APP_KEY");
+        String app_secret= bundle.getString("APP_SECRET");
+        if(StringUtils.isBlank(host)||StringUtils.isBlank(app_key)||StringUtils.isBlank(app_secret)){
+            logger.error("参数错误，无法执行更新库存");
+        }
+
+
+        MarylouStockImpl impl = new MarylouStockImpl();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        logger.info("更新数据库开始");
+        impl.updateProductStock(host,app_key,app_secret, "2015-01-01 00:00", format.format(new Date()));
+        logger.info("更新数据库结束");
+        System.exit(0);
+
+//        List<String> skuNo = new ArrayList<>();
+//        skuNo.add("1986242872_10");
+//        Map returnMap = impl.grabStock(skuNo);
+//        System.out.println("test return size is "+returnMap.keySet().size());
+//        System.out.println("test return value is "+returnMap.get("1986242872_10"));
 
     }
 }
