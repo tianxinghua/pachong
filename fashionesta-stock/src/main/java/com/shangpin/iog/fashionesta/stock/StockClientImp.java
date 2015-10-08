1a6865c10c1d708425a9c1c0a12c5ae91434d363
lizhongren <lizhongren@vansky.cn>
2015-10-08 10:42:33 +0800

    下单封装

--- fashionesta-stock/src/main/java/com/shangpin/iog/fashionesta/stock/StockClientImp.java
+++ fashionesta-stock/src/main/java/com/shangpin/iog/fashionesta/stock/StockClientImp.java
@@ -6,22 +6,37 @@ import com.shangpin.iog.fashionesta.stock.dto.Item;
 import com.shangpin.iog.fashionesta.stock.dto.Product;
 import com.shangpin.iog.fashionesta.stock.utils.DownloadAndReadCSV;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
  * Created by monkey on 2015/9/28.
  */
 public class StockClientImp extends AbsUpdateProductStock {
+
+	private static Logger logger = Logger.getLogger("info");
+	private static Logger loggerError = Logger.getLogger("error");
+
+	private static ResourceBundle bdl = null;
+	private static String supplierId;
+	private static String url;
+	static {
+		if (null == bdl)
+			bdl = ResourceBundle.getBundle("param");
+		supplierId = bdl.getString("supplierId");
+		url = bdl.getString("url");
+	}
+
 	@Autowired
 	DownloadAndReadCSV csvUtil;
 
 	@Override
 	public Map<String, String> grabStock(Collection<String> skuNo)
 			throws ServiceException, Exception {
-		ResourceBundle bundle = ResourceBundle.getBundle("param");
-		String supplierId = bundle.getString("supplier");
+
 		Map<String, String> skustock = new HashMap<>(skuNo.size());
 		List<Product> list = csvUtil.readLocalCSV();
 		Iterator<String> it = skuNo.iterator();
@@ -40,4 +55,15 @@ public class StockClientImp extends AbsUpdateProductStock {
 		}
 		return skustock;
 	}
+
+	public static void main(String[] args) throws Exception {
+		AbsUpdateProductStock stockImp = new StockClientImp();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		logger.info("更新数据库开始");
+		stockImp.updateProductStock(supplierId, "2015-01-01 00:00", format.format(new Date()));
+		logger.info("更新数据库结束");
+		System.exit(0);
+	}
+
+
 }
