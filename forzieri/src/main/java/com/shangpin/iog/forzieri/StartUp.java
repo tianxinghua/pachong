51cf49d3b6d3cc56a69917120960a145d39751fd
monkey <18623978203@163.com>
2015-10-30 14:12:47 +0800

    forzieri

---
+++ forzieri/src/main/java/com/shangpin/iog/forzieri/StartUp.java
@@ -0,0 +1,37 @@
+package com.shangpin.iog.forzieri;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.shangpin.iog.app.AppContext;
+import com.shangpin.iog.forzieri.service.FetchProduct;
+
+public class StartUp {
+	 private static Logger log = Logger.getLogger("info");
+
+	    private static ApplicationContext factory;
+		private static void loadSpringContext()
+
+		{
+
+	        factory = new AnnotationConfigApplicationContext(AppContext.class);
+		}
+		
+		public static void main(String[] args)
+		{
+
+	        //加载spring
+	        log.info("----拉取forzieri数据开始----");
+			loadSpringContext();
+	        log.info("----初始SPRING成功----");
+	        //拉取数据
+	        FetchProduct fetchProduct =(FetchProduct)factory.getBean("forzieri");
+	        fetchProduct.fetchProductAndSave();
+	        log.info("----拉取forzieri数据完成----");
+
+
+			System.out.println("-------fetch end---------");
+
+		}
+}
