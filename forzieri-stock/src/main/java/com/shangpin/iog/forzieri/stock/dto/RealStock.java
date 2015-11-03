8bb6722810e9953a008bb2ba218858f80bc125a8
monkey <18623978203@163.com>
2015-11-03 10:20:55 +0800

    forzieri -stock

---
+++ forzieri-stock/src/main/java/com/shangpin/iog/forzieri/stock/dto/RealStock.java
@@ -0,0 +1,20 @@
+package com.shangpin.iog.forzieri.stock.dto;
+
+public class RealStock {
+	private String status;
+	private SkuData data;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public SkuData getData() {
+		return data;
+	}
+	public void setData(SkuData data) {
+		this.data = data;
+	}
+	
+	
+}
