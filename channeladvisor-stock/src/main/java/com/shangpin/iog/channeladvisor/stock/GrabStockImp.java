2b28c6760c45366083413ab9f03d7396e797a287
lubaijiang <lubaijiang@shangpin.com>
2015-11-05 18:51:37 +0800

    modify channeladvisor-stock

--- channeladvisor-stock/src/main/java/com/shangpin/iog/channeladvisor/stock/GrabStockImp.java
+++ channeladvisor-stock/src/main/java/com/shangpin/iog/channeladvisor/stock/GrabStockImp.java
@@ -37,6 +37,45 @@ public class GrabStockImp extends AbsUpdateProductStock{
 			throws ServiceException, Exception {
 		//通过refresh-token获取新的token
 		OutTimeConfig timeConfig = new OutTimeConfig(1000*5, 1000*60 * 5, 1000*60 * 5);
+        String access_token = getNewToken(timeConfig);
+        //通过新的token获取库存信息
+        Map<String, String> skustock = new HashMap<>();
+		Map<String,String> stockMap = new HashMap<>();
+		
+		String url = "https://api.channeladvisor.com/v1/Products?access_token="+access_token+"&";
+		while(StringUtils.isNotBlank(url) && !url.equals("null")){
+			String content = HttpUtil45.get(url, timeConfig, null);
+			JSONObject jsonObj = JSONObject.fromObject(content);
+			if(jsonObj.containsKey("error")){
+				String newToken = getNewToken(timeConfig);
+				url = url.replaceFirst(url.substring(url.indexOf("=")+1, url.indexOf("&")),newToken);
+				content = HttpUtil45.get(url, timeConfig, null);
+				jsonObj = JSONObject.fromObject(content);
+			}
+			JSONArray array = jsonObj.getJSONArray("value");
+			for(int i=0;i<array.size();i++){
+				JSONObject obj = array.getJSONObject(i);
+				String skuId = obj.getString("Sku");
+				String stock = obj.getString("TotalAvailableQuantity");
+				if(StringUtils.isNotBlank(skuId) && StringUtils.isNotBlank(stock) && !stock.equals("0")){
+					stockMap.put(skuId, stock);
+				}
+			}
+			url = jsonObj.getString("@odata.nextLink");
+		}
+		for (String skuno : skuNo) {
+            if(stockMap.containsKey(skuno)){
+                skustock.put(skuno, stockMap.get(skuno));
+            } else{
+                skustock.put(skuno, "0");
+            }
+        }
+		
+		//System.out.println(stockMap.toString());
+		return skustock;
+	}
+	
+	public String getNewToken(OutTimeConfig timeConfig){
 		String application_id = "qwmmx12wu7ug39a97uter3dz29jbij3j";
         String shared_secret = "TqMSdN6-LkCFA0n7g7DWuQ";
         Map<String,String> map = new HashMap<>();
@@ -45,32 +84,7 @@ public class GrabStockImp extends AbsUpdateProductStock{
         map.put("redirect_uri","https://49.213.13.167:8443/iog/download/code");
         String kk = HttpUtil45.postAuth("https://api.channeladvisor.com/oauth2/token", map, timeConfig,application_id,shared_secret);
         System.out.println("kk = "  + kk);
-        String access_token = JSONObject.fromObject(kk).getString("access_token");
-        //通过新的token获取库存信息
-        Map<String, String> skustock = new HashMap<>();
-		Map<String,String> stockMap = new HashMap<>();
-		String url = "https://api.channeladvisor.com/v1/Products?access_token="+access_token;
-		String content = HttpUtil45.get(url, timeConfig, null);
-		JSONObject jsonObj = JSONObject.fromObject(content);
-		JSONArray array = jsonObj.getJSONArray("value");
-		for(int i=0;i<array.size();i++){
-			JSONObject obj = array.getJSONObject(i);
-			String skuId = obj.getString("Sku");
-			String stock = obj.getString("TotalAvailableQuantity");
-			if(StringUtils.isNotBlank(skuId) && StringUtils.isNotBlank(stock) && !stock.equals("0")){
-				stockMap.put(skuId, stock);
-			}
-			for (String skuno : skuNo) {
-	            if(stockMap.containsKey(skuno)){
-	                skustock.put(skuno, stockMap.get(skuno));
-	            } else{
-	                skustock.put(skuno, "0");
-	            }
-	        }
-		}
-		
-		//System.out.println(stockMap.toString());
-		return skustock;
+        return JSONObject.fromObject(kk).getString("access_token");
 	}
 	
 	public static void main(String[] args) {
