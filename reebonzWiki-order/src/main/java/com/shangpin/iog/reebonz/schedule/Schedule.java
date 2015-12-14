2d95c74185b9fe6b849e7bb09d907f5d88a4d9a9
lizhongren <lizhongren@vansky.cn>
2015-12-14 20:48:23 +0800

    修改date5 增加季节

--- reebonzWiki-order/src/main/java/com/shangpin/iog/reebonz/schedule/Schedule.java
+++ reebonzWiki-order/src/main/java/com/shangpin/iog/reebonz/schedule/Schedule.java
@@ -3,6 +3,7 @@ package com.shangpin.iog.reebonz.schedule;
 import com.shangpin.iog.reebonz.service.OrderImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Component;
  * Created by sunny on 2015/9/16.
  */
 @Component
+@PropertySource("classpath:conf.properties")
 public class Schedule {
 	
 	@Autowired
 	OrderImpl orderService;
 	
-	@Scheduled(cron = "0/20 * * * * ? ")
+//	@Scheduled(cron = "0/20 * * * * ? ")
+    @Scheduled(cron="${jobsSchedule}")
 	public void start() {
 		orderService.loopExecute();
 	}
