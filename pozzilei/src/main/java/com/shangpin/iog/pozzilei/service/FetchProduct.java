12ddfb962b5f1c28068c01138fa711b0b95dc916
lubaijiang <lubaijiang@shangpin.com>
2015-12-18 19:19:47 +0800

    冲突

--- pozzilei/src/main/java/com/shangpin/iog/pozzilei/service/FetchProduct.java
+++ pozzilei/src/main/java/com/shangpin/iog/pozzilei/service/FetchProduct.java
@@ -3,6 +3,7 @@ package com.shangpin.iog.pozzilei.service;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.shangpin.framework.ServiceException;
+import com.shangpin.iog.common.utils.DateTimeUtil;
 import com.shangpin.iog.common.utils.UUIDGenerator;
 import com.shangpin.iog.common.utils.httpclient.HttpUtil45;
 import com.shangpin.iog.common.utils.httpclient.HttpUtils;
@@ -12,7 +13,9 @@ import com.shangpin.iog.dto.SkuDTO;
 import com.shangpin.iog.dto.SpuDTO;
 import com.shangpin.iog.pozzilei.dto.*;
 import com.shangpin.iog.service.ProductFetchService;
+import com.shangpin.iog.service.ProductSearchService;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,9 @@ import org.springframework.stereotype.Component;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 /**
  * Created by loyalty on 15/6/8.
@@ -28,20 +34,42 @@ import java.util.Date;
 @Component("pozzilei")
 public class FetchProduct {
     final Logger logger = Logger.getLogger(this.getClass());
-    String supplierId = "2015092801547";
+    private static Logger logError = Logger.getLogger("error");
+    private static ResourceBundle bdl = null;
+	private static String supplierId = "";
+	private static int day;
+    //String supplierId = "2015092801547";
+	
+	static {
+		if (null == bdl)
+			bdl = ResourceBundle.getBundle("conf");
+		supplierId = bdl.getString("supplierId");
+		day = Integer.valueOf(bdl.getString("day"));
+	}
 
     @Autowired
     private ProductFetchService pfs;
-
-
+    
+    @Autowired
+	ProductSearchService productSearchService;
 
     public void fetchProductAndSave() {
 
         //首先获取季节码  http://185.58.119.177/spinnakerapi/Myapi/Productslist/GetAllSeasonCode?DBContext=Default&key=8IZk2x5tVN
     	//地址  http://net13serverpo.net/pozziapi/Myapi/Productslist/GetProducts?DBContext=Default&CategoryId=&BrandId=&SeasonCode=078&StartIndex=1&EndIndex=100&key=5jq3vkBd7d
-
+    	
+    	Date startDate,endDate= new Date();
+		startDate = DateTimeUtil.getAppointDayFromSpecifiedDay(endDate,day*-1,"D");
+		//获取原有的SKU 仅仅包含价格和库存
+		Map<String,SkuDTO> skuDTOMap = new HashedMap();
+		try {
+			skuDTOMap = productSearchService.findStockAndPriceOfSkuObjectMap(supplierId,startDate,endDate);
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}
+    	
         Gson gson = new Gson();
-
+        
         String[]  databaseArray = new String[] {"Default"}; //
         OutTimeConfig outTimeConfig  = new OutTimeConfig(1000*60*2,1000*60*2,1000*60*2);
         for(String database:databaseArray){
@@ -97,18 +125,24 @@ public class FetchProduct {
                                 spudto.setSpuId(spu.getProduct_id());
                                 spudto.setId(UUIDGenerator.getUUID());
                                 spudto.setMaterial(spu.getProduct_detail());
-//                                if(spu.getProduct_detail().indexOf("made in")!=-1){
-//                                	String ProductOrigin = spu.getProduct_detail().substring(spu.getProduct_detail().indexOf("made in"), spu.getProduct_detail().length());
-//                                	spudto.setProductOrigin(ProductOrigin.substring(0,ProductOrigin.indexOf(" ")));
-//                                }else{
-                                	spudto.setProductOrigin("无");
-//                                }
+                                if(spu.getProduct_detail().indexOf("Made In")!=-1){
+                                	String ProductOrigin = spu.getProduct_detail().substring(spu.getProduct_detail().indexOf("Made In")+8, spu.getProduct_detail().length());
+                                	spudto.setProductOrigin(ProductOrigin.substring(0,ProductOrigin.indexOf(" ")));
+                                }else{
+                                	spudto.setProductOrigin(" ");
+                                }
                                 spudto.setPicUrl(spu.getUrl());
                                 spudto.setSpuName(spu.getDescription());
-                                spudto.setProductOrigin(spu.getProduct_origin());
                                 try {
                                     pfs.saveSPU(spudto);
                                 } catch (ServiceException e) {
+                                	logError.error(e.getMessage());
+                	            	try{
+                	            		pfs.updateMaterial(spudto);
+                	            	}catch(ServiceException ex){
+                	            		logError.error(ex.getMessage());
+                	            		ex.printStackTrace();
+                	            	}
                                     e.printStackTrace();
                                 }
 
@@ -201,17 +235,23 @@ public class FetchProduct {
                 }
             }
         }
-
-
-
-
+        
+        
+      //更新网站不再给信息的老数据
+		for(Iterator<Map.Entry<String,SkuDTO>> itor = skuDTOMap.entrySet().iterator();itor.hasNext(); ){
+			 Map.Entry<String,SkuDTO> entry =  itor.next();
+			if(!"0".equals(entry.getValue().getStock())){//更新不为0的数据 使其库存为0
+				entry.getValue().setStock("0");
+				try {
+					pfs.updatePriceAndStock(entry.getValue());
+				} catch (ServiceException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
 
     }
-
-
-
-
 
 
 }
