227c3dd1de0d3ca564c8579849aa9457844a1658
monkey <18623978203@163.com>
2016-01-12 15:12:01 +0800

    daniello拉去产品 更新库存

--- Daniello/src/main/java/com/shangpin/iog/daniello/dto/Item.java
+++ Daniello/src/main/java/com/shangpin/iog/daniello/dto/Item.java
@@ -1,5 +1,10 @@
 package com.shangpin.iog.daniello.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Item {
 	private String spuId;
 	private String marketPrice;
@@ -8,47 +13,6 @@ public class Item {
 	private String color;
 	private String description;
 	private String productCode;
-	public String getSpuId() {
-		return spuId;
-	}
-	public void setSpuId(String spuId) {
-		this.spuId = spuId;
-	}
-	public String getMarketPrice() {
-		return marketPrice;
-	}
-	public void setMarketPrice(String marketPrice) {
-		this.marketPrice = marketPrice;
-	}
-	public String getSalePrice() {
-		return salePrice;
-	}
-	public void setSalePrice(String salePrice) {
-		this.salePrice = salePrice;
-	}
-	public String getSupplierPrice() {
-		return supplierPrice;
-	}
-	public void setSupplierPrice(String supplierPrice) {
-		this.supplierPrice = supplierPrice;
-	}
-	public String getColor() {
-		return color;
-	}
-	public void setColor(String color) {
-		this.color = color;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getProductCode() {
-		return productCode;
-	}
-	public void setProductCode(String productCode) {
-		this.productCode = productCode;
-	}
-	
+	private String styleCode;
+	private String colorCode;
 }
