8f309197e59b03b427a993438303459f50107fb7
monkey <18623978203@163.com>
2016-01-22 17:49:34 +0800

    syso

--- smets-stock/src/main/java/com/shangpin/iog/smets/stock/StockClientImp.java
+++ smets-stock/src/main/java/com/shangpin/iog/smets/stock/StockClientImp.java
@@ -47,7 +47,7 @@ public class StockClientImp extends AbsUpdateProductStock {
 	public Map<String, String> grabStock(Collection<String> skuNo)
 			throws ServiceException, Exception {
 		String skuId ="";
-		
+		System.out.println("开始获取sku");
 		List<TxtDTO> skuLists = TXTUtil.readLocalCSV(TxtDTO.class, ";");
 		System.out.println("获取sku完毕");
 		logger.info("获取sku完毕");
@@ -75,15 +75,19 @@ public class StockClientImp extends AbsUpdateProductStock {
 
 	public static void main(String[] args) throws Exception {
 		//加载spring
+		System.out.println("加载spring");
         loadSpringContext();
+        System.out.println("加载spring结束");
         StockClientImp stockImp =(StockClientImp)factory.getBean("smetsstock");
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		logger.info("更新数据库开始");
+		System.out.println("更新数据库开始");
 		try {
 			stockImp.updateProductStock(supplierId, "2015-01-01 00:00", format.format(new Date()));
 		} catch (Exception e) {
 			logger.info("更新库存数据库出错"+e.toString());
 		}
+		System.out.println("更新数据库结束");
 		logger.info("更新数据库结束");
 		System.exit(0);
 	}
