6b2c24f5a8ffe06f2cae995f89f7cff21e3d18db
zhaogenchun <1569762716@qq.com>
2016-01-27 16:32:49 +0800

    s

--- iog-common-product2/src/main/java/com/shangpin/iog/product/service/ProductSearchServiceImpl.java
+++ iog-common-product2/src/main/java/com/shangpin/iog/product/service/ProductSearchServiceImpl.java
@@ -834,10 +834,4 @@ public class ProductSearchServiceImpl implements ProductSearchService {
 		}
 	}
 
-	@Override
-	public ProductDTO findProductBySupplierIdAndSkuId(String supplierId,
-			String skuId) throws ServiceException {
-		return  productDAO.findProductBySupplierIdAndSkuId(supplierId,skuId);
-	}
-
 }
