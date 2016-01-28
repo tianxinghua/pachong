8abf7a787528a14d57cbfc24b45af24addbcadb4
zhaogenchun <1569762716@qq.com>
2016-01-28 11:24:38 +0800

    efashion-order提交

---
+++ efashion-order/src/main/java/com/shangpin/iog/efashion/dto/Result.java
@@ -0,0 +1,16 @@
+package com.shangpin.iog.efashion.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Result {
+	
+	private String reqCode;
+	private String description;
+	private String result;
+	
+}
---
+++ efashion-order/src/main/java/com/shangpin/iog/efashion/dto/ReturnObject.java
@@ -0,0 +1,13 @@
+package com.shangpin.iog.efashion.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ReturnObject {
+
+	private Result results;
+}
