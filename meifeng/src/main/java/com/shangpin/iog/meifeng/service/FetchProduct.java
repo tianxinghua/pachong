30682f8d3af955c2f7b735dd56585cbd35dc7385
monkey <18623978203@163.com>
2016-02-15 10:15:43 +0800

    美峰

---
+++ meifeng/src/main/java/com/shangpin/iog/meifeng/service/FetchProduct.java
@@ -0,0 +1,149 @@
+package com.shangpin.iog.meifeng.service;
+
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.shangpin.framework.ServiceException;
+import com.shangpin.iog.common.utils.DateTimeUtil;
+import com.shangpin.iog.common.utils.UUIDGenerator;
+import com.shangpin.iog.dto.SkuDTO;
+import com.shangpin.iog.dto.SpuDTO;
+import com.shangpin.iog.meifeng.dto.MeiFengSPU;
+import com.shangpin.iog.meifeng.dto.MeiFengSku;
+import com.shangpin.iog.meifeng.util.Excel2DTO;
+import com.shangpin.iog.service.ProductFetchService;
+import com.shangpin.iog.service.ProductSearchService;
+
+/**
+ * Created by houkun on 2016/01/25.
+ */
+@Component("meifeng")
+public class FetchProduct {
+	private static Logger logger = Logger.getLogger("info");
+	private static ResourceBundle bdl = null;
+	private static String supplierId;
+	private static String filepath;
+	public static int day;
+
+	static {
+		if (null == bdl)
+			bdl = ResourceBundle.getBundle("conf");
+		supplierId = bdl.getString("supplierId");
+		filepath = bdl.getString("filepath");
+		day = Integer.valueOf(bdl.getString("day"));
+		
+	}
+	@Autowired
+	ProductSearchService productSearchService;
+	@Autowired
+	private ProductFetchService productFetchService;
+    public void fetchProductAndSave(){
+        	Date startDate,endDate= new Date();
+			startDate = DateTimeUtil.getAppointDayFromSpecifiedDay(endDate,day*-1,"D");
+			//获取原有的SKU 仅仅包含价格和库存
+			Map<String,SkuDTO> skuDTOMap = new HashedMap();
+			try {
+				skuDTOMap = productSearchService.findStockAndPriceOfSkuObjectMap(supplierId,startDate,endDate);
+			} catch (ServiceException e) {
+				e.printStackTrace();
+			}
+			System.out.println("请求数据");
+			Excel2DTO excel2dto = new Excel2DTO();
+			Short[] needColsNo = new Short[]{1,2,3,4,5,6,7,23,24,26,28,31,32,33,34};
+			List<MeiFengSPU> spuList = excel2dto.toDTO(filepath, 0, needColsNo, MeiFengSPU.class);
+			
+			Map<String,MeiFengSPU> spuMap = new HashMap<String, MeiFengSPU>();
+			
+			Short[] stockneedColsNo = new Short[]{0,1,4,5};
+			List<MeiFengSku> skuList = excel2dto.toDTO(filepath, 1, stockneedColsNo , MeiFengSku.class);
+			
+			System.out.println("开始保存spu");
+			
+			for (MeiFengSPU product : spuList) {
+				spuMap.put(product.getSpuId(), product);
+				SpuDTO spu = new SpuDTO();
+				spu.setId(UUIDGenerator.getUUID());
+				spu.setSupplierId(supplierId);
+				spu.setSpuId(product.getSpuId());
+				spu.setBrandName(product.getBrandName());
+				spu.setCategoryName(product.getCategory());
+				spu.setCategoryGender(product.getGender().contains("男")?"男":"女");
+				spu.setSeasonName(product.getYear()+product.getSeason());
+				spu.setMaterial(product.getMaterial());
+				spu.setProductOrigin(product.getMadeIn());
+				spu.setSpuName(product.getSpuName());
+				//保存spu
+				try {
+					productFetchService.saveSPU(spu);
+				} catch (ServiceException e) {
+					e.printStackTrace();
+					try {
+						productFetchService.updateMaterial(spu);
+					} catch (ServiceException e1) {
+						e1.printStackTrace();
+					}
+				}
+				
+			}
+			
+			//保存sku
+			MeiFengSPU detail = null;
+			for (MeiFengSku item : skuList) {
+				SkuDTO sku = new SkuDTO();
+				detail = spuMap.get(item.getSpuId());
+				sku.setId(UUIDGenerator.getUUID());
+				sku.setSupplierId(supplierId);
+				sku.setSpuId(item.getSpuId());
+				sku.setSkuId(item.getSkuId());
+				sku.setProductSize(item.getSize());
+				sku.setProductCode(detail.getProductCode());
+				sku.setStock(item.getQty());
+				sku.setColor(detail.getColor());
+				sku.setMarketPrice(detail.getMarketPrice());
+				sku.setSupplierPrice(detail.getSupplierPrice());
+				sku.setSaleCurrency("HDK");
+				try {
+					
+					if(skuDTOMap.containsKey(sku.getSkuId())){
+						skuDTOMap.remove(sku.getSkuId());
+					}
+					productFetchService.saveSKU(sku);
+				} catch (ServiceException e) {
+					try {
+	    				if (e.getMessage().equals("数据插入失败键重复")) {
+	    					//更新价格和库存
+	    					productFetchService.updatePriceAndStock(sku);
+	    				} else {
+	    					e.printStackTrace();
+	    				}
+	    			} catch (ServiceException e1) {
+	    				e1.printStackTrace();
+	    			}			
+				}
+			}
+        	//更新网站不再给信息的老数据
+			for(Iterator<Map.Entry<String,SkuDTO>> itor = skuDTOMap.entrySet().iterator();itor.hasNext(); ){
+				 Map.Entry<String,SkuDTO> entry =  itor.next();
+				if(!"0".equals(entry.getValue().getStock())){//更新不为0的数据 使其库存为0
+					entry.getValue().setStock("0");
+					try {
+						productFetchService.updatePriceAndStock(entry.getValue());
+					} catch (ServiceException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+    }
+    public static void main(String[] args) {
+    }
+}
---
+++ meifeng/src/main/java/com/shangpin/iog/meifeng/util/Excel2DTO.java
@@ -0,0 +1,100 @@
+package com.shangpin.iog.meifeng.util;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.shangpin.iog.meifeng.dto.MeiFengSku;
+/**
+ * @author houkun
+ * @version 20160203
+ */
+public class Excel2DTO {
+	/**
+	 * 将excel文件转换成dto
+	 * @param filePath excel路径
+	 * @param sheetNum 要转换excel的第几个sheet
+	 * @param needColsNo 需要excel的哪几列
+	 * @param clazz 要转换成的dto
+	 * @return List<DTO>
+	 */
+	@SuppressWarnings("resource")
+	public <T> List<T> toDTO(String filePath, Integer sheetNum,
+			Short[] needColsNo, Class<T> clazz) {
+		List<T> returnList = new ArrayList<T>();
+		XSSFWorkbook wb = null;
+		
+		XSSFSheet sheet = null;
+		try {
+			wb = new XSSFWorkbook(filePath);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		XSSFRow row = null;
+		sheet = wb.getSheetAt(sheetNum);
+		for (int j = 1; j < sheet.getPhysicalNumberOfRows(); j++) {
+			row = sheet.getRow(j);
+			try {
+				T t = fillDTO(clazz.newInstance(), row, needColsNo);
+				returnList.add(t);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return returnList;
+	}
+
+	private <T> T fillDTO(T t, XSSFRow row, Short[] needColsNo) {
+		try {
+			Field[] fields = t.getClass().getDeclaredFields();
+			for (int i = 0; i < needColsNo.length; i++) {
+				fields[i].setAccessible(true);
+				fields[i].set(t, getStrinCellValue(row.getCell(needColsNo[i])));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return t;
+	}
+
+	private String getStrinCellValue(XSSFCell xssfCell) {
+
+		if (xssfCell == null) {
+			return "";
+		}
+		String strCell = "";
+		switch (xssfCell.getCellType()) {
+		case XSSFCell.CELL_TYPE_STRING:
+			strCell = xssfCell.toString();
+			break;
+		case XSSFCell.CELL_TYPE_NUMERIC:
+			strCell = String.valueOf((int)xssfCell.getNumericCellValue());
+			break;
+		case XSSFCell.CELL_TYPE_BOOLEAN:
+			strCell = String.valueOf(xssfCell.getStringCellValue());
+		case XSSFCell.CELL_TYPE_BLANK:
+			strCell = "";
+			break;
+		default:
+			strCell = "";
+		}
+		if (strCell.equals("") || strCell == null) {
+			return "";
+		}
+		return strCell;
+	}
+	public static void main(String[] args) {
+		String filePath = "E:\\20160126美峰供货尚品.xls";
+		Integer sheetNum = 1;
+		Short[] needColsNo = new Short[]{0,1,4,5};
+		List<MeiFengSku> dto = new Excel2DTO().toDTO(filePath, sheetNum, needColsNo, MeiFengSku.class);
+		System.out.println(dto.size());
+	}
+}
