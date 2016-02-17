bd8771d30964d89d759a3c704f0bdb805ed69461
zhaogenchun <1569762716@qq.com>
2016-02-17 10:19:20 +0800

    inviqa提交

--- inviqa/src/main/java/com/shangpin/iog/inviqa/service/FetchProduct.java
+++ inviqa/src/main/java/com/shangpin/iog/inviqa/service/FetchProduct.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
@@ -22,6 +23,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.model.OAuthRequest;
 import com.github.scribejava.core.model.Response;
 import com.github.scribejava.core.model.Token;
@@ -33,8 +35,8 @@ import com.shangpin.framework.ServiceException;
 import com.shangpin.iog.common.utils.UUIDGenerator;
 import com.shangpin.iog.dto.SkuDTO;
 import com.shangpin.iog.dto.SpuDTO;
+import com.shangpin.iog.inviqa.dto.API;
 import com.shangpin.iog.inviqa.dto.Product;
-import com.shangpin.iog.inviqa.util.MyJsonUtil;
 import com.shangpin.iog.service.EventProductService;
 import com.shangpin.iog.service.ProductFetchService;
 
@@ -51,21 +53,57 @@ public class FetchProduct {
 	EventProductService eventProductService;
 	private static Logger logger = Logger.getLogger("info");
 	private static ResourceBundle bdl = null;
+	private static String MAGENTO_API_KEY = null;
+    private static String MAGENTO_API_SECRET = null;
+    private static String MAGENTO_REST_API_URL = null;
 	private static String supplierId;
 	static {
 		if (null == bdl)
 			bdl = ResourceBundle.getBundle("conf");
 		supplierId = bdl.getString("supplierId");
+		
+		MAGENTO_API_KEY = bdl.getString("MAGENTO_API_KEY");
+		MAGENTO_API_SECRET = bdl.getString("MAGENTO_API_SECRET");
+		MAGENTO_REST_API_URL = bdl.getString("MAGENTO_REST_API_URL");
 	}
+    
+    
+	private int i = 1;
 	
-	
+	public  void getProduct(OAuthService service,Token accessToken,int page){
+		//https://glamorous-uat.phplab.co.uk/api/rest
+		OAuthRequest request = new OAuthRequest(Verb.GET,
+				"http://glamorous-uat.phplab.co.uk/api/rest/shangpin/product?limit=100&page="+i,
+				service);
+		service.signRequest(accessToken, request);
+		Response response = request.send();
+		String json = response.getBody();
+//		System.out.println(json);
+		if(!json.isEmpty()){
+			List<Product> retList = new Gson().fromJson(json,  
+	                new TypeToken<List<Product>>() {  
+	                }.getType());  
+			System.out.println("拉取的数量："+retList.size());
+			if(retList.size()==100){
+				i++;
+				messMappingAndSave(retList);
+				System.out.println("save success");
+				getProduct(service,accessToken,i);
+			}else{
+				messMappingAndSave(retList);
+			}
+		}
+	}
 	/**
 	 * fetch product and save into db
 	 */
 	public void fetchProductAndSave() {
-
-		List<Product> list = MyJsonUtil.getProductList();
-		messMappingAndSave(list);
+		
+		OAuthService service = new ServiceBuilder().provider(API.class)
+				.apiKey(MAGENTO_API_KEY).apiSecret(MAGENTO_API_SECRET).build();
+		Token accessToken = new Token("tcm525shdvbw0jg68ju87njxpmwot41v",
+				"hugcwsx5e8yrze1z7c11hgdaxe8aeits");
+		getProduct(service,accessToken,0);
 	}
 	
 	
@@ -76,10 +114,7 @@ public class FetchProduct {
 	private void messMappingAndSave(List<Product> array) {
 
 		if (array != null) {
-			
-			
 			for (Product item : array) {
-				
 				if(item.getSkuId()!=null){
 					SpuDTO spu = new SpuDTO();
 					try {
