22e1345b233255ea5b6984ea0f1073c00e02b0ec
lubaijiang <lubaijiang@shangpin.com>
2016-02-26 11:08:09 +0800

    chong tu

--- KIX/src/main/java/com/shangpin/iog/kix/util/KIXUtil.java
+++ KIX/src/main/java/com/shangpin/iog/kix/util/KIXUtil.java
@@ -1,6 +1,6 @@
 package com.shangpin.iog.kix.util;
 
-public class KIXUtil {
+public class KIXUtil  {
 	
 	public static String getAllProducts(){
 		//https://c1e1d0fa10d3f3dc9ec8390e1aaeb007:0fc94701904077a2fb8eca2da6800522@kix-files.myshopify.com/admin/products.json
