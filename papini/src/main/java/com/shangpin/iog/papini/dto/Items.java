e34a602212de6ab0d0bd654fb7d322e77ccb75a3
houkun <houkun@shangpin.com>
2016-03-01 15:32:14 +0800

    papini

---
+++ papini/src/main/java/com/shangpin/iog/papini/dto/Items.java
@@ -0,0 +1,25 @@
+package com.shangpin.iog.papini.dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * Created by loyalty on 15/6/5.
+ */
+@XmlRootElement(name="items")
+@XmlAccessorType(XmlAccessType.NONE)
+public class Items {
+    @XmlElement(name="item")
+    private List<Item> items;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
