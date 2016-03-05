e3f85c2884740c12fd32bff959133790337b2087
lizhongren <lizhongren@vansky.cn>
2016-03-05 12:44:05 +0800

    Daniello 修改 部署到香港

--- Daniello-stock/src/main/java/com/shangpin/iog/daniello/stock/DanielloStockImp.java
+++ Daniello-stock/src/main/java/com/shangpin/iog/daniello/stock/DanielloStockImp.java
@@ -1,11 +1,13 @@
 package com.shangpin.iog.daniello.stock;
 
 import com.shangpin.framework.ServiceException;
-import com.shangpin.ice.ice.AbsUpdateProductStock;
+
+
 import com.shangpin.iog.app.AppContext;
 import com.shangpin.iog.common.utils.httpclient.HttpUtil45;
 import com.shangpin.iog.common.utils.httpclient.OutTimeConfig;
 
+import com.shangpin.sop.AbsUpdateProductStock;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
@@ -23,6 +25,7 @@ public class DanielloStockImp  extends AbsUpdateProductStock {
     private static Logger logger = Logger.getLogger("info");
     private static ResourceBundle bdl=null;
     private static String supplierId;
+    private static String user,password;
     private static String url;
     private static ApplicationContext factory;
     private static void loadSpringContext()
@@ -31,16 +34,18 @@ public class DanielloStockImp  extends AbsUpdateProductStock {
     }
     static {
         if(null==bdl)
-            bdl=ResourceBundle.getBundle("conf");
+            bdl=ResourceBundle.getBundle("sop");
         supplierId = bdl.getString("supplierId");
         url = bdl.getString("url");
+        user = bdl.getString("user");
+        password = bdl.getString("password");
     }
     @Override
-    public Map<String,String> grabStock(Collection<String> skuNo) throws ServiceException, Exception {
+    public Map<String,Integer> grabStock(Collection<String> skuNo) throws ServiceException, Exception {
     	Map<String,String> skuMap = new HashMap<String,String>();
     	String data = "";
-    	String skuData = HttpUtil45.post(url+"GetAllAvailabilityMarketplace",
-				new OutTimeConfig(1000*60*10,1000*60*10,1000*60*10));
+    	String skuData = HttpUtil45.postAuth(url+"GetAllAvailabilityMarketplace", null,
+				new OutTimeConfig(1000*60*10,1000*60*60,1000*60*60),user,password);
 		String[] skuStrings = skuData.split("\\r\\n");
 		for (int i = 1; i < skuStrings.length; i++) {
 			if (StringUtils.isNotBlank(skuStrings[i])) {
@@ -56,7 +61,7 @@ public class DanielloStockImp  extends AbsUpdateProductStock {
         			skuMap.put(skuArr[0]+"-"+barCode, stock);
 			}
 		}
-        Map<String,String> returnMap = new HashMap<String,String>();
+        Map<String,Integer> returnMap = new HashMap<String,Integer>();
         Iterator<String> iterator=skuNo.iterator();
         //为供应商循环赋值
         logger.info("循环赋值");
@@ -67,9 +72,13 @@ public class DanielloStockImp  extends AbsUpdateProductStock {
         	if (StringUtils.isNotBlank(skuId)) {
         		if (skuMap.containsKey(skuId)) {
         			stock = skuMap.get(skuId);
-        			returnMap.put(skuId, stock);
-				}else{
-					returnMap.put(skuId, "0");
+                    try {
+                        returnMap.put(skuId, Integer.valueOf(stock));
+                    } catch (NumberFormatException e) {
+                        returnMap.put(skuId, 0);
+                    }
+                }else{
+					returnMap.put(skuId, 0);
 				}
 			}
         }
@@ -83,7 +92,11 @@ public class DanielloStockImp  extends AbsUpdateProductStock {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         logger.info("daniello更新数据库开始");
         try {
-			stockImp.updateProductStock(supplierId, "2015-01-01 00:00", format.format(new Date()));
+            String host = bdl.getString("HOST");
+            String app_key = bdl.getString("APP_KEY");
+            String app_secret= bdl.getString("APP_SECRET");
+            String startDate = bdl.getString("startDate");
+			stockImp.updateProductStock(host,app_key,app_secret,supplierId, startDate, format.format(new Date()));
 		} catch (Exception e) {
 			logger.info("daniello更新库存数据库出错"+e.toString());
 		}
--- log-template/src/main/java/com/shangpin/logger/LoggerUtil.java
+++ log-template/src/main/java/com/shangpin/logger/LoggerUtil.java
@@ -15,7 +15,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
 
-public class LoggerUtil {
+public class LoggerUtil  {
 
 	private final Logger logger;
 
