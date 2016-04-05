3e3a56b0fcef78b65b4cf9c326d86798544dd173
houkun <houkun@shangpin.com>
2016-04-05 11:24:22 +0800

    hk edit

---
+++ divo/src/main/java/com/test/quartz/factory/ScheduleFactoryBean.java
@@ -0,0 +1,8 @@
+package com.test.quartz.factory;
+
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.stereotype.Component;
+@Component("schedulerFactoryBean")
+public class ScheduleFactoryBean extends SchedulerFactoryBean{
+
+}
