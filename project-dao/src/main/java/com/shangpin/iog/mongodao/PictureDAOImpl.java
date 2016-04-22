e938ee2971e5ed2bbf0e3983b84ec6bbc762ed06
houkun <houkun@shangpin.com>
2016-04-22 18:07:47 +0800

    mongo romove

--- project-dao/src/main/java/com/shangpin/iog/mongodao/PictureDAOImpl.java
+++ project-dao/src/main/java/com/shangpin/iog/mongodao/PictureDAOImpl.java
@@ -1,14 +1,15 @@
 package com.shangpin.iog.mongodao;
 
-import com.shangpin.framework.ServiceException;
-import com.shangpin.iog.mongodomain.ProductPicture;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+import static org.springframework.data.mongodb.core.query.Update.update;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
-import static org.springframework.data.mongodb.core.query.Query.query;
-import static org.springframework.data.mongodb.core.query.Update.update;
-import static org.springframework.data.mongodb.core.query.Criteria.where;
+import com.shangpin.framework.ServiceException;
+import com.shangpin.iog.mongodomain.Test;
 
 /**
  * Created by lizhongren on 2016/4/21.
@@ -20,6 +21,7 @@ public class PictureDAOImpl {
 
 
     public void updatePicStatus(String _id) throws ServiceException {
-        mongoTemplate.updateFirst(query(where("name").is("Joe")), update("age", 35),ProductPicture.class) ;
+    	mongoTemplate.remove(query(where("name").is( _id )),Test.class);
+//        mongoTemplate.updateFirst(query(where("name").is( _id )), update("age", 66),Test.class) ;
     };
 }
