d464bd174ac3b4669f3de9cccaa6a3adf8786361
houkun <houkun@shangpin.com>
2016-05-10 17:41:54 +0800

    revolve

---
+++ revolve/src/main/java/com/shangpin/iog/revolve/sepStrategy/BackImp.java
@@ -0,0 +1,18 @@
+package com.shangpin.iog.revolve.sepStrategy;
+
+import java.util.List;
+
+public class BackImp extends ISepStrategy{
+
+	public BackImp(String strategy) {
+		super(strategy);
+	}
+
+	@Override
+	public String merge(List<String> dataList) {
+		String[] split = strategy.split("%");
+		return dataList.get(0)+split[3]+dataList.get(1).split(split[1])[Integer.valueOf(split[2])];
+	}
+
+
+}
