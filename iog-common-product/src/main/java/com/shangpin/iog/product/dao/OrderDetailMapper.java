6fd27fa79724c2b6aee186cd127db5d7d8f70272
houkun <houkun@shangpin.com>
2016-05-27 12:03:22 +0800

    service

--- iog-common-product/src/main/java/com/shangpin/iog/product/dao/OrderDetailMapper.java
+++ iog-common-product/src/main/java/com/shangpin/iog/product/dao/OrderDetailMapper.java
@@ -1,8 +1,11 @@
 package com.shangpin.iog.product.dao;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
 
 import com.shangpin.iog.dao.base.IBaseDao;
 import com.shangpin.iog.dao.base.Mapper;
@@ -10,8 +13,8 @@ import com.shangpin.iog.dto.OrderDTO;
 import com.shangpin.iog.dto.OrderDetailDTO;
 
 @Mapper
-public interface OrderDetailMapper extends IBaseDao<OrderDetailDTO>{
-	
+public interface OrderDetailMapper extends IBaseDao<OrderDetailDTO> {
+
 	public void saveOrderDetailDTO(OrderDetailDTO orderDetailDTO);
 
 	/**
@@ -20,20 +23,24 @@ public interface OrderDetailMapper extends IBaseDao<OrderDetailDTO>{
 	 * @param orderDetailDTO
 	 */
 	public void updateByOrderNo(OrderDetailDTO orderDetailDTO);
+
 	/**
 	 * 通过主订单号查询子订单列表
 	 * 
 	 * @param masterOrderNo
 	 * @return 子订单列表
 	 */
-	public List<OrderDetailDTO> findSubOrderListByMOrderNo(@Param("spMorderNo") String masterOrderNo);
+	public List<OrderDetailDTO> findSubOrderListByMOrderNo(
+			@Param("spMorderNo") String masterOrderNo);
+
 	/**
 	 * 通过采购单号查询子订单
 	 * 
 	 * @param spPurchaseNo
 	 * @return 子订单
 	 */
-	public OrderDetailDTO findSubOrderByPurchaseNo(@Param("purchaseNo") String spPurchaseNo);
+	public OrderDetailDTO findSubOrderByPurchaseNo(
+			@Param("purchaseNo") String spPurchaseNo);
 
 	/**
 	 * 通过自定义号查询子订单
@@ -43,6 +50,7 @@ public interface OrderDetailMapper extends IBaseDao<OrderDetailDTO>{
 	 * @return 子订单 OrderDetailDTO
 	 */
 	public OrderDetailDTO findSubOrderByOrderNo(@Param("orderNo") String orderNo);
+
 	/**
 	 * 根据订单唯一编号获取信息
 	 * 
@@ -51,7 +59,7 @@ public interface OrderDetailMapper extends IBaseDao<OrderDetailDTO>{
 	 * @return OrderDetailDTO
 	 */
 	public OrderDetailDTO findByUuId(@Param("uuid") String uuid);
-	
+
 	/**
 	 * 通过supplierId,SupplierNo,status查询子订单
 	 * 
@@ -64,6 +72,7 @@ public interface OrderDetailMapper extends IBaseDao<OrderDetailDTO>{
 			@Param("supplierId") String supplierId,
 			@Param("supplierNo") String supplierNo,
 			@Param("status") String status);
+
 	/**
 	 * 查询供货商异常的待处理订单
 	 * 
@@ -77,8 +86,69 @@ public interface OrderDetailMapper extends IBaseDao<OrderDetailDTO>{
 	 */
 	public List<OrderDetailDTO> findBySupplierIdAndOrderStatusAndDateAndExcSatus(
 			@Param("supplierId") String supplierId,
-			@Param("supplierNo") String supplierNo,
 			@Param("status") String status, @Param("excState") String excState,
 			@Param("date") String date, @Param("interval") int interval);
-	
+	/**
+	 * 通过status,supplierid查询
+	 * @param supplierId
+	 * @param status
+	 * @return
+	 */
+	public List<OrderDetailDTO> findBySupplierIdAndStatus(
+			@Param("supplierId") String supplierId,
+			@Param("status") String status);
+	/**
+	 * 通过status supplierid date 查询未超过12h的
+	 * @param supplierId
+	 * @param status
+	 * @param date
+	 * @return
+	 */
+	public List<OrderDetailDTO> findBySupplierIdAndStatusAndDate(
+			@Param("supplierId") String supplierId,
+			@Param("status") String status, @Param("date") String date);
+    /**
+     * 查询订单
+     * @param supplierId
+     * @param status 订单状态
+     * @param startTime 开始时间
+     * @param endTime 结束时间
+     * @return
+     */
+    public List<OrderDetailDTO> findBySupplierIdAndStatusAndTime(@Param("supplierId") String supplierId,
+            @Param("status") String status,@Param("startTime")String startTime,@Param("endTime")String endTime) ;
+
+    /**
+     * 根据订单更新时间查询订单
+     * @param supplierId
+     * @param status
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    public List<OrderDetailDTO> findBySupplierIdAndStatusAndUpdateTime(@Param("supplierId") String supplierId,
+            @Param("status") String status,@Param("startTime")String startTime,@Param("endTime")String endTime) ;
+
+    public List<OrderDetailDTO> getOrderBySupplierIdAndTime(@Param("supplierId")String supplier, @Param("startDate")Date startDate,
+    		@Param("endDate")Date endDate,RowBounds rowBounds);
+    
+    public List<OrderDetailDTO> getOrderBySupplierIdAndTime(@Param("supplierId")String supplier, @Param("startDate")Date startDate, @Param("endDate")Date endDate);
+    /**
+     * 获取异常订单信息
+     * @return
+     */
+    public List<OrderDetailDTO> findExceptionOrder() ;
+    /**
+     * 更新订单状态
+     * @param paraMap
+     * @return
+     */
+    public int updateOrderStatus(Map<String,String> paraMap);
+
+    /**
+     * 增加异常信息
+     * @param paraMap
+     * @return
+     */
+    public int updateOrderExceptionMsg(Map<String,String> paraMap);
 }
--- iog-service/src/main/java/com/shangpin/iog/service/OrderDetailService.java
+++ iog-service/src/main/java/com/shangpin/iog/service/OrderDetailService.java
@@ -1,8 +1,11 @@
 package com.shangpin.iog.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import com.shangpin.framework.ServiceException;
+import com.shangpin.iog.dto.OrderDTO;
 import com.shangpin.iog.dto.OrderDetailDTO;
 
 public interface OrderDetailService {
@@ -26,5 +29,103 @@ public interface OrderDetailService {
 	 */
 	public List<OrderDetailDTO> getOrderDetailByMorderNo(String masterOrderNo)throws ServiceException;
 	
-	
+	   /**
+     * 更新订单信息
+     * @param orderDTO
+     * @throws ServiceException
+     */
+    public void update(OrderDetailDTO orderDetailDTO) throws ServiceException;
+    	
+    /**
+     * 获取订单状态 根据供货商ID 和 订单状态
+     * @param supplierId 供货商ID
+     * @param status 状态码
+     * @return
+     * @throws ServiceException
+     */
+    public List<String> getOrderIdBySupplierIdAndOrderStatus(String supplierId,String status) throws ServiceException;
+    
+    /**
+     * 判断时间是否超过一天
+     * @param supplierId
+     * @param status
+     * @param date
+     * @return
+     * @throws ServiceException
+     */
+    public List<OrderDetailDTO>  getOrderBySupplierIdAndOrderStatus(String supplierId,String status,String date) throws ServiceException;
+    /**
+     * 查询异常的订单
+     * @param supplierId   ：供货商ID
+     * @param status   : 订单状态
+     * @param excState  ：异常状态
+     * @param date  ：需要的处理时间
+     * @param interval  :时间间隔
+     * @return
+     * @throws ServiceException
+     */
+    public List<OrderDetailDTO>  getOrderBySupplierIdAndOrderStatusAndExceptionStatus(String supplierId,String status,String excState ,String date,int interval) throws ServiceException;
+
+    /**
+     * 根据订单的创建时间获取订单   根据订单ID,订单状态,开始时间,结束时间
+     * @param supplierId
+     * @param status
+     * @param startTime
+     * @param endTime
+     * @return
+     * @throws ServiceException
+     */
+    public List<OrderDetailDTO>  getOrderBySupplierIdAndOrderStatusAndTime(String supplierId,String status,String startTime,String endTime) throws ServiceException;
+
+    /**
+     *  根据更新状态的时间 获取订单   根据订单ID,订单状态,开始时间,结束时间
+      * @param supplierId
+     * @param status
+     * @param startTime
+     * @param endTime
+     * @return
+     * @throws ServiceException
+     */
+    public List<OrderDetailDTO>  getOrderBySupplierIdAndOrderStatusAndUpdateTime(String supplierId,String status,String startTime,String endTime) throws ServiceException;
+    /**
+     * 查询订单
+     * @param supplier
+     * @param startDate
+     * @param endDate
+     * @param pageIndex
+     * @param pageSize
+     * @return
+     */
+    public List<OrderDetailDTO> getOrderBySupplierIdAndTime(String supplier, Date startDate,
+			Date endDate,Integer pageIndex, Integer pageSize);
+    
+    /**
+     * 查询订单，不分页
+     * @param supplier
+     * @param startDate
+     * @param endDate
+     * @return
+     */
+    public List<OrderDetailDTO> getOrderBySupplierIdAndTime(String supplier, Date startDate, Date endDate);
+    /**
+     * 获取异常订单信息
+     * @return
+     * @throws ServiceException
+     */
+    public List<OrderDetailDTO> getExceptionOrder() throws ServiceException;
+    
+    /**
+     * 修改订单状态
+     * @param statusMap 状态信息  ORDERID(UUID),STATUS
+     * @throws ServiceException
+     */
+    public void updateOrderStatus(Map<String,String> statusMap) throws ServiceException;
+
+    /**
+     * 增加异常信息
+     * @param exceptionMap key 包括 excState，excDesc,excTime,uuid
+     * @throws ServiceException
+     */
+    public void  updateExceptionMsg(Map<String,String> exceptionMap) throws ServiceException;
+
 }
