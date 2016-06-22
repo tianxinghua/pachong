ee6bfad879135711efca1e9be922929337ff348a
houkun <houkun@shangpin.com>
2016-06-22 19:22:30 +0800

    注释

--- iog-generater-util/src/main/java/com/shangpin/iog/generater/util/Xml2DTO.java
+++ iog-generater-util/src/main/java/com/shangpin/iog/generater/util/Xml2DTO.java
@@ -17,7 +17,14 @@ import com.shangpin.iog.generater.util.xml.XMLConvertHandler;
 
 
 public class Xml2DTO {
-	
+	/**
+	 * 
+	 * @param xmlFile
+	 * @param skuStartTag sku开始标签,若为"",则返回为sku的list,否则对象中的productList保存了sku对象
+	 * @param spuStartTag spu开始标签
+	 * @param tagNameMap 标签与productDTO字段对照   如 标签名skucode,skucode=skuId
+	 * @return
+	 */
 	public static List<ProductDTO> toDTO(File xmlFile,String skuStartTag,String spuStartTag,Map<String,String> tagNameMap){
 		List<ProductDTO> spuList = null;
 		try {
