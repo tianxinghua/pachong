91b36444ec0c8e6969dc30dfafffee0bf3603380
lizhongren <lizhongren@vansky.cn>
2016-08-20 14:25:23 +0800

    图片下载

---
+++ picturedown/src/main/java/com/shangpin/iog/picture/service/PictureDownService.java
@@ -0,0 +1,172 @@
+package com.shangpin.iog.picture.service;
+
+import com.shangpin.framework.ServiceException;
+import com.shangpin.iog.common.utils.DowmImage;
+import com.shangpin.iog.common.utils.queue.PicQueue;
+import com.shangpin.iog.service.ProductReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by lizhongren on 2016/8/20.
+ */
+@Service("pictureDownService")
+public class PictureDownService {
+
+    private static org.apache.log4j.Logger loggerInfo = org.apache.log4j.Logger
+            .getLogger("info");
+    private static org.apache.log4j.Logger loggerError = org.apache.log4j.Logger
+            .getLogger("error");
+
+    private static ResourceBundle bdl = null;
+    private static String filepath = null;
+
+    private static String smtpHost = null;
+    private static String from = null;
+    private static String fromUserPassword = null;
+    private static String to = null;
+    private static String subject = null;
+    private static String messageType = null;
+    private static String supplierId = null;
+    private static String startDate = null;
+    private static String endDate = null;
+    private static String downloadPath = null;
+    private static String userName = null;
+    private static String password = null;
+
+    @Autowired
+    ProductReportService productReportService;
+
+    static {
+        if (null == bdl) {
+            bdl = ResourceBundle.getBundle("conf");
+        }
+        smtpHost  = bdl.getString("smtpHost");
+        from = bdl.getString("from");
+        fromUserPassword = bdl.getString("fromUserPassword");
+        to = bdl.getString("to");
+        subject = bdl.getString("subject");
+        messageType = bdl.getString("messageType");
+
+        filepath = bdl.getString("filepath");
+
+        supplierId = bdl.getString("supplierId");
+        startDate = bdl.getString("startDate");
+        endDate = bdl.getString("endDate");
+        downloadPath = bdl.getString("downloadPath");
+        userName = bdl.getString("userName");
+        password = bdl.getString("password");
+
+    }
+    public void downPic(){
+        Map<String,String> picMap = new HashMap<>();
+        Map<String,String> supplierDateMap = null;
+        try {
+            supplierDateMap = productReportService.findPicture(picMap,supplierId,startDate,endDate);
+            if(null!=supplierDateMap&&supplierDateMap.size()>0){
+                //获取日期
+                String key = "",supplierId = "",date= "",spukeyValue = "";
+                ThreadPoolExecutor executor = new ThreadPoolExecutor(3, 15, 300, TimeUnit.MILLISECONDS,new ArrayBlockingQueue<Runnable>(6),new ThreadPoolExecutor.CallerRunsPolicy());
+                PicQueue picQueue = new PicQueue();
+                for(Map.Entry<String,String> supplierDate:supplierDateMap.entrySet()){
+                     key = supplierDate.getKey();
+
+//    	String dirPath = "F:/usr/local/picturetem/"+new Date().getTime();
+                    supplierId  = key.substring(0,key.indexOf("|"));
+                    date = key.substring(key.indexOf("|")+1,key.length());
+                    String dirPath =downloadPath  + supplierId +"/" + date;
+                    File f1 = new File(dirPath);
+                    if (!f1.exists()) {
+                        f1.mkdirs();
+                    }
+                    spukeyValue=supplierDate.getValue();
+                    String[] spuArray = spukeyValue.split("\\|\\|\\|\\|\\|");
+                    for(String spu :spuArray){
+                        int a = 0;
+                        //下载保存图片
+
+
+                            System.out.println("++++"+a+"++++++");
+                            a++;
+                            String[] ingArr = picMap.get(spu).split(",");
+                            int i = 0;
+                            for (String img : ingArr) {
+                                if (org.apache.commons.lang.StringUtils.isNotBlank(img)) {
+                                    try {
+                                        i++;
+                                        File f = new File(dirPath+"/"+spu+" ("+i+").jpg");
+                                        if (f.exists()) {
+                                            continue;
+                                        }
+                                        executor.execute(new DowmImage(img.trim(),spu+" ("+i+").jpg",dirPath,picQueue,null, null,userName,password));
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+
+
+
+
+                    }
+                }
+                delay(executor);
+                //重新下载失败的
+                String failUrl = "";
+                String[] split = null;
+                Map<String,Integer> recordMap = new HashMap<String, Integer>();
+                while(executor.getActiveCount()>0||!picQueue.unVisitedUrlsEmpty()){
+                    if (picQueue.unVisitedUrlsEmpty()&&executor.getActiveCount()>=0) {
+                        System.out.println("============================================都为空=======================================================");
+                        try {
+                            Thread.sleep(1000*15);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        continue;
+                    }
+                    failUrl = picQueue.unVisitEdUrlDeQueue();
+                    if (recordMap.containsKey(failUrl)) {
+                        if (recordMap.get(failUrl)>10) {
+                            continue;
+                        }
+                        recordMap.put(failUrl, recordMap.get(failUrl)+1);
+                    }else{
+                        recordMap.put(failUrl, 1);
+                    }
+                    split = failUrl.split(";");
+                    executor.execute(new DowmImage(split[0],split[2],split[1],picQueue,null, null,userName,password));
+
+                }
+                delay(executor);
+            }
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private void delay(ThreadPoolExecutor executor){
+        while(true){
+            if(executor.getActiveCount()==0){
+                loggerInfo.error("线程活动数为0");
+                break;
+            }
+            try {
+                Thread.sleep(1000*30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ project-common/src/main/java/com/shangpin/iog/common/utils/DowmImage.java
@@ -0,0 +1,140 @@
+package com.shangpin.iog.common.utils;
+
+
+import com.shangpin.framework.ServiceException;
+import com.shangpin.iog.common.utils.httpclient.HttpUtil45;
+import com.shangpin.iog.common.utils.httpclient.OutTimeConfig;
+import com.shangpin.iog.common.utils.queue.PicQueue;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Map;
+
+public class DowmImage extends Thread{
+	private String urlString;
+	private String filename;
+	private String savePath;
+	private String userName;
+	private String password;
+	private PicQueue picQueue;
+
+	private Map<String,String> paraMap ;
+	private Map<String,String> headerMap ;
+	private Logger log = LoggerFactory.getLogger(DowmImage.class) ;
+
+	@Override
+	public void run() {
+		downImage();
+	}
+	  public void downImage(String url,String filepath,String filename){
+	    	log.info("下载"+filepath+"/"+filename);
+	    	// 创建文件对象  
+	        File f = new File(filepath+"/"+filename);  
+	        if (f.exists()) {
+	        	log.error("image has been download");
+	        	return;
+			}
+	    	HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+			CloseableHttpClient httpClient = httpClientBuilder.build();
+			HttpClientContext context = HttpClientContext.create();
+			context.setRequestConfig(RequestConfig.custom()
+					.setConnectionRequestTimeout(1000*60*2)
+					.setConnectTimeout(1000*60*2)
+					.setSocketTimeout(1000*60*2)
+					.build());
+			HttpGet get = new HttpGet(replaceSpecialChar(url));
+			try {
+				CloseableHttpResponse response = httpClient.execute(get,context);
+				if (response.getStatusLine().getStatusCode() == 200) {
+	                byte[] result = EntityUtils.toByteArray(response.getEntity());
+	                BufferedOutputStream bw = null;
+	                try {
+	                    // 创建文件路径
+	                    if (!f.getParentFile().exists())
+	                        f.getParentFile().mkdirs();
+	                    // 写入文件
+	                    bw = new BufferedOutputStream(new FileOutputStream(filepath+"/"+filename));
+	                    bw.write(result);
+	                } catch (Exception e) {
+	                	log.error("保存图片出错");
+	                } finally {
+	                    try {
+	                        if (bw != null)
+	                            bw.close();
+	                    } catch (Exception e) {
+	                    	log.error("关闭出错");
+	                    }
+	                }
+	            }
+			} catch (Exception e) {
+				log.error("下载图片出错保存重新下载"+e.getMessage());
+				String key = url+";"+filepath+";"+filename;
+				picQueue.addUnvisitedUrl(key);
+			}
+	    }
+
+	public void downImage(){
+		log.info("下载"+savePath+"/"+filename);
+		// 创建文件对象
+		File f = new File(savePath+"/"+filename);
+		if (f.exists()) {
+			log.error("image has been download");
+			return;
+		}
+
+		try {
+			HttpUtil45.downloadPicture(urlString,paraMap,headerMap,savePath,filename,new OutTimeConfig(1000*60,1000*60*5,1000*60*5),
+                    userName,password);
+		} catch (ServiceException e) {
+			String key = urlString+";"+savePath+";"+filename;
+			picQueue.addUnvisitedUrl(key);
+		}
+
+	}
+
+
+
+	 private String replaceSpecialChar(String url){
+    	return url.replace(" ", "%20");
+    }
+	public DowmImage(String urlString, String filename, String savePath) {
+		super();
+		this.urlString = urlString;
+		this.filename = filename;
+		this.savePath = savePath;
+	}
+	public DowmImage(String urlString, String filename, String savePath,
+					 PicQueue picQueue) {
+		super();
+		this.urlString = urlString;
+		this.filename = filename;
+		this.savePath = savePath;
+		this.picQueue = picQueue;
+	}
+
+
+	public DowmImage(String urlString, String filename, String savePath,
+					 PicQueue picQueue,Map<String,String> paraMap,Map<String,String> headMap,String userName,String password) {
+		super();
+		this.urlString = urlString;
+		this.filename = filename;
+		this.savePath = savePath;
+		this.picQueue = picQueue;
+		this.paraMap  = paraMap;
+		this.headerMap = headMap;
+		this.userName  =userName;
+		this.password = password;
+	}
+	
+	
+}
