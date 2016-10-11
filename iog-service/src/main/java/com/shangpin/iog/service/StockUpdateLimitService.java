c1fa675aec48c9c504abb61431f2e44f3ae91984
zhaogenchun <1569762716@qq.com>
2016-10-11 17:56:43 +0800

    s

--- iog-service/src/main/java/com/shangpin/iog/service/StockUpdateLimitService.java
+++ iog-service/src/main/java/com/shangpin/iog/service/StockUpdateLimitService.java
@@ -6,7 +6,15 @@ import com.shangpin.iog.dto.StockUpdateLimitDTO;
 public interface StockUpdateLimitService {
 
 	  public void save(StockUpdateLimitDTO hubSku) throws ServiceException;
+	  
+	  
 	  public void updateLimitNum(StockUpdateLimitDTO skuDTO) throws ServiceException;
+	  
+	  /**
+	   * 按当天查询
+	   * @param supplierId
+	   * @return
+	   */
 	  public StockUpdateLimitDTO findBySupplierId(String supplierId);
 
 }
