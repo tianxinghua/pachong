fda3bb95df1ff0aaad609674e84dd964c54f32bc
yanxiaobin <yanxiaobin@shangpin.com>
2016-12-01 11:21:33 +0800

    EPHUB商品中心服务父总工程初始化：将汇聚内置所有的hub商品中心服务

---
+++ ephub-data-mongodb-service/src/main/java/com/shangpin/ephub/data/mongodb/EphubDataMongodbServiceApplication.java
@@ -0,0 +1,12 @@
+package com.shangpin.ephub.data.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EphubDataMongodbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EphubDataMongodbServiceApplication.class, args);
+	}
+}
---
+++ ephub-data-mysql-service/src/main/java/com/shangpin/ephub/data/mysql/EphubDataMysqlServiceApplication.java
@@ -0,0 +1,12 @@
+package com.shangpin.ephub.data.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EphubDataMysqlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EphubDataMysqlServiceApplication.class, args);
+	}
+}
