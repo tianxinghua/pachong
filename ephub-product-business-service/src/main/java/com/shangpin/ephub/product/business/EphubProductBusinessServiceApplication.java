a59639b11ff534532f753e7c13b8b5f6dcde157d
Administrator <Administrator@PC-201611270905>
2016-12-12 10:39:42 +0800

    product initial

---
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/EphubProductBusinessServiceApplication.java
@@ -0,0 +1,12 @@
+package com.shangpin.ephub.product.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EphubProductBusinessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EphubProductBusinessServiceApplication.class, args);
+	}
+}
