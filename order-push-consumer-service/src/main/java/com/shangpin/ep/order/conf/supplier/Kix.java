95fef4ab39a50df6b4efbd1e892dec4bce2e6c71
zhaogenchun <1569762716@qq.com>
2016-12-13 14:53:50 +0800

    kix

---
+++ order-push-consumer-service/src/main/java/com/shangpin/ep/order/conf/supplier/Kix.java
@@ -0,0 +1,23 @@
+package com.shangpin.ep.order.conf.supplier;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+/**
+ * <p>Title:Aladuca.java </p>
+ * <p>Description: Aladuca供货商配置信息</p>
+ * <p>Company: www.shangpin.com</p> 
+ * @author yanxiaobin
+ * @date 2016年11月18日 下午5:24:57
+ */
+@Setter
+@Getter
+@ToString
+public class Kix extends SupplierCommon implements Serializable {
+	
+	private static final long serialVersionUID = 5246775956886237522L;
+	
+
+}
---
+++ order-push-consumer-service/src/main/java/com/shangpin/ep/order/module/orderapiservice/impl/dto/kix/OrderDetail.java
@@ -0,0 +1,12 @@
+package com.shangpin.ep.order.module.orderapiservice.impl.dto.kix;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderDetail {
+	private String variant_id;
+	private String quantity;
+	
+}
