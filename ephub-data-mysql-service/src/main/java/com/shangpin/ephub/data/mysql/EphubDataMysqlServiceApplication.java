0160c0c11af2b54955b147cb9cd0d066d4e5b508
Administrator <Administrator@PC-201611270905>
2016-12-13 10:23:31 +0800

    YOLO

--- ephub-data-mysql-service/src/main/java/com/shangpin/ephub/data/mysql/EphubDataMysqlServiceApplication.java
+++ ephub-data-mysql-service/src/main/java/com/shangpin/ephub/data/mysql/EphubDataMysqlServiceApplication.java
@@ -2,8 +2,10 @@ package com.shangpin.ephub.data.mysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EphubDataMysqlServiceApplication {
 
 	public static void main(String[] args) {
--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/EphubProductBusinessServiceApplication.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/EphubProductBusinessServiceApplication.java
@@ -2,8 +2,10 @@ package com.shangpin.ephub.product.business;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EphubProductBusinessServiceApplication {
 
 	public static void main(String[] args) {
