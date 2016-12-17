787ce5792a86c88d2566befcd4a26d4d271a26dc
zhaogenchun <1569762716@qq.com>
2016-12-17 18:52:58 +0800

    g

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/task/spuimport/service/TaskService.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/task/spuimport/service/TaskService.java
@@ -1,5 +1,11 @@
 package com.shangpin.ephub.product.business.task.spuimport.service;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -10,12 +16,13 @@ import java.util.Map.Entry;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.shangpin.ephub.product.business.task.spuimport.dto.HubSpuImportTaskDto;
-
+import com.sun.mail.iap.ByteArray;
 
 /**
  * <p>
@@ -35,8 +42,18 @@ import com.shangpin.ephub.product.business.task.spuimport.dto.HubSpuImportTaskDt
 public class TaskService {
 
 	public boolean uploadFileAndSave(HubSpuImportTaskDto task) {
-		
-		
+		BufferedOutputStream bos = null;
+		FileOutputStream fos = null;
+		try {
+			File dir = new File(task.getFileName());
+			fos = new FileOutputStream(dir);
+			bos = new BufferedOutputStream(fos);
+			bos.write(task.getUploadfile());
+			bos.flush();
+			bos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return true;
 	}
 
