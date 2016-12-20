5c1852eedd0d5a75d3ee0963b13a80d3279593ba
lizhongren <lizhongren@vansky.cn>
2016-12-20 12:04:07 +0800

    modify package for prodct-business-service

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/pendingPage/controller/PendingProductController.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/pendingPage/controller/PendingProductController.java
@@ -3,16 +3,29 @@ package com.shangpin.ephub.product.business.pendingPage.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.shangpin.ephub.product.business.hubPage.task.spuimport.dto.HubSpuImportTask;
+import com.shangpin.ephub.product.business.pendingPage.dto.SpSkuDTO;
+import com.shangpin.ephub.response.ResponseContent;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/pendingProduct")
+@RequestMapping("/pending-product")
 public class PendingProductController {
 
 	public String pendingList(HttpServletRequest request,HttpServletResponse response,String query){
 		
 		
 		return null;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value = "setspskuno",method = RequestMethod.POST)
+	public ResponseContent countByCriteria(@RequestBody SpSkuDTO dto){
+
+        return null;
+
 	}
 }
