104888a5e5a88f52ba73a8425997a6c0e6d07947
yanxiaobin <yanxiaobin@shangpin.com>
2016-12-21 11:12:41 +0800

    去除警告

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/task/spuimport/dto/HubImportTaskRequestDto.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/task/spuimport/dto/HubImportTaskRequestDto.java
@@ -2,8 +2,6 @@ package com.shangpin.ephub.product.business.ui.task.spuimport.dto;
 
 import java.io.Serializable;
 
-import java.util.Date;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
