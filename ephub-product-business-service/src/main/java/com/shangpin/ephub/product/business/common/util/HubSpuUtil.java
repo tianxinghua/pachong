1ada8e070ae25e0264626eee4e85accee29b2657
lizhongren <lizhongren@vansky.cn>
2016-12-24 14:13:14 +0800

    Merge branch 'master' of 192.168.9.103:yanxiaobin/ephub-product-center-service-parent
    # Conflicts:
    #	ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/pendingCrud/controller/PendingProductController.java

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/common/util/HubSpuUtil.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/common/util/HubSpuUtil.java
@@ -20,7 +20,7 @@ public class HubSpuUtil {
 
     /**
      * 创建SPU编号
-     *
+     * 无法获取时 返回空值
      * @return
      */
     public String createHubSpuNo(Long num){
@@ -46,5 +46,39 @@ public class HubSpuUtil {
 
 
        return result;
+    }
+
+    /**
+     * 或许SPU下的SKU编号
+     * @param spuNo  ：spu编号
+     * @param total  ：需要增加几个值
+
+     * @return
+     */
+    public String createHubSkuNo(String spuNo,int total){
+        Long tmpSpuNo = 0L;
+        String result = "";
+        StringBuffer buffer = new StringBuffer();
+        try {
+            String tmpSku="";
+            for(int i=0;i<total;i++){
+                tmpSpuNo= shangpinRedis.incr(ConstantProperty.EP_HUB_SPU_NO_CREATE_KEY+"_"+ spuNo);
+                tmpSku = "000" + String.valueOf(tmpSpuNo);
+                tmpSku = tmpSku.substring(tmpSku.length()-3,tmpSku.length());
+                buffer.append(tmpSku).append(",");
+            }
+            result = buffer.toString().substring(0,buffer.toString().length()-1);
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+            log.error("redis 出错，无法获取sku编号");
+            return  "";
+            //TODO ADD EMAIL
+        }
+
+        return result;
     }
 }
