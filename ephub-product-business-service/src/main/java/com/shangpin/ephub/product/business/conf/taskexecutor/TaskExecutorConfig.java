391ef2dcd80265469c7dfb252933d6768fa15ed0
lizhongren <lizhongren@vansky.cn>
2016-12-28 14:54:48 +0800

    move unnecessary executor

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/conf/taskexecutor/TaskExecutorConfig.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/conf/taskexecutor/TaskExecutorConfig.java
@@ -12,18 +12,18 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 public class TaskExecutorConfig {
 
-    @Bean
-     public TaskExecutor taskExecutor() {
-
-        ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
-        //线程池所使用的缓冲队列
-         poolTaskExecutor.setQueueCapacity(200);
-         //线程池维护线程的最少数量
-         poolTaskExecutor.setCorePoolSize(5);
-         //线程池维护线程的最大数量
-         poolTaskExecutor.setMaxPoolSize(1000);
-         //线程池维护线程所允许的空闲时间
-         poolTaskExecutor.setKeepAliveSeconds(30000);
-        return poolTaskExecutor;
-    }
+//    @Bean
+//     public TaskExecutor taskExecutor() {
+//
+//        ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
+//        //线程池所使用的缓冲队列
+//         poolTaskExecutor.setQueueCapacity(200);
+//         //线程池维护线程的最少数量
+//         poolTaskExecutor.setCorePoolSize(5);
+//         //线程池维护线程的最大数量
+//         poolTaskExecutor.setMaxPoolSize(1000);
+//         //线程池维护线程所允许的空闲时间
+//         poolTaskExecutor.setKeepAliveSeconds(30000);
+//        return poolTaskExecutor;
+//    }
 }
