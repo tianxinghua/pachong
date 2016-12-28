ae36e520a4b11beed750b06eed93122b97a6227f
lubaijiang <1649675756@qq.com>
2016-12-28 10:19:24 +0800

    添加日志

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/common/service/supplier/SupplierService.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/common/service/supplier/SupplierService.java
@@ -12,6 +12,8 @@ import com.shangpin.commons.redis.IShangpinRedis;
 import com.shangpin.ephub.client.util.JsonUtil;
 import com.shangpin.ephub.product.business.common.dto.SupplierDTO;
 import com.shangpin.ephub.product.business.common.enumeration.GlobalConstant;
+
+import lombok.extern.slf4j.Slf4j;
 /**
  * <p>Title:SupplierService </p>
  * <p>Description: 通过供应商编号获取供应商信息的service</p>
@@ -21,6 +23,7 @@ import com.shangpin.ephub.product.business.common.enumeration.GlobalConstant;
  *
  */
 @Component
+@Slf4j
 public class SupplierService {
 	
 	@Autowired
@@ -29,29 +32,35 @@ public class SupplierService {
 	private RestTemplate httpClient;
 
 	/**
-     * 通过供货商编号查询供货商信息
+     * 通过供货商编号查询供货商信息,失败返回null
      * @param supplierNo
      * @return
      */
     public SupplierDTO getSupplier(String supplierNo) {
-        //先获取缓存中的数据
-        String supplierMsg = shangpinRedis.get(GlobalConstant.REDIS_ORDER_SUPPLIER_KEY+"_"+supplierNo);
-        if(!StringUtils.isEmpty(supplierMsg)){
-        	return JsonUtil.deserialize(supplierMsg, SupplierDTO.class);
-        }else{
-        	//调用接口获取供货商信息
-            Map<String, String> paraMap = new HashMap<>();
-            paraMap.put("supplierNo", supplierNo);
-            String url = "http://scm.shangpin.com/scms/Supplier/GetSupplierInfoListByNo?supplierNo="+supplierNo;
-            SupplierDTO supplierDto = httpClient.getForEntity(url, SupplierDTO.class).getBody();
-            try {
-            	//缓存到redis中
-                shangpinRedis.setex(GlobalConstant.REDIS_ORDER_SUPPLIER_KEY+"_"+supplierNo,1000*60*5,JsonUtil.serialize(supplierDto));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-            return supplierDto;            
-        }
+    	try {
+    		//先获取缓存中的数据
+            String supplierMsg = shangpinRedis.get(GlobalConstant.REDIS_ORDER_SUPPLIER_KEY+"_"+supplierNo);
+            if(!StringUtils.isEmpty(supplierMsg)){
+            	return JsonUtil.deserialize(supplierMsg, SupplierDTO.class);
+            }else{
+            	//调用接口获取供货商信息
+                Map<String, String> paraMap = new HashMap<>();
+                paraMap.put("supplierNo", supplierNo);
+                String url = "http://scm.shangpin.com/scms/Supplier/GetSupplierInfoListByNo?supplierNo="+supplierNo;
+                SupplierDTO supplierDto = httpClient.getForEntity(url, SupplierDTO.class).getBody();
+                try {
+                	//缓存到redis中
+                    shangpinRedis.setex(GlobalConstant.REDIS_ORDER_SUPPLIER_KEY+"_"+supplierNo,1000*60*5,JsonUtil.serialize(supplierDto));
+    			} catch (Exception e) {
+    				log.error(supplierNo+"缓存供应商信息到redis时出错，错误信息为："+e.getMessage(),e); 
+    			}
+                return supplierDto;            
+            }
+		} catch (Exception e) {
+			log.error(supplierNo+"通过supplierNo获取supplierName时出错，错误信息为："+e.getMessage(),e); 
+			return null;
+		}
+        
 
     }
 }
--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/pending/controller/PendingProductController.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/pending/controller/PendingProductController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shangpin.ephub.product.business.ui.pending.dto.Ids;
 import com.shangpin.ephub.product.business.ui.pending.dto.PendingQuryDto;
 import com.shangpin.ephub.product.business.ui.pending.service.IPendingProductService;
 import com.shangpin.ephub.product.business.ui.pending.vo.PendingProductDto;
