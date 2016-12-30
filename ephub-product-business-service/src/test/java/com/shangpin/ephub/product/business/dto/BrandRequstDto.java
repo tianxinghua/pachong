9c66c87aabec8d258cf0dae2847e581501a7399f
yanxiaobin <yanxiaobin@shangpin.com>
2016-12-30 18:18:48 +0800

    YOLO

--- ephub-product-business-service/src/test/java/com/shangpin/ephub/product/business/dto/BrandRequstDto.java
+++ ephub-product-business-service/src/test/java/com/shangpin/ephub/product/business/dto/BrandRequstDto.java
@@ -5,13 +5,31 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-//@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BrandRequstDto {
 
-	//@JsonIgnore
+	@JsonIgnore
 	public String brandNo;  //单个查询
     public List<String> BrandNos;//编号查询
     public String BrandEnName; //品牌英文名称
+	public String getBrandNo() {
+		return brandNo;
+	}
+	public void setBrandNo(String brandNo) {
+		this.brandNo = brandNo;
+	}
+	public List<String> getBrandNos() {
+		return BrandNos;
+	}
+	public void setBrandNos(List<String> brandNos) {
+		BrandNos = brandNos;
+	}
+	public String getBrandEnName() {
+		return BrandEnName;
+	}
+	public void setBrandEnName(String brandEnName) {
+		BrandEnName = brandEnName;
+	}
     
 		
 }
