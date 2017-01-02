91570284f7b6bfb8f3f6d520346cda90af236faa
yanxiaobin <yanxiaobin@shangpin.com>
2017-01-02 16:42:23 +0800

    图片上传服务

---
+++ ephub-fdfs-client-service/src/main/java/com/shangpin/ephub/fdfs/client/service/EphubFdfsClientServiceApplication.java
@@ -0,0 +1,12 @@
+package com.shangpin.ephub.fdfs.client.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EphubFdfsClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EphubFdfsClientServiceApplication.class, args);
+	}
+}
