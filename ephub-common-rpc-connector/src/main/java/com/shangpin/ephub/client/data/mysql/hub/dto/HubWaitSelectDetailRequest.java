25fd8cf036cd210e31e2f64f7f0e94965f48fbc9
zhaogenchun <1569762716@qq.com>
2017-01-04 18:23:45 +0800

    detail

---
+++ ephub-common-rpc-connector/src/main/java/com/shangpin/ephub/client/data/mysql/hub/dto/HubWaitSelectDetailRequest.java
@@ -0,0 +1,20 @@
+package com.shangpin.ephub.client.data.mysql.hub.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class HubWaitSelectDetailRequest {
+	
+	private Long skuSupplierMappingId;
+}
--- ephub-data-mysql-service/src/main/java/com/shangpin/ephub/data/mysql/hub/waitselect/controller/HubWaitSelectController.java
+++ ephub-data-mysql-service/src/main/java/com/shangpin/ephub/data/mysql/hub/waitselect/controller/HubWaitSelectController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shangpin.ephub.data.mysql.hub.waitselect.bean.HubWaitSelectDetailRequest;
 import com.shangpin.ephub.data.mysql.hub.waitselect.bean.HubWaitSelectRequest;
 import com.shangpin.ephub.data.mysql.hub.waitselect.bean.HubWaitSelectRequestWithPage;
 import com.shangpin.ephub.data.mysql.hub.waitselect.po.HubWaitSelectResponse;
@@ -33,6 +34,10 @@ public class HubWaitSelectController {
 	@RequestMapping(value = "/select-with-page")
     public List<HubWaitSelectResponse> selectByCriteriaWithRowbounds(@RequestBody HubWaitSelectRequestWithPage criteriaWithRowBounds){
     	return hubSkuService.selectList(criteriaWithRowBounds);
+    }
+	@RequestMapping(value = "/select-detail")
+    public List<HubWaitSelectResponse> selectDetail(@RequestBody HubWaitSelectDetailRequest criteriaWithRowBounds){
+    	return hubSkuService.selectDetail(criteriaWithRowBounds);
     }
 	
 }
