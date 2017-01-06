8cd2f4bc3952e7fba099bf139f60f65c444fabe8
zhaogenchun <1569762716@qq.com>
2017-01-06 11:24:44 +0800

    rpc

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/hub/waitselected/controller/HubWaitSelectedController.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/hub/waitselected/controller/HubWaitSelectedController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shangpin.ephub.client.data.mysql.enumeration.SupplierSelectState;
 import com.shangpin.ephub.client.data.mysql.hub.dto.HubWaitSelectDetailRequest;
 import com.shangpin.ephub.client.data.mysql.hub.dto.HubWaitSelectRequestDto;
 import com.shangpin.ephub.client.data.mysql.hub.dto.HubWaitSelectRequestWithPageDto;
@@ -56,12 +57,12 @@ public class HubWaitSelectedController {
 			log.info("待选品请求参数：{}",dto);
 			HubWaitSelectRequestDto hubWaitSelectRequest = new HubWaitSelectRequestDto();
 			BeanUtils.copyProperties(dto, hubWaitSelectRequest);
-			hubWaitSelectRequest.setSupplierSelectState((byte)0);
+			hubWaitSelectRequest.setSupplierSelectState((byte)SupplierSelectState.NO_SELECTED.getIndex());
 			Long total = HubWaitSelectGateWay.count(hubWaitSelectRequest);
 			log.info("待选品查询到数据总数："+total);
 			if(total>0){
 				dto.setPageNo(dto.getPageNo()-1);
-				dto.setSupplierSelectState((byte)0);
+				dto.setSupplierSelectState((byte)SupplierSelectState.NO_SELECTED.getIndex());
 				List<HubWaitSelectResponseDto> list = HubWaitSelectGateWay.selectByPage(dto);
 				List<HubWaitSelectedResponse> arr = new ArrayList<HubWaitSelectedResponse>();
 				for(HubWaitSelectResponseDto hubWaitSelectResponseDto:list){
@@ -73,7 +74,6 @@ public class HubWaitSelectedController {
 				HubWaitSelectedResponseWithPage HubWaitSelectedResponseWithPageDto = new HubWaitSelectedResponseWithPage();
 				HubWaitSelectedResponseWithPageDto.setTotal(Integer.parseInt(String.valueOf(total)));
 				HubWaitSelectedResponseWithPageDto.setList(arr);
-				log.info("待选品返回的数据：{}",HubWaitSelectedResponseWithPageDto);
 				return HubResponse.successResp(HubWaitSelectedResponseWithPageDto);
 			}else{
 				return HubResponse.successResp("列表页为空");
