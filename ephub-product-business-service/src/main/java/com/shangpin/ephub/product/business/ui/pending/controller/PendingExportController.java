a35817fca2cb75646913c425c59704e67e6aa979
lubaijiang <1649675756@qq.com>
2017-01-10 12:19:14 +0800

    异步导出

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/pending/controller/PendingExportController.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/pending/controller/PendingExportController.java
@@ -1,7 +1,5 @@
 package com.shangpin.ephub.product.business.ui.pending.controller;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.shangpin.ephub.product.business.ui.pending.dto.PendingQuryDto;
 import com.shangpin.ephub.product.business.ui.pending.service.IPendingProductService;
+import com.shangpin.ephub.response.HubResponse;
 /**
  * <p>Title:PendingExportController </p>
  * <p>Description: 待处理页的两个导出</p>
@@ -26,11 +25,11 @@ public class PendingExportController {
 	private IPendingProductService pendingProductService;
 
 	@RequestMapping(value="/spu",method=RequestMethod.POST)
-	public void exportSpu(@RequestBody PendingQuryDto pendingQuryDto,HttpServletResponse response){
-		pendingProductService.exportSpu(pendingQuryDto);
+	public HubResponse<?> exportSpu(@RequestBody PendingQuryDto pendingQuryDto){
+		return pendingProductService.exportSpu(pendingQuryDto);
 	}
 	@RequestMapping(value="/sku",method=RequestMethod.POST)
-	public void exportSku(@RequestBody PendingQuryDto pendingQuryDto,HttpServletResponse response){
-		pendingProductService.exportSku(pendingQuryDto);
+	public HubResponse<?> exportSku(@RequestBody PendingQuryDto pendingQuryDto){
+		return pendingProductService.exportSku(pendingQuryDto);
 	}
 }
