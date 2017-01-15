44824a7f9e1c32af50dd338ef990c32477d27c29
zhaogenchun <1569762716@qq.com>
2017-01-15 23:11:02 +0800

    a

--- supplier-product-message-service/src/test/java/com/shangpin/supplier/product/message/SupplierProductMessageServiceApplicationTests.java
+++ supplier-product-message-service/src/test/java/com/shangpin/supplier/product/message/SupplierProductMessageServiceApplicationTests.java
@@ -1,15 +1,15 @@
-package com.shangpin.supplier.product.message;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SupplierProductMessageServiceApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-}
+//package com.shangpin.supplier.product.message;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class SupplierProductMessageServiceApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//}
