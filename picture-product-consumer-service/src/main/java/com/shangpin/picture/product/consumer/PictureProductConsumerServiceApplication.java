719bc4a8e40d65396cf0e54fb46abff441840860
yanxiaobin <yanxiaobin@shangpin.com>
2017-01-16 17:35:04 +0800

    删除测试代码

--- picture-product-consumer-service/src/main/java/com/shangpin/picture/product/consumer/PictureProductConsumerServiceApplication.java
+++ picture-product-consumer-service/src/main/java/com/shangpin/picture/product/consumer/PictureProductConsumerServiceApplication.java
@@ -1,14 +1,9 @@
 package com.shangpin.picture.product.consumer;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-
-import com.shangpin.ephub.client.data.mysql.spu.gateway.HubSpuPendingGateWay;
 /**
  * <p>Title:PictureProductConsumerServiceApplication.java </p>
  * <p>Description: 图片处理消费者</p>
@@ -19,27 +14,9 @@ import com.shangpin.ephub.client.data.mysql.spu.gateway.HubSpuPendingGateWay;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients("com.shangpin.ephub")
-public class PictureProductConsumerServiceApplication implements ApplicationRunner {
+public class PictureProductConsumerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PictureProductConsumerServiceApplication.class, args);
-	}
-
-	@Autowired
-	private HubSpuPendingGateWay gateWay;
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		//InputStream resourceAsStream = this.getClass().getResourceAsStream("/banner/gtl.jpg");
-//		URL url = new URL("http://www.tonyboutique.com/media/catalog/product/b/e/be08955185_960-1_1.jpg");
-//		URLConnection connection = url.openConnection();
-//		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0");
-//		InputStream stream = connection.getInputStream();
-//		String base64 = new BASE64Encoder().encode(IOUtils.toByteArray(stream));
-//		stream.close();
-//		UploadPicDto file = new UploadPicDto();
-//		file.setBase64(base64);
-//		file.setExtension("jpg");
-//		String upload = uploadPicGateway.upload(file);
-//		System.out.println(upload);
 	}
 }
