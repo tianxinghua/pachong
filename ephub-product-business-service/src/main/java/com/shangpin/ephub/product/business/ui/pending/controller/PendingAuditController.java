0c753c2112dccb5e2c706666f711fe2d2410f2a4
lizhongren <lizhongren@vansky.cn>
2017-02-10 15:48:29 +0800

    tmp modify audit data search

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/pending/controller/PendingAuditController.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/pending/controller/PendingAuditController.java
@@ -2,6 +2,7 @@ package com.shangpin.ephub.product.business.ui.pending.controller;
 
 import java.util.List;
 
+import com.shangpin.ephub.product.business.common.enumeration.SpuStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
