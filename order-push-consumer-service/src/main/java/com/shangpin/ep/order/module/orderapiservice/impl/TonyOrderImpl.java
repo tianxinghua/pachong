e62d908b41936f4d0fc3c151bbeee2bcacd68143
zhaogenchun <1569762716@qq.com>
2017-02-16 11:06:50 +0800

    tony订单

--- order-push-consumer-service/src/main/java/com/shangpin/ep/order/module/orderapiservice/impl/TonyOrderImpl.java
+++ order-push-consumer-service/src/main/java/com/shangpin/ep/order/module/orderapiservice/impl/TonyOrderImpl.java
@@ -276,7 +276,9 @@ public class TonyOrderImpl implements IOrderService {
 	            	}else if(message.contains("quantity not available in the stock")){
 	            		orderDTO.setDescription(orderDTO.getLogContent());
 	                	orderDTO.setPushStatus(PushStatus.NO_STOCK);
-	            		
+	            	}else if(message.contains("Shop order id - Shop order id - Order already exists")){
+	            		orderDTO.setLockStockTime(new Date());
+		            	orderDTO.setPushStatus(PushStatus.LOCK_PLACED);
 	            	}else{
 	            		orderDTO.setErrorType(ErrorStatus.API_ERROR);
 	            		orderDTO.setDescription(orderDTO.getLogContent());
