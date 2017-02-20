24a0195bf1e5e5f400a276e63a1429809bdb82af
lubaijiang <1649675756@qq.com>
2017-02-20 16:12:49 +0800

    piccadilly

--- supplier-product-consumer-service/src/main/java/com/shangpin/supplier/product/consumer/supplier/adapter/OriginalProductStreamListenerAdapter.java
+++ supplier-product-consumer-service/src/main/java/com/shangpin/supplier/product/consumer/supplier/adapter/OriginalProductStreamListenerAdapter.java
@@ -72,6 +72,9 @@ public class OriginalProductStreamListenerAdapter {
 	@Autowired
 	@Qualifier("pritelliHandler")
 	private ISupplierHandler pritelliHandler;
+	@Autowired
+	@Qualifier("piccadillyHandler")
+	private ISupplierHandler piccadillyHandler;
 	
 	/**
 	 * 此队列重名，未使用
@@ -416,7 +419,7 @@ public class OriginalProductStreamListenerAdapter {
 	 * @param headers 消息头
 	 */
 	public void piccadillyStreamListen(SupplierProduct message, Map<String, Object> headers) {
-		// TODO Auto-generated method stub
+		piccadillyHandler.handleOriginalProduct(message, headers);
 		
 	}
 	
