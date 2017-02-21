133e7df1dcb2f5182b70bd8accb991c87a72bb33
lubaijiang <1649675756@qq.com>
2017-02-21 17:27:24 +0800

    dto

--- ephub-common-rpc-connector/src/main/java/com/shangpin/ephub/client/product/business/hubpending/spu/result/PendingProducts.java
+++ ephub-common-rpc-connector/src/main/java/com/shangpin/ephub/client/product/business/hubpending/spu/result/PendingProducts.java
@@ -2,8 +2,6 @@ package com.shangpin.ephub.client.product.business.hubpending.spu.result;
 
 import java.util.List;
 
-import com.shangpin.ephub.client.data.mysql.sku.dto.HubSkuPendingDto;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,12 +12,4 @@ public class PendingProducts {
 	private List<PendingProductDto> produts;
 	private int total;
 	private String createUser;
-	private String supplierName;
-	private String hubBrandName;
-	private String hubCategoryName;
-	private List<HubSkuPendingDto> hubSkus;
-	private String spPicUrl;
-	private List<String> picUrls;
-	
-	private String supplierCategoryname;
 }
