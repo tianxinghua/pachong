4ec088847450d295e0ee996478c49a497362dfea
lubaijiang <1649675756@qq.com>
2017-03-09 11:55:54 +0800

    日志

--- supplier-product-consumer-service/src/main/java/com/shangpin/supplier/product/consumer/supplier/giglio/GiglioHandler.java
+++ supplier-product-consumer-service/src/main/java/com/shangpin/supplier/product/consumer/supplier/giglio/GiglioHandler.java
@@ -18,7 +18,6 @@ import com.shangpin.ephub.client.message.original.body.SupplierProduct;
 import com.shangpin.ephub.client.message.picture.body.SupplierPicture;
 import com.shangpin.ephub.client.message.picture.image.Image;
 import com.shangpin.ephub.client.util.JsonUtil;
-import com.shangpin.supplier.product.consumer.exception.EpHubSupplierProductConsumerException;
 import com.shangpin.supplier.product.consumer.service.SupplierProductSaveAndSendToPending;
 import com.shangpin.supplier.product.consumer.supplier.ISupplierHandler;
 import com.shangpin.supplier.product.consumer.supplier.common.enumeration.Isexistpic;
@@ -60,7 +59,7 @@ public class GiglioHandler implements ISupplierHandler {
 					supplierProductSaveAndSendToPending.saveAndSendToPending(message.getSupplierNo(), message.getSupplierId(), message.getSupplierName(), hubSpu, hubSkus, supplierPicture);
 				}
 			}
-		} catch (EpHubSupplierProductConsumerException e) {
+		} catch (Exception e) {
 			log.error("giglio异常："+e.getMessage(),e); 
 		}
 		
