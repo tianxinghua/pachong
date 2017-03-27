2baa693ad904bb9583a6fd0840593247028b2485
Ben <1718818678@qq.com>
2017-03-27 15:35:11 +0800

    增加删除图片接口并注册服务供消费者调用

---
+++ ephub-fdfs-client-service/src/main/java/com/shangpin/ephub/fdfs/client/service/delete/service/DeletePicService.java
@@ -0,0 +1,37 @@
+package com.shangpin.ephub.fdfs.client.service.delete.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shangpin.ephub.fdfs.client.service.upload.manager.FastDFSClientManager;
+
+/**
+ * <p>Title:UploadPicService.java </p>
+ * <p>Description: </p>
+ * <p>Company: www.shangpin.com</p> 
+ * @author yanxiaobin
+ * @date 2017年1月2日 下午5:34:23
+ */
+@Service
+public class DeletePicService {
+	
+	@Autowired
+	private FastDFSClientManager dfsClientManager;
+	/**
+	 * 上传图片
+	 * @param buffer
+	 * @param extension
+	 * @return
+	 */
+	public String uploadFile(byte[] buffer, String extension) {
+		return dfsClientManager.uploadFile(buffer,extension);
+	}
+	/**
+	 * 删除图片
+	 * @param url 图片地址
+	 */
+	public void deleteByUrl(String url) {
+		dfsClientManager.deleteFile(url);
+	}
+
+}
