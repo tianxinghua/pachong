8052b6d2139258f7c6cfb692d0eed81046f9fb75
lizhongren <lizhongren@vansky.cn>
2017-03-30 10:12:05 +0800

    add supplier message

---
+++ price-push-consumer-service/src/main/java/com/shangpin/ephub/price/consumer/conf/supplier/Daniello.java
@@ -0,0 +1,60 @@
+package com.shangpin.ephub.price.consumer.conf.supplier;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * <p>Title:Daniello.java </p>
+ * <p>Description: </p>
+ * <p>Company: www.shangpin.com</p> 
+ * @author yanxiaobin
+ * @date 2016年11月18日 下午6:29:42
+ */
+@Setter
+@Getter
+@ToString
+public class Daniello extends SupplierCommon implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3814118581934543929L;
+	/**
+	 * 
+	 */
+	private String url;
+	/**
+	 * 
+	 */
+	private String createOrderInterface;
+	/**
+	 * 
+	 */
+	private String setStatusInterface;
+	/**
+	 * 
+	 */
+	private String getStatusInterface;
+	/**
+	 * 
+	 */
+	private String getItemStockInterface;
+	/**
+	 * 
+	 */
+	private String user;
+	/**
+	 * 
+	 */
+	private String password;
+	/**
+	 * 
+	 */
+	private String messageType;
+	
+
+
+}
---
+++ price-push-consumer-service/src/main/java/com/shangpin/ephub/price/consumer/conf/supplier/Stefania.java
@@ -0,0 +1,39 @@
+package com.shangpin.ephub.price.consumer.conf.supplier;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * <p>Title:Stefania.java </p>
+ * <p>Description: </p>
+ * <p>Company: www.shangpin.com</p> 
+ * @author yanxiaobin
+ * @date 2016年11月18日 下午8:21:38
+ */
+@Setter
+@Getter
+@ToString
+public class Stefania extends SupplierCommon implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4168933008467718747L;
+	/**
+	 * 
+	 */
+	private String authKey;
+	/**
+	 * 
+	 */
+	private String channel;
+	/**
+	 * 
+	 */
+	private String jssecacerts;
+	
+
+}
