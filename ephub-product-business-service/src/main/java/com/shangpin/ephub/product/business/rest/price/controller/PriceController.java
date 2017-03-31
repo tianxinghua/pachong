1be8ec1407ee6fdcec1d645fece50825015fb9cf
lubaijiang <1649675756@qq.com>
2017-03-31 10:45:29 +0800

    提交代码

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/rest/price/controller/PriceController.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/rest/price/controller/PriceController.java
@@ -1,17 +1,20 @@
 package com.shangpin.ephub.product.business.rest.price.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shangpin.ephub.client.data.mysql.sku.dto.HubSupplierSkuDto;
+import com.shangpin.ephub.client.product.business.price.dto.PriceDto;
+import com.shangpin.ephub.product.business.rest.price.service.PriceService;
 
 @RestController
 @RequestMapping("price")
-
 public class PriceController {
+	
+	private PriceService priceService;
 
 	@RequestMapping(value = "/save")
-	public void savePriceRecordAndSendConsumer(HubSupplierSkuDto hubSupplierSkuDto) throws Exception{
-		
+	public void savePriceRecordAndSendConsumer(@RequestBody PriceDto priceDto) throws Exception{
+		priceService.savePriceRecordAndSendConsumer(priceDto);
 	}
 }
