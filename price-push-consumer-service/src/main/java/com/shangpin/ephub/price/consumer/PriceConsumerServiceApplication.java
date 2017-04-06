dcdbd8e7386fa28d40c1015f11055cd392a8092a
Ben <1718818678@qq.com>
2017-04-06 19:02:46 +0800

    YOLO

--- price-push-consumer-service/src/main/java/com/shangpin/ephub/price/consumer/PriceConsumerServiceApplication.java
+++ price-push-consumer-service/src/main/java/com/shangpin/ephub/price/consumer/PriceConsumerServiceApplication.java
@@ -1,6 +1,8 @@
 package com.shangpin.ephub.price.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 * <p>Description: 价格处理消费者</p>
 
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class,RedisRepositoriesAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients("com.shangpin.ephub")
 @ComponentScan(basePackages={"com.shangpin"})
