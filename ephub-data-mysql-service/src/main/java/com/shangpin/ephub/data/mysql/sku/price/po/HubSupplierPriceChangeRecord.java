1efa3225e911d3f98ffe92859796baba7666f0c8
lizhongren <lizhongren@vansky.cn>
2017-04-06 11:42:58 +0800

    The table of  price-record add column

--- ephub-data-mysql-service/src/main/java/com/shangpin/ephub/data/mysql/sku/price/po/HubSupplierPriceChangeRecord.java
+++ ephub-data-mysql-service/src/main/java/com/shangpin/ephub/data/mysql/sku/price/po/HubSupplierPriceChangeRecord.java
@@ -23,6 +23,8 @@ public class HubSupplierPriceChangeRecord implements Serializable {
 
     private String currency;
 
+    private String supplierSeason;
+
     private String marketYear;
 
     private String marketSeason;
@@ -33,9 +35,11 @@ public class HubSupplierPriceChangeRecord implements Serializable {
     private Byte type;
 
     /**
-     * 0:未处理  1、推送消息队列完成   2：处理完成 3：处理失败
+     * 0:未处理 1、推送消息队列完成 2：消息推送失败 3：处理完成 4：处理失败
      */
     private Byte state;
+
+    private String createUserName;
 
     private Date createTime;
 
@@ -117,6 +121,14 @@ public class HubSupplierPriceChangeRecord implements Serializable {
         this.currency = currency == null ? null : currency.trim();
     }
 
+    public String getSupplierSeason() {
+        return supplierSeason;
+    }
+
+    public void setSupplierSeason(String supplierSeason) {
+        this.supplierSeason = supplierSeason == null ? null : supplierSeason.trim();
+    }
+
     public String getMarketYear() {
         return marketYear;
     }
@@ -147,6 +159,14 @@ public class HubSupplierPriceChangeRecord implements Serializable {
 
     public void setState(Byte state) {
         this.state = state;
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName == null ? null : createUserName.trim();
     }
 
     public Date getCreateTime() {
@@ -188,10 +208,12 @@ public class HubSupplierPriceChangeRecord implements Serializable {
         sb.append(", marketPrice=").append(marketPrice);
         sb.append(", supplyPrice=").append(supplyPrice);
         sb.append(", currency=").append(currency);
+        sb.append(", supplierSeason=").append(supplierSeason);
         sb.append(", marketYear=").append(marketYear);
         sb.append(", marketSeason=").append(marketSeason);
         sb.append(", type=").append(type);
         sb.append(", state=").append(state);
+        sb.append(", createUserName=").append(createUserName);
         sb.append(", createTime=").append(createTime);
         sb.append(", updateTime=").append(updateTime);
         sb.append(", memo=").append(memo);
@@ -221,10 +243,12 @@ public class HubSupplierPriceChangeRecord implements Serializable {
             && (this.getMarketPrice() == null ? other.getMarketPrice() == null : this.getMarketPrice().equals(other.getMarketPrice()))
             && (this.getSupplyPrice() == null ? other.getSupplyPrice() == null : this.getSupplyPrice().equals(other.getSupplyPrice()))
             && (this.getCurrency() == null ? other.getCurrency() == null : this.getCurrency().equals(other.getCurrency()))
+            && (this.getSupplierSeason() == null ? other.getSupplierSeason() == null : this.getSupplierSeason().equals(other.getSupplierSeason()))
             && (this.getMarketYear() == null ? other.getMarketYear() == null : this.getMarketYear().equals(other.getMarketYear()))
             && (this.getMarketSeason() == null ? other.getMarketSeason() == null : this.getMarketSeason().equals(other.getMarketSeason()))
             && (this.getType() == null ? other.getType() == null : this.getType().equals(other.getType()))
             && (this.getState() == null ? other.getState() == null : this.getState().equals(other.getState()))
+            && (this.getCreateUserName() == null ? other.getCreateUserName() == null : this.getCreateUserName().equals(other.getCreateUserName()))
             && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
             && (this.getUpdateTime() == null ? other.getUpdateTime() == null : this.getUpdateTime().equals(other.getUpdateTime()))
             && (this.getMemo() == null ? other.getMemo() == null : this.getMemo().equals(other.getMemo()));
@@ -243,10 +267,12 @@ public class HubSupplierPriceChangeRecord implements Serializable {
         result = prime * result + ((getMarketPrice() == null) ? 0 : getMarketPrice().hashCode());
         result = prime * result + ((getSupplyPrice() == null) ? 0 : getSupplyPrice().hashCode());
         result = prime * result + ((getCurrency() == null) ? 0 : getCurrency().hashCode());
+        result = prime * result + ((getSupplierSeason() == null) ? 0 : getSupplierSeason().hashCode());
         result = prime * result + ((getMarketYear() == null) ? 0 : getMarketYear().hashCode());
         result = prime * result + ((getMarketSeason() == null) ? 0 : getMarketSeason().hashCode());
         result = prime * result + ((getType() == null) ? 0 : getType().hashCode());
         result = prime * result + ((getState() == null) ? 0 : getState().hashCode());
+        result = prime * result + ((getCreateUserName() == null) ? 0 : getCreateUserName().hashCode());
         result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
         result = prime * result + ((getUpdateTime() == null) ? 0 : getUpdateTime().hashCode());
         result = prime * result + ((getMemo() == null) ? 0 : getMemo().hashCode());
