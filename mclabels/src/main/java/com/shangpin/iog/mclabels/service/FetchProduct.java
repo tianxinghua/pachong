f4ea834d642837643b522ad589f28224e576b918
zhaogenchun <1569762716@qq.com>
2017-04-10 17:14:46 +0800

    thecluther

--- mclabels/src/main/java/com/shangpin/iog/mclabels/service/FetchProduct.java
+++ mclabels/src/main/java/com/shangpin/iog/mclabels/service/FetchProduct.java
@@ -4,18 +4,10 @@ package com.shangpin.iog.mclabels.service;
  * Created by wang on 2015/9/21.
  */
 
-import java.lang.reflect.Type;
-import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.ResourceBundle;
-
-import net.sf.json.JSONObject;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +15,6 @@ import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.mongodb.util.JSON;
-import com.shangpin.framework.ServiceException;
 import com.shangpin.iog.common.utils.UUIDGenerator;
 import com.shangpin.iog.common.utils.httpclient.HttpUtil45;
 import com.shangpin.iog.common.utils.httpclient.OutTimeConfig;
@@ -42,7 +32,7 @@ import com.shangpin.product.AbsSaveProduct;
 /**
  * Created by 赵根春 on 2015/9/25.
  */
-@Component("efashion")
+@Component("mclabels")
 public class FetchProduct extends AbsSaveProduct{
 
 	@Autowired
@@ -60,104 +50,104 @@ public class FetchProduct extends AbsSaveProduct{
 		gson = new Gson();
 	}
 	static int i=0;
-	private List<Item> allItemList = new ArrayList<Item>();
 	public void save(){
 		handleData("spu", supplierId, day, null);
 	}
 	public  List<Item>   getProductList(){
 		String url = "http://nodo.azurewebsites.net/api/channel/export/?channelName=Shangpin&token=bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNlcnZpY2VzQHNoYW5ncGluLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JEdrcDZENmdYVkQxVmtOYWdGandrak9yRGRQekcvdjBuN3ZTUzFScEhKYnNyNHZXY0g2OTNPIiwiaWQiOjEzLCJpYXQiOjE0ODA2OTgxMTF9.7kRekC3N7xe0TjuFGbbpK93Kv4ry7eyrS897qmPvBIc";
 		String json = HttpUtil45
-				.get(url,new OutTimeConfig(1000 * 60 *2, 1000 * 60*2, 1000 * 60*2),null);
+				.get(url,new OutTimeConfig(1000 * 60 *20, 1000 * 60*20, 1000 * 60*20),null);
 		List<Item> obj = null;
 		try{
 			obj = gson.fromJson(json,new TypeToken<List<Item>>(){}.getType());
-			if(obj!=null){
-				fetchProductAndSave();
-			}
 		}catch(Exception e){
+			System.out.println(e.getMessage());
 			i++;
+			if(i!=5){
+				getProductList();
+			}
+
 		}
 		return obj;
 	}
 	public  Map<String, Object> fetchProductAndSave() {
-
-		logger.info("总的商品数量：" + allItemList.size());
 		Map<String, Object> returnMap = new HashMap<String, Object>();
 		List<SkuDTO> skuList = new ArrayList<SkuDTO>();
 		List<SpuDTO> spuList = new ArrayList<SpuDTO>();
 		Map<String, List<String>> imageMap = new HashMap<String, List<String>>();
 		List<Item> list = getProductList();
-		for (Item item : list) {
-			if("true".equals(item.getVariationInfo().getIsParent())){
-				SpuDTO spu = new SpuDTO();
-				spu.setId(UUIDGenerator.getUUID());
-				spu.setSupplierId(supplierId);
-				spu.setSpuId(item.getSku());
-				spu.setBrandName(item.getBrand());
-				List<AttributeInfo> attributeInfolist = item.getAttributeList().getAttributeInfo();
-				String season = null;
-				String gender = null;
-				String material = null;
-				for(AttributeInfo attr:attributeInfolist){
-					if("season".equals(attr.getName())){
-						season = attr.getValue();
+		logger.info("总的商品数量：" + list.size());
+		if(list!=null&&list.size()>0){
+			for (Item item : list) {
+				if("true".equals(item.getVariationInfo().getIsParent())){
+					SpuDTO spu = new SpuDTO();
+					spu.setId(UUIDGenerator.getUUID());
+					spu.setSupplierId(supplierId);
+					spu.setSpuId(item.getSku());
+					spu.setBrandName(item.getBrand());
+					List<AttributeInfo> attributeInfolist = item.getAttributeList().getAttributeInfo();
+					String season = null;
+					String gender = null;
+					String material = null;
+					for(AttributeInfo attr:attributeInfolist){
+						if("season".equals(attr.getName())){
+							season = attr.getValue();
+						}
+						if("Gender".equals(attr.getName())){
+							gender = attr.getValue();
+						}
+						if("Materials_node".equals(attr.getName())){
+							material = attr.getValue();
+						}
 					}
-					if("Gender".equals(attr.getName())){
-						gender = attr.getValue();
+					spu.setSpuName(item.getTitle());
+					spu.setCategoryName(item.getClassification());
+					spu.setMaterial(material);
+					spu.setSeasonName(season);
+					spu.setCategoryGender(gender);
+					spuList.add(spu);
+					List<ImageInfoSubmit> listImages = item.getImageList().getImageInfoSubmit();
+					List<String> listImag = new ArrayList<String>();
+					if(listImages!=null){
+						for(ImageInfoSubmit imageInfoSubmit:listImages){
+							listImag.add(imageInfoSubmit.getFilenameOrUrl());
+						}
 					}
-					if("Materials_node".equals(attr.getName())){
-						material = attr.getValue();
+					imageMap.put(spu.getSpuId()+";"+spu.getSpuId(),listImag);
+				}else{
+					SkuDTO sku = new SkuDTO();
+					sku.setId(UUIDGenerator.getUUID());
+					sku.setSupplierId(supplierId);
+					sku.setSpuId(item.getVariationInfo().getParentSku());
+					sku.setSkuId(item.getSku());
+					sku.setBarcode(item.getEAN());
+					List<AttributeInfo> attributeInfolist = item.getAttributeList().getAttributeInfo();
+					String size = null;
+					String color = null;
+					for(AttributeInfo attr:attributeInfolist){
+						if("size".equals(attr.getName())){
+							size = attr.getValue();
+						}
+						if("Color".equals(attr.getName())){
+							color = attr.getValue();
+						}
 					}
-				}
-				spu.setSpuName(item.getTitle());
-				spu.setCategoryName(item.getClassification());
-				spu.setMaterial(material);
-//				spu.setProductOrigin(item.getMade_in());
-				spu.setSeasonName(season);
-				spu.setCategoryGender(gender);
-				spuList.add(spu);
-				List<ImageInfoSubmit> listImages = item.getImageList().getImageInfoSubmit();
-				List<String> listImag = new ArrayList<String>();
-				if(listImages!=null){
-					for(ImageInfoSubmit imageInfoSubmit:listImages){
-						listImag.add(imageInfoSubmit.getFilenameOrUrl());
-					}
-				}
-				imageMap.put(spu.getSpuId()+";"+spu.getSpuId(),listImag);
-			}else{
-				SkuDTO sku = new SkuDTO();
-				sku.setId(UUIDGenerator.getUUID());
-				sku.setSupplierId(supplierId);
-				sku.setSpuId(item.getVariationInfo().getParentSku());
-				sku.setSkuId(item.getSku());
-				sku.setBarcode(item.getEAN());
-				List<AttributeInfo> attributeInfolist = item.getAttributeList().getAttributeInfo();
-				String size = null;
-				String color = null;
-				for(AttributeInfo attr:attributeInfolist){
-					if("size".equals(attr.getName())){
-						size = attr.getValue();
-					}
-					if("Color".equals(attr.getName())){
-						color = attr.getValue();
-					}
-				
-				}
-				DistributionCenterInfoSubmit distributionCenterInfoSubmit = item.getDistributionCenterList().getDistributionCenterInfoSubmit();
-				sku.setProductSize(size);
-				sku.setStock(distributionCenterInfoSubmit.getQuantity());
-				sku.setProductCode(item.getVariationInfo().getParentSku());
-				sku.setMarketPrice(item.getPriceInfo().getRetailPrice());
-				sku.setColor(color);
-				sku.setProductName(item.getTitle());
-				sku.setProductDescription(item.getDescription());
-//				sku.setSaleCurrency(item.getCurrency());
-//				sku.setSalePrice("");
-//				sku.setSupplierPrice(item.getPrice());
-				skuList.add(sku);
+					DistributionCenterInfoSubmit distributionCenterInfoSubmit = item.getDistributionCenterList().getDistributionCenterInfoSubmit();
+					sku.setProductSize(size);
+					sku.setStock(distributionCenterInfoSubmit.getQuantity());
+					sku.setProductCode(item.getVariationInfo().getParentSku());
+					System.out.println(item.getPriceInfo().getChannelPrice());
+					sku.setMarketPrice(item.getPriceInfo().getRetailPrice());
+					sku.setSalePrice(item.getPriceInfo().getChannelPrice());
+					sku.setColor(color);
+					sku.setProductName(item.getTitle());
+					sku.setProductDescription(item.getDescription());
+					skuList.add(sku);
 
+				}
 			}
 		}
+		
 		returnMap.put("sku", skuList);
 		returnMap.put("spu", spuList);
 		returnMap.put("image", imageMap);
---
+++ theclutcher-stock/src/main/java/com/shangpin/iog/theclutcher/utils/Test.java
@@ -0,0 +1,102 @@
+package com.shangpin.iog.theclutcher.utils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TimeZone;
+
+import com.shangpin.iog.common.utils.httpclient.HttpUtil45;
+import com.shangpin.iog.common.utils.httpclient.OutTimeConfig;
+
+public class Test
+{
+	
+//	public static void main(String[] args) {
+//		readLine(getJson());
+//	}
+
+  public static String getJson() {
+    String app_secret = "hM&L1dqGA5YGjGK%fU8*715D$g&z$B";
+    Map<String,String> params = new LinkedHashMap<String,String>();
+    String app_key = "SHNGPN-001";
+    params.put("app_key", app_key);
+    params.put("request", "");
+    String time = new StringBuilder().append(getUTCTime().getTime()).append("").toString();
+    time = time.substring(0, 10);
+    params.put("time_stamp", time);
+    String charset = "UTF-8";
+
+    String json = null;
+    try {
+      String md5_sign = new StringBuilder().append("app_key=").append(ToolsUtils.urlEncode(app_key, charset)).append((params.containsKey("request")) ? new StringBuilder().append("&request=").append(ToolsUtils.urlEncode((String)params.get("request"), charset)).toString() : "").append("&time_stamp=").append(ToolsUtils.urlEncode((String)params.get("time_stamp"), charset)).append("_").append(app_secret).toString();
+      String md5_result = MD5.encrypt32(md5_sign);
+      params.put("sign", md5_result);
+      Map<String,String> headerMap = new LinkedHashMap<String,String>();
+      headerMap.put("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+      json = HttpUtil45.operateData("get", null, "http://ws.theclutcher.com/shangpin.svc/stock", new OutTimeConfig(1000*60*20, 1000*60*20,1000*60*20), params, null, headerMap, null, null);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      e.printStackTrace();
+    }
+    return json;
+  }
+
+  private static Date getUTCTime() {
+    Calendar cal = Calendar.getInstance();
+    cal.setTimeZone(TimeZone.getTimeZone("GMT"));
+    System.out.println(cal.getTime());
+
+    int zoneOffset = cal.get(15);
+
+    int dstOffset = cal.get(16);
+
+    cal.add(14, -(zoneOffset + dstOffset));
+    return cal.getTime();
+  }
+
+  public static long GetTicks()
+  {
+    SimpleDateFormat s = new SimpleDateFormat("yyyy/MM/dd/HH/mm/ss");
+    String date = s.format(getUTCTime());
+    String[] ds = date.split("/");
+
+    Calendar calStart = Calendar.getInstance();
+    calStart.set(1, 1, 3, 0, 0, 0);
+
+    Calendar calEnd = Calendar.getInstance();
+    calEnd.set(Integer.parseInt(ds[0]), Integer.parseInt(ds[1]), Integer.parseInt(ds[2]), Integer.parseInt(ds[3]), Integer.parseInt(ds[4]), Integer.parseInt(ds[5]));
+
+    long epochStart = calStart.getTime().getTime();
+
+    long epochEnd = calEnd.getTime().getTime();
+
+    long all = epochEnd - epochStart;
+
+    long ticks = all / 1000L * 1000000L * 10L;
+
+    return ticks;
+  }
+
+  private static void readLine(String content)
+  {
+    File file = new File("C://clutcher222.json");
+    FileWriter fwriter = null;
+    try {
+      fwriter = new FileWriter(file);
+      fwriter.write(content);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    } finally {
+      try {
+        fwriter.flush();
+        fwriter.close();
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+  }
+}
