db83ff347db1f2af47dabbcfc7f4ce9c5873573c
lizhongren <lizhongren@vansky.cn>
2017-04-21 11:37:10 +0800

    add materail judge

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/service/pending/impl/PendingServiceImpl.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/service/pending/impl/PendingServiceImpl.java
@@ -448,9 +448,11 @@ public class PendingServiceImpl implements com.shangpin.ephub.product.business.s
         if(StringUtils.isBlank(auditVO.getMaterial())){
             result = result + "材质不能为空 ";
         }else {
-            if(!RegexUtil.excludeLetter(auditVO.getMaterial())){
-                result = result + "材质中包含不能包含英文 ";
+            result=RegexUtil.specialCategoryMatch(auditVO.getCategoryNo(),auditVO.getMaterial());
+            if(null==result){
+                result = "";
             }
+
         }
         if(StringUtils.isBlank(auditVO.getOrigin())){
             result = result + "产地不能为空";
