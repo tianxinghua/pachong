d332ae8b54c4f951c574ec851baa5378dd73333d
lubaijiang <1649675756@qq.com>
2017-05-24 10:21:48 +0800

    环境

--- ephub-data-schedule-service/src/main/java/com/shangpin/ephub/data/schedule/service/PricePushDataService.java
+++ ephub-data-schedule-service/src/main/java/com/shangpin/ephub/data/schedule/service/PricePushDataService.java
@@ -1,22 +1,28 @@
 package com.shangpin.ephub.data.schedule.service;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.shangpin.ephub.client.data.mysql.enumeration.PriceHandleState;
 import com.shangpin.ephub.client.data.mysql.sku.dto.HubSupplierPriceChangeRecordCriteriaDto;
 import com.shangpin.ephub.client.data.mysql.sku.dto.HubSupplierPriceChangeRecordDto;
 import com.shangpin.ephub.client.data.mysql.sku.gateway.HubSupplierPriceChangeRecordGateWay;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by lizhongren on 2017/5/23.
  */
 @Component
 public class PricePushDataService {
+	
+	private static String pattern = "yyyy-MM-dd HH:mm:ss";
+	
     @Autowired
     HubSupplierPriceChangeRecordGateWay priceChangeRecordGateWay;
 
@@ -24,10 +30,12 @@ public class PricePushDataService {
      * 如果多个相同的供货商下的同一个商品 取最新的 老的不再处理
      * @return
      */
-    public List<HubSupplierPriceChangeRecordDto>  findPushMqErrorRecordList(){
+    public List<HubSupplierPriceChangeRecordDto>  findPushMqErrorRecordList() throws Exception{
+		String source = "2017-05-20 00:00:00";
+		Date start = new SimpleDateFormat(pattern).parse(source);
         HubSupplierPriceChangeRecordCriteriaDto criteriaDto = new HubSupplierPriceChangeRecordCriteriaDto();
         criteriaDto.setOrderByClause(" create_time desc ");
-        criteriaDto.createCriteria().andStateEqualTo(PriceHandleState.PUSHED_ERROR.getIndex()).andMarketSeasonIsNotNull();
+        criteriaDto.createCriteria().andStateEqualTo(PriceHandleState.PUSHED_ERROR.getIndex()).andMarketSeasonIsNotNull().andCreateTimeGreaterThanOrEqualTo(start); 
         List<HubSupplierPriceChangeRecordDto> hubSupplierPriceChangeRecordDtos = priceChangeRecordGateWay.selectByCriteria(criteriaDto);
         Map<String,String> priceChangeMap = new HashMap<>();
         for(int i= 0 ;i<hubSupplierPriceChangeRecordDtos.size();i++){
