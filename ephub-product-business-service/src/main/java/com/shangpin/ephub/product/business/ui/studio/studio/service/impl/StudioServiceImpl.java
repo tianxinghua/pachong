c47405749968cb5c38631d87db6746dbbe0e6ebb
wanghui <wanghui@shangpin.com>
2017-06-09 11:51:35 +0800

    merge

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/studio/studio/service/impl/StudioServiceImpl.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/studio/studio/service/impl/StudioServiceImpl.java
@@ -1,9 +1,5 @@
 package com.shangpin.ephub.product.business.ui.studio.studio.service.impl;
 
-import com.shangpin.ephub.client.data.mysql.studio.gateway.HubStudioGateWay;
-import com.shangpin.ephub.client.data.mysql.studio.gateway.StudioGateWay;
-import com.shangpin.ephub.product.business.ui.studio.service.IStudioService;
-import com.shangpin.ephub.product.business.ui.studio.vo.StudioQueryDto;
 import com.shangpin.ephub.product.business.ui.studio.studio.service.IStudioService;
 import com.shangpin.ephub.product.business.ui.studio.studio.vo.StudioQueryDto;
 import com.shangpin.ephub.response.HubResponse;
@@ -12,9 +8,6 @@ import com.shangpin.ephub.response.HubResponse;
  * Created by Administrator on 2017/6/8.
  */
 public class StudioServiceImpl implements IStudioService {
-
-    private StudioGateWay studioGateWay;
-    private HubStudioGateWay hubStudioGateWay;
 
     public HubResponse<?> getPendingProductList(StudioQueryDto queryDto){
         return null;
