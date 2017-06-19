c6affa8679b6f9cd633c34d2b38bb3254fad3813
lubaijiang <1649675756@qq.com>
2017-06-19 15:51:16 +0800

    studio

--- ephub-common-rpc-connector/src/main/java/com/shangpin/ephub/client/data/studio/slot/spu/dto/StudioSlotSpuSendDetailWithCriteriaDto.java
+++ ephub-common-rpc-connector/src/main/java/com/shangpin/ephub/client/data/studio/slot/spu/dto/StudioSlotSpuSendDetailWithCriteriaDto.java
@@ -1,10 +1,14 @@
 package com.shangpin.ephub.client.data.studio.slot.spu.dto;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import lombok.*;
-
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
@@ -19,7 +23,7 @@ public class StudioSlotSpuSendDetailWithCriteriaDto implements Serializable {
 	 */
 	private static final long serialVersionUID = 5103951128810875746L;
 
-	private StudioSlotSpuSendDetailDto studioSlotSpuSendDetailDto;
+	private StudioSlotSpuSendDetailDto studioSlotSpuSendDetail;
 	
 	private StudioSlotSpuSendDetailCriteriaDto criteria;
 }
