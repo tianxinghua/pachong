0575a014ada55363a453dd8533846d36cfab5fdd
lubaijiang <1649675756@qq.com>
2017-06-26 19:09:54 +0800

    图片上传

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/studio/openbox/vo/CheckDetailVo.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/studio/openbox/vo/CheckDetailVo.java
@@ -2,7 +2,7 @@ package com.shangpin.ephub.product.business.ui.studio.openbox.vo;
 
 import java.util.List;
 
-import com.shangpin.ephub.client.data.studio.slot.spu.dto.StudioSlotSpuSendDetailDto;
+import com.shangpin.ephub.product.business.ui.studio.common.operation.vo.detail.StudioSlotSpuSendDetailVo;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,9 +14,9 @@ public class CheckDetailVo {
 	/**
 	 * 盘盈商品
 	 */
-	private List<StudioSlotSpuSendDetailDto> inventoryProfit;
+	private List<StudioSlotSpuSendDetailVo> inventoryProfit;
 	/**
 	 * 盘亏商品
 	 */
-	private List<StudioSlotSpuSendDetailDto> inventoryLosses;
+	private List<StudioSlotSpuSendDetailVo> inventoryLosses;
 }
