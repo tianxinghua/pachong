e1a3d27afec1ea65d0f640a9569862d7819d0f97
lubaijiang <1649675756@qq.com>
2017-06-29 17:23:59 +0800

    价格逻辑

--- ephub-product-business-service/src/test/java/com/shangpin/ephub/product/business/EphubProductBusinessServiceApplicationTests.java
+++ ephub-product-business-service/src/test/java/com/shangpin/ephub/product/business/EphubProductBusinessServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.shangpin.ephub.product.business;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -297,5 +298,12 @@ public class EphubProductBusinessServiceApplicationTests {
 	// e.printStackTrace();
 	// }
 	// }
+	
+	@Test
+	public void testBigDeci(){
+		BigDecimal b1 = new BigDecimal("12.00");
+		BigDecimal b2 = new BigDecimal("10.00");
+		System.out.println(b1.compareTo(b2));
+	}
 
 }
