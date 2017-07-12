72090e6e7c6e3deaeba19758e0b4bfc87e90178f
wanghui <wanghui@shangpin.com>
2017-07-12 17:10:38 +0800

    修改bug

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/studio/studio/service/impl/StudioServiceImpl.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/studio/studio/service/impl/StudioServiceImpl.java
@@ -913,12 +913,12 @@ public class StudioServiceImpl implements IStudioService {
         try {
             StudioSlotCriteriaDto dto = new StudioSlotCriteriaDto();
             StudioSlotCriteriaDto.Criteria criteria = dto.createCriteria();
-            //TODO：：注意 测试时注销的，需要放开
-//            if(!StringUtils.isEmpty(history) && history ==1){
-//                criteria.andApplyStatusGreaterThan(StudioSlotApplyState.WAIT_APPLY.getIndex().byteValue());
-//            }else{
-//                criteria.andApplyStatusEqualTo(StudioSlotApplyState.WAIT_APPLY.getIndex().byteValue());
-//            }
+            ///T-ODO：：注意 测试时注销的，需要放开
+            if(!StringUtils.isEmpty(history) && history ==1){
+                criteria.andApplyStatusGreaterThan(StudioSlotApplyState.WAIT_APPLY.getIndex().byteValue());
+            }else{
+                criteria.andApplyStatusEqualTo(StudioSlotApplyState.WAIT_APPLY.getIndex().byteValue());
+            }
 
             if (StudioId != null) {
                 criteria.andStudioIdEqualTo(StudioId);
