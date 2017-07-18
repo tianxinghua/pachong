a44dfc3e56c8571e79fc50f5b45ee31a6a488a3e
zhaogenchun <zhaogenchun@shangpin.com>
2017-07-18 16:48:59 +0800

    pro

--- ephub-data-mysql-service/src/main/java/com/shangpin/ephub/data/mysql/slot/dicbrand/controller/HubDicStudioBrandController.java
+++ ephub-data-mysql-service/src/main/java/com/shangpin/ephub/data/mysql/slot/dicbrand/controller/HubDicStudioBrandController.java
@@ -1,6 +1,10 @@
 package com.shangpin.ephub.data.mysql.slot.dicbrand.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,54 +20,54 @@ public class HubDicStudioBrandController {
 	@Autowired
 	private HubDicStudioBrandService hubDicStudioBrandService;
 
-//	@RequestMapping(value = "/count-by-criteria")
-//    public int countByCriteria(@RequestBody HubDicStudioBrandCriteria criteria){
-//    	return hubDicStudioBrandService.countByCriteria(criteria);
-//    }
-//	@RequestMapping(value = "/delete-by-criteria")
-//    public int deleteByCriteria(@RequestBody HubDicStudioBrandCriteria criteria){
-//    	return hubDicStudioBrandService.deleteByCriteria(criteria);
-//    }
-//	@RequestMapping(value = "/delete-by-primary-key")
-//    public int deleteByPrimaryKey(Long skuId){
-//    	return hubDicStudioBrandService.deleteByPrimaryKey(skuId);
-//    }
-//	@RequestMapping(value = "/insert")
-//    public Long  insert(@RequestBody HubDicStudioBrand hubDicStudioBrand){
-//    	 hubDicStudioBrandService.insert(hubDicStudioBrand);
-//    	 return  hubDicStudioBrand.getStudioBrandId();
-//    }
-//	@RequestMapping(value = "/insert-selective")
-//    public Long  insertSelective(@RequestBody HubDicStudioBrand hubDicStudioBrand){
-//    	 hubDicStudioBrandService.insertSelective(hubDicStudioBrand);
-//		return  hubDicStudioBrand.getStudioBrandId();
-//    }
-//	@RequestMapping(value = "/select-by-criteria-with-rowbounds")
-//    public List<HubDicStudioBrand> selectByCriteriaWithRowbounds(@RequestBody HubDicStudioBrandCriteriaWithRowBounds criteriaWithRowBounds){
-//    	return hubDicStudioBrandService.selectByCriteriaWithRowbounds(criteriaWithRowBounds);
-//    }
-//	@RequestMapping(value = "/select-by-criteria")
-//    public List<HubDicStudioBrand> selectByCriteria(@RequestBody HubDicStudioBrandCriteria criteria){
-//    	return hubDicStudioBrandService.selectByCriteria(criteria);
-//    }
-//	@RequestMapping(value = "/select-by-primary-key/{skuId}")
-//    public HubDicStudioBrand selectByPrimaryKey(@PathVariable(value = "skuId") Long skuId){
-//    	return hubDicStudioBrandService.selectByPrimaryKey(skuId);
-//    }
-//	@RequestMapping(value = "/update-by-criteria-selective")
-//    public int updateByCriteriaSelective(@RequestBody HubDicStudioBrandWithCriteria hubDicStudioBrandWithCriteria){
-//    	return hubDicStudioBrandService.updateByCriteriaSelective(hubDicStudioBrandWithCriteria);
-//    }
-//	@RequestMapping(value = "/update-by-criteria")
-//    public int updateByCriteria(@RequestBody HubDicStudioBrandWithCriteria hubDicStudioBrandWithCriteria){
-//    	return hubDicStudioBrandService.updateByCriteria(hubDicStudioBrandWithCriteria);
-//    }
-//	@RequestMapping(value = "/update-by-primary-key-selective")
-//    public int updateByPrimaryKeySelective(@RequestBody HubDicStudioBrand hubDicStudioBrand){
-//    	return hubDicStudioBrandService.updateByPrimaryKeySelective(hubDicStudioBrand);
-//    }
-//	@RequestMapping(value = "/update-by-primary-key")
-//    public int updateByPrimaryKey(@RequestBody HubDicStudioBrand hubDicStudioBrand){
-//    	return hubDicStudioBrandService.updateByPrimaryKey(hubDicStudioBrand);
-//    }
+	@RequestMapping(value = "/count-by-criteria")
+    public int countByCriteria(@RequestBody HubDicStudioBrandCriteria criteria){
+    	return hubDicStudioBrandService.countByCriteria(criteria);
+    }
+	@RequestMapping(value = "/delete-by-criteria")
+    public int deleteByCriteria(@RequestBody HubDicStudioBrandCriteria criteria){
+    	return hubDicStudioBrandService.deleteByCriteria(criteria);
+    }
+	@RequestMapping(value = "/delete-by-primary-key")
+    public int deleteByPrimaryKey(Long skuId){
+    	return hubDicStudioBrandService.deleteByPrimaryKey(skuId);
+    }
+	@RequestMapping(value = "/insert")
+    public Long  insert(@RequestBody HubDicStudioBrand hubDicStudioBrand){
+    	 hubDicStudioBrandService.insert(hubDicStudioBrand);
+    	 return  hubDicStudioBrand.getStudioBrandId();
+    }
+	@RequestMapping(value = "/insert-selective")
+    public Long  insertSelective(@RequestBody HubDicStudioBrand hubDicStudioBrand){
+    	 hubDicStudioBrandService.insertSelective(hubDicStudioBrand);
+		return  hubDicStudioBrand.getStudioBrandId();
+    }
+	@RequestMapping(value = "/select-by-criteria-with-rowbounds")
+    public List<HubDicStudioBrand> selectByCriteriaWithRowbounds(@RequestBody HubDicStudioBrandCriteriaWithRowBounds criteriaWithRowBounds){
+    	return hubDicStudioBrandService.selectByCriteriaWithRowbounds(criteriaWithRowBounds);
+    }
+	@RequestMapping(value = "/select-by-criteria")
+    public List<HubDicStudioBrand> selectByCriteria(@RequestBody HubDicStudioBrandCriteria criteria){
+    	return hubDicStudioBrandService.selectByCriteria(criteria);
+    }
+	@RequestMapping(value = "/select-by-primary-key/{skuId}")
+    public HubDicStudioBrand selectByPrimaryKey(@PathVariable(value = "skuId") Long skuId){
+    	return hubDicStudioBrandService.selectByPrimaryKey(skuId);
+    }
+	@RequestMapping(value = "/update-by-criteria-selective")
+    public int updateByCriteriaSelective(@RequestBody HubDicStudioBrandWithCriteria hubDicStudioBrandWithCriteria){
+    	return hubDicStudioBrandService.updateByCriteriaSelective(hubDicStudioBrandWithCriteria);
+    }
+	@RequestMapping(value = "/update-by-criteria")
+    public int updateByCriteria(@RequestBody HubDicStudioBrandWithCriteria hubDicStudioBrandWithCriteria){
+    	return hubDicStudioBrandService.updateByCriteria(hubDicStudioBrandWithCriteria);
+    }
+	@RequestMapping(value = "/update-by-primary-key-selective")
+    public int updateByPrimaryKeySelective(@RequestBody HubDicStudioBrand hubDicStudioBrand){
+    	return hubDicStudioBrandService.updateByPrimaryKeySelective(hubDicStudioBrand);
+    }
+	@RequestMapping(value = "/update-by-primary-key")
+    public int updateByPrimaryKey(@RequestBody HubDicStudioBrand hubDicStudioBrand){
+    	return hubDicStudioBrandService.updateByPrimaryKey(hubDicStudioBrand);
+    }
 }
