0ed91af992ad9172c69fe95ea16fffd4e172fb88
lubaijiang <1649675756@qq.com>
2017-08-04 18:32:12 +0800

    导入

---
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/rest/hubpending/spu/controller/PendingNohandleReasonController.java
@@ -0,0 +1,13 @@
+package com.shangpin.ephub.product.business.rest.hubpending.spu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PendingNohandleReasonController {
+
+	public void insertNohandleReason(){
+		
+	}
+}
