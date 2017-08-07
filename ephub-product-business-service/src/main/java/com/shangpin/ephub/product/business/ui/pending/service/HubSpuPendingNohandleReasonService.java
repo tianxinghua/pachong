e685ed6fc6b40cf47db0e6d8e18e75efc031ff0e
lubaijiang <1649675756@qq.com>
2017-08-07 16:12:37 +0800

    nohandle reason

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/pending/service/HubSpuPendingNohandleReasonService.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/pending/service/HubSpuPendingNohandleReasonService.java
@@ -6,11 +6,13 @@ import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shangpin.ephub.client.data.mysql.enumeration.ErrorReason;
 import com.shangpin.ephub.client.data.mysql.spu.dto.HubSpuPendingCriteriaDto;
 import com.shangpin.ephub.client.data.mysql.spu.dto.HubSpuPendingDto;
 import com.shangpin.ephub.client.data.mysql.spu.dto.HubSpuPendingNohandleReasonDto;
 import com.shangpin.ephub.client.data.mysql.spu.gateway.HubSpuPendingGateWay;
 import com.shangpin.ephub.client.data.mysql.spu.gateway.HubSpuPendingNohandleReasonGateWay;
+import com.shangpin.ephub.client.product.business.hubpending.spu.dto.NohandleReason;
 import com.shangpin.ephub.client.util.JsonUtil;
 import com.shangpin.ephub.product.business.ui.pending.dto.Reason;
 import com.shangpin.ephub.product.business.ui.pending.dto.Reasons;
@@ -32,9 +34,27 @@ public class HubSpuPendingNohandleReasonService {
 	private HubSpuPendingGateWay hubSpuPendingGateWay;
 	@Autowired
 	private HubSpuPendingNohandleReasonGateWay reasonGateWay;
+	
+	/**
+	 * 添加无法处理原因，任务调用
+	 * @param nohandleReason
+	 * @return
+	 */
+	public boolean insertNohandleReason(NohandleReason nohandleReason){
+		try {
+			List<HubSpuPendingDto> list = findPendingSpus(nohandleReason.getSupplierId(),nohandleReason.getSupplierSpuNo());
+			if(CollectionUtils.isNotEmpty(list)){
+				nohandleReason.getReasons().forEach(reason -> insert(reason , list.get(0)));
+				return true;
+			}
+		} catch (Exception e) {
+			log.error("插入无法处理原因异常："+e.getMessage(),e);
+		}
+		return false;
+	}
 
 	/**
-	 * 添加无法处理原因
+	 * 添加无法处理原因，页面调用
 	 * @param reasons
 	 */
 	public void addUnableReason(Reasons reasons){
@@ -51,6 +71,8 @@ public class HubSpuPendingNohandleReasonService {
 		}
 	}
 	
+	//==========================================================================================================================
+	
 	private List<HubSpuPendingDto>  findPendingSpus (List<Long> values){
 		HubSpuPendingCriteriaDto criteria = new HubSpuPendingCriteriaDto();
 		criteria.setFields("spu_pending_id,supplier_id,supplier_no,supplier_spu_id");
@@ -60,14 +82,49 @@ public class HubSpuPendingNohandleReasonService {
 		return hubSpuPendingGateWay.selectByCriteria(criteria);
 	}
 	
+	private List<HubSpuPendingDto>  findPendingSpus (String supplierId, String supplierSpuNo){
+		HubSpuPendingCriteriaDto criteria = new HubSpuPendingCriteriaDto();
+		criteria.setFields("spu_pending_id,supplier_id,supplier_no,supplier_spu_id");
+		criteria.createCriteria().andSupplierIdEqualTo(supplierId).andSupplierSpuNoEqualTo(supplierSpuNo);
+		return hubSpuPendingGateWay.selectByCriteria(criteria);
+	}
+	
 	private void insert(Reason reason, HubSpuPendingDto dto){
+		HubSpuPendingNohandleReasonDto reasonDto = convertFromSpuPendingDto(dto);
+		reasonDto.setErrorType(Byte.valueOf(reason.getErrorType()));
+		reasonDto.setErrorReason(Byte.valueOf(reason.getErrorReason())); 
+		reasonGateWay.insert(reasonDto);
+	}
+	
+	private void insert(String reason, HubSpuPendingDto dto){
+		HubSpuPendingNohandleReasonDto reasonDto = convertFromSpuPendingDto(dto);
+		ErrorReason errorReason = getErrorReason(reason);
+		if(null != errorReason){
+			reasonDto.setErrorType(errorReason.getErrorType().getIndex());
+			reasonDto.setErrorReason(errorReason.getIndex());
+			reasonGateWay.insert(reasonDto);
+		}else{
+			log.error("没有找到 "+reason+" 对应的错误类型");
+		}
+		
+	}
+
+	private HubSpuPendingNohandleReasonDto convertFromSpuPendingDto(HubSpuPendingDto dto) {
 		HubSpuPendingNohandleReasonDto reasonDto = new HubSpuPendingNohandleReasonDto();
 		reasonDto.setSpuPendingId(dto.getSpuPendingId());
 		reasonDto.setSupplierSpuId(dto.getSupplierSpuId());
 		reasonDto.setSupplierId(dto.getSupplierId());
 		reasonDto.setSupplierNo(dto.getSupplierNo());
-		reasonDto.setErrorType(Byte.valueOf(reason.getErrorType()));
-		reasonDto.setErrorReason(Byte.valueOf(reason.getErrorReason())); 
-		reasonGateWay.insert(reasonDto);
+		return reasonDto;
 	}
+	
+	private ErrorReason getErrorReason(String desCh){
+		for(ErrorReason errorReason : ErrorReason.values()){
+			if(errorReason.getDesCh().equals(desCh)){
+				return errorReason;
+			}
+		}
+		return null;
+	}
+	
 }
