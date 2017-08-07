5bae6be9aae9f8506a180c6f24e218079e3aa4d5
zhaogenchun <zhaogenchun@shangpin.com>
2017-08-07 16:01:36 +0800

    bug

--- supplier-product-consumer-service/src/main/java/com/shangpin/supplier/product/consumer/supplier/coccolebimbi/CoccolebimbiCommonHandler.java
+++ supplier-product-consumer-service/src/main/java/com/shangpin/supplier/product/consumer/supplier/coccolebimbi/CoccolebimbiCommonHandler.java
@@ -1,14 +1,11 @@
 package com.shangpin.supplier.product.consumer.supplier.coccolebimbi;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-
 import com.shangpin.ephub.client.data.mysql.sku.dto.HubSupplierSkuDto;
 import com.shangpin.ephub.client.data.mysql.spu.dto.HubSupplierSpuDto;
 import com.shangpin.ephub.client.message.original.body.SupplierProduct;
@@ -19,11 +16,9 @@ import com.shangpin.supplier.product.consumer.exception.EpHubSupplierProductCons
 import com.shangpin.supplier.product.consumer.service.SupplierProductMongoService;
 import com.shangpin.supplier.product.consumer.service.SupplierProductSaveAndSendToPending;
 import com.shangpin.supplier.product.consumer.supplier.ISupplierHandler;
-import com.shangpin.supplier.product.consumer.supplier.coccolebimbi.dto.Sku;
-import com.shangpin.supplier.product.consumer.supplier.coccolebimbi.dto.Spu;
+import com.shangpin.supplier.product.consumer.supplier.coccolebimbi.dto.Item;
 import com.shangpin.supplier.product.consumer.supplier.common.picture.PictureHandler;
 import com.shangpin.supplier.product.consumer.supplier.common.util.StringUtil;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Component("coccolebimbiCommonHandler")
@@ -41,28 +36,25 @@ public class CoccolebimbiCommonHandler implements ISupplierHandler {
 	public void handleOriginalProduct(SupplierProduct message, Map<String, Object> headers) {
 		try {
 			if(!StringUtils.isEmpty(message.getData())){
-				Spu item = JsonUtil.deserialize(message.getData(), Spu.class);
+				Item item = JsonUtil.deserialize(message.getData(), Item.class);
 				String supplierId = message.getSupplierId();
 				
-				mongoService.save(supplierId, item.getSpuId(), item);
+				mongoService.save(supplierId, item.getCODICE(), item);
 				
 				HubSupplierSpuDto hubSpu = new HubSupplierSpuDto();
 				boolean success = convertSpu(supplierId,item,hubSpu);
 				List<HubSupplierSkuDto> hubSkus = new ArrayList<HubSupplierSkuDto>();
 				HubSupplierSkuDto hubSku = new HubSupplierSkuDto();
 				
-				List<Sku> skus = item.getSkus();
-				for(Sku sku:skus){
-					boolean skuSuc = convertSku(supplierId,hubSpu.getSupplierSpuId(),sku,hubSku);
+					boolean skuSuc = convertSku(supplierId,hubSpu.getSupplierSpuId(),item,hubSku);
 					if(skuSuc){
 						hubSkus.add(hubSku);
 					}
-				}
 				
 				//处理图片				
 				SupplierPicture supplierPicture = null;
 //				if(pictureHandler.isCurrentSeason(message.getSupplierId(), hubSpu.getSupplierSeasonname())){
-					supplierPicture = pictureHandler.initSupplierPicture(message, hubSpu, converImage(item.getImages()));
+					supplierPicture = pictureHandler.initSupplierPicture(message, hubSpu, converImage(item));
 //				}
 				if(success){
 					supplierProductSaveAndSendToPending.saveAndSendToPending(message.getSupplierNo(),supplierId, message.getSupplierName(), hubSpu, hubSkus,supplierPicture);
@@ -79,12 +71,47 @@ public class CoccolebimbiCommonHandler implements ISupplierHandler {
 	 * @param itemImages
 	 * @return
 	 */
-	private List<Image> converImage(List<String> itemImages){
+	private List<Image> converImage(Item itemImages){
 		List<Image> images = new ArrayList<Image>();
-		if(null != itemImages){			
-			for(String url : itemImages){
+		if(null != itemImages){		
+			if(!StringUtils.isEmpty(itemImages.getFOTO1())){
 				Image image = new Image();
-				image.setUrl(url);
+				image.setUrl(itemImages.getUri()+"/foto?"+itemImages.getFOTO1());
+				images.add(image);
+			}
+			if(!StringUtils.isEmpty(itemImages.getFOTO2())){
+				Image image = new Image();
+				image.setUrl(itemImages.getUri()+"/foto?"+itemImages.getFOTO2());
+				images.add(image);
+			}
+			if(!StringUtils.isEmpty(itemImages.getFOTO3())){
+				Image image = new Image();
+				image.setUrl(itemImages.getUri()+"/foto?"+itemImages.getFOTO3());
+				images.add(image);
+			}
+			if(!StringUtils.isEmpty(itemImages.getFOTO4())){
+				Image image = new Image();
+				image.setUrl(itemImages.getUri()+"/foto?"+itemImages.getFOTO4());
+				images.add(image);
+			}
+			if(!StringUtils.isEmpty(itemImages.getFOTO5())){
+				Image image = new Image();
+				image.setUrl(itemImages.getUri()+"/foto?"+itemImages.getFOTO5());
+				images.add(image);
+			}
+			if(!StringUtils.isEmpty(itemImages.getFOTO6())){
+				Image image = new Image();
+				image.setUrl(itemImages.getUri()+"/foto?"+itemImages.getFOTO6());
+				images.add(image);
+			}
+			if(!StringUtils.isEmpty(itemImages.getFOTO7())){
+				Image image = new Image();
+				image.setUrl(itemImages.getUri()+"/foto?"+itemImages.getFOTO7());
+				images.add(image);
+			}
+			if(!StringUtils.isEmpty(itemImages.getFOTO8())){
+				Image image = new Image();
+				image.setUrl(itemImages.getUri()+"/foto?"+itemImages.getFOTO8());
 				images.add(image);
 			}
 		}
@@ -97,20 +124,48 @@ public class CoccolebimbiCommonHandler implements ISupplierHandler {
 	 * @param item 供应商原始dto
 	 * @param hubSpu hub spu表
 	 */
-	public boolean convertSpu(String supplierId,Spu item, HubSupplierSpuDto hubSpu) throws EpHubSupplierProductConsumerRuntimeException{
+	public boolean convertSpu(String supplierId,Item item, HubSupplierSpuDto hubSpu) throws EpHubSupplierProductConsumerRuntimeException{
 		if(null != item){			
+			
+			String desc = item.getDESCRIZIONE_LUNGA_ENG();
+			if(desc!=null&&desc.split("#").length>1){
+				String productName = desc.split("#")[0];
+				hubSpu.setSupplierSpuName(productName);
+			}
 			hubSpu.setSupplierId(supplierId);
-			hubSpu.setSupplierSpuNo(item.getSpuId());
-//			hubSpu.setSupplierSpuModel(item.getProduct_reference()+" "+item.getColor_reference());
-			hubSpu.setSupplierSpuName(item.getSpuName());
-			hubSpu.setSupplierSpuColor(item.getColor());
-			hubSpu.setSupplierGender(item.getGender());
-			hubSpu.setSupplierCategoryname(item.getCategoryName());
-			hubSpu.setSupplierBrandname(item.getBrandName());
-			hubSpu.setSupplierSeasonname(item.getSeasonName());
-			hubSpu.setSupplierMaterial(item.getMaterial());
-			hubSpu.setSupplierOrigin(item.getProductOrigin());
-			hubSpu.setSupplierSpuDesc(item.getProductDescription());
+			hubSpu.setSupplierSpuNo(item.getCODICE());
+			hubSpu.setSupplierSpuModel(item.getARTICOLO());
+			
+			
+			if(item.getCOLORE_ENG()!=null){
+				hubSpu.setSupplierSpuColor(item.getCOLORE_ENG());	
+			}else{
+				hubSpu.setSupplierSpuColor(item.getCOLORE_ITA());
+			}
+			
+			hubSpu.setSupplierGender(item.getGRTIPO_ENG());
+			
+			hubSpu.setSupplierCategoryname(item.getMODELLO_ENG());
+			hubSpu.setSupplierBrandname(item.getBRAND());
+			
+			if(item.getSTAGIONE_ENG()!=null){
+				hubSpu.setSupplierSeasonname(item.getSTAGIONE_ENG());	
+			}else{
+				hubSpu.setSupplierSeasonname(item.getSTAGIONE_ITA());
+			}
+			
+			
+			if(item.getMATERIALE_ENG()!=null){
+				hubSpu.setSupplierMaterial(item.getMATERIALE_ENG());	
+			}else{
+				hubSpu.setSupplierMaterial(item.getMATERIALE_ITA());
+			}
+			if(item.getMADEIN_ENG()!=null){
+				hubSpu.setSupplierOrigin(item.getMADEIN_ENG());
+			}else{
+				hubSpu.setSupplierOrigin(item.getMADEIN());
+			}
+			hubSpu.setSupplierSpuDesc(desc);
 			return true;
 		}else{
 			return false;
@@ -125,18 +180,17 @@ public class CoccolebimbiCommonHandler implements ISupplierHandler {
 	 * @param hubSku
 	 * @return
 	 */
-	public boolean convertSku(String supplierId, Long supplierSpuId,Sku item, HubSupplierSkuDto hubSku) throws EpHubSupplierProductConsumerRuntimeException{
+	public boolean convertSku(String supplierId, Long supplierSpuId,Item item, HubSupplierSkuDto hubSku) throws EpHubSupplierProductConsumerRuntimeException{
 		if(null != item){			
 			hubSku.setSupplierSpuId(supplierSpuId);
 			hubSku.setSupplierId(supplierId);
-			hubSku.setSupplierSkuNo(item.getSkuId());
-			hubSku.setSupplierBarcode(item.getBarCode());
-			hubSku.setMarketPrice(new BigDecimal(StringUtil.verifyPrice(item.getMarketPrice())));
-			hubSku.setSupplyPrice(new BigDecimal(StringUtil.verifyPrice(item.getSupplierPrice())));
+			hubSku.setSupplierSkuNo(item.getKEY());
+//			hubSku.setSupplierBarcode(item.getBarCode());
+			hubSku.setMarketPrice(new BigDecimal(StringUtil.verifyPrice(item.getPREZZO_NETTO())));
 //			hubSku.setMarketPriceCurrencyorg(item.getCurrency());
 //			hubSku.setSupplyPriceCurrency(item.getCurrency());
-			hubSku.setSupplierSkuSize(item.getSize());
-			hubSku.setStock(StringUtil.verifyStock((item.getQty())));
+			hubSku.setSupplierSkuSize(item.getTAGLIA());
+			hubSku.setStock(StringUtil.verifyStock((item.getDISPO())));
 			return true;
 		}else{
 			return false;
