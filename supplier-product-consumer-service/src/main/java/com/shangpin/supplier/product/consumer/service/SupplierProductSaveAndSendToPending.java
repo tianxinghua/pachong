e8bb5c361223a564597d65c7bf83ff3b74218266
zhaogenchun <zhaogenchun@shangpin.com>
2017-10-09 15:40:21 +0800

    bug

--- supplier-product-consumer-service/src/main/java/com/shangpin/supplier/product/consumer/service/SupplierProductSaveAndSendToPending.java
+++ supplier-product-consumer-service/src/main/java/com/shangpin/supplier/product/consumer/service/SupplierProductSaveAndSendToPending.java
@@ -99,7 +99,9 @@ public class SupplierProductSaveAndSendToPending {
 			supplierProductRetryManager.insert(dto);
 		}
 		if(org.apache.commons.lang.StringUtils.isNotBlank(hubSpu.getSupplierSeasonname())){
-			HubSeasonDicDto hubSeason = supplierProductRetryManager.findSupplierSeason(supplierId, hubSpu.getSupplierSeasonname());
+			log.info("supplierId:"+supplierId+",季节:"+hubSpu.getSupplierSeasonname());
+			HubSeasonDicDto hubSeason = supplierProductRetryManager.findSupplierSeason(supplierId.trim(), hubSpu.getSupplierSeasonname().trim());
+			log.info("返回参数：{}",hubSeason);
 			if(hubSeason==null){
 				hubSeason = new HubSeasonDicDto(); 
 				hubSeason.setCreateTime(new Date());
