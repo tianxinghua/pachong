016027c0617b3d110075d8750decfe59655dc61e
zhaogenchun <zhaogenchun@shangpin.com>
2017-10-18 15:51:08 +0800

    productCode

--- ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/purchase/service/impl/ProductServiceImpl.java
+++ ephub-product-business-service/src/main/java/com/shangpin/ephub/product/business/ui/purchase/service/impl/ProductServiceImpl.java
@@ -39,6 +39,7 @@ public class ProductServiceImpl implements IProductService {
                     Product item = new Product();
                     item.setSkuId(result.getSkuId());
                     item.setSpuId(result.getSpuId());
+                    item.setProductCode(result.getProductCode);
                     item.setProductName(result.getProductName());
                     item.setColor(result.getColor());
                     item.setSize(result.getSize());
