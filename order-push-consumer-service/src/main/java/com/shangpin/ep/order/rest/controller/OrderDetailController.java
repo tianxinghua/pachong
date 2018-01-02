6cfc3949f8a1a901b8fcf815f9b2cc2ddda228d6
Moon <Moon@LAPTOP-TQ3O1GN1>
2018-01-02 17:58:38 +0800

    order

--- order-push-consumer-service/src/main/java/com/shangpin/ep/order/rest/controller/OrderDetailController.java
+++ order-push-consumer-service/src/main/java/com/shangpin/ep/order/rest/controller/OrderDetailController.java
@@ -5,16 +5,16 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shangpin.ep.order.module.order.bean.HubOrderDetail;
-import com.shangpin.ep.order.module.order.bean.SkuSearchResult;
 import com.shangpin.ep.order.module.order.service.IHubOrderService;
 import com.shangpin.ep.order.module.order.service.impl.OrderCommonUtil;
 import com.shangpin.ep.order.module.orderapiservice.impl.OrderHandleSearch;
+import com.shangpin.ep.order.rest.controller.dto.OrderDetailRequestDto;
 
 /**
  * Created by lizhongren on 2016/11/18.
@@ -34,10 +34,9 @@ public class OrderDetailController {
     OrderCommonUtil orderCommonUtil;
 
     @RequestMapping(value="/getOrderNoByMasterNo", method = RequestMethod.POST)
-    public String getOrderCountBySpSkuNo(@RequestParam(value = "supplierId")  String  supplierId,@RequestParam(value = "masterOrderNo")  String  masterOrderNo
-    		,@RequestParam(value = "spSkuNo")  String  spSkuNo){
-    	
-    	List<HubOrderDetail> hubOrderDetails = hubOrderService.getOrderDetailBySupplierIdAndSpMasterOrderNoAndSpSkuNo(supplierId, masterOrderNo,spSkuNo);
+    public String getOrderCountBySpSkuNo(@RequestBody OrderDetailRequestDto orderDetail){
+    	log.info("请求参数：{}",orderDetail);
+    	List<HubOrderDetail> hubOrderDetails = hubOrderService.getOrderDetailBySupplierIdAndSpMasterOrderNoAndSpSkuNo(orderDetail.getSupplierId(), orderDetail.getMasterOrderNo(),orderDetail.getSpSkuNo());
     	if(hubOrderDetails!=null&&hubOrderDetails.size()>0){
     		return hubOrderDetails.get(0).getOrderNo();
     	}
