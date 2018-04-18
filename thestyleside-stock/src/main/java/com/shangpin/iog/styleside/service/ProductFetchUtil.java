c50973a68b52386ecb0038fc0bb95c193b05d787
wanner <wanner.guo@shangpinwang.com>
2018-04-18 14:59:50 +0800

    提交供应商 theStyleSide 获取库存utils 以及配置文件

--- thestyleside-stock/src/main/java/com/shangpin/iog/styleside/service/ProductFetchUtil.java
+++ thestyleside-stock/src/main/java/com/shangpin/iog/styleside/service/ProductFetchUtil.java
@@ -8,12 +8,17 @@ import com.shangpin.iog.common.utils.httpclient.OutTimeConfig;
 
 import com.shangpin.iog.styleside.dto.StockQuery;
 import com.shangpin.iog.styleside.dto.StockResult;
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 import org.apache.log4j.Logger;
+import org.datacontract.schemas._2004._07.maximag_connector_vidra_service.ArrayOfArticoloFlatExtLocaleVO;
+import org.datacontract.schemas._2004._07.maximag_connector_vidra_service.ArticoloFlatExtLocaleVO;
+import org.datacontract.schemas._2004._07.maximag_connector_vidra_service.MgDispo;
 import org.springframework.stereotype.Component;
+import org.tempuri.IVidraSvcOfArticoloFlatExtVOArticoloFlatVO;
+import org.tempuri.VidraSvc;
 
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.math.BigDecimal;
+import java.util.*;
 
 /**
  * Created by wangchao on 2017/10/26.
@@ -22,13 +27,24 @@ import java.util.ResourceBundle;
 public class ProductFetchUtil {
 	private static Logger logger = Logger.getLogger("info");
 	private static ResourceBundle bdl = null;
-	private static String stockUrl = "",supplierId="";
+	private static String supplierId = "",year="",month="",days="",hour="",minute="",second="",millisecond="",usr="",pwd="",filter="",recordCount="",language="";
 	static {
 		if (null == bdl){
 			bdl = ResourceBundle.getBundle("conf");
 		}
-
-		stockUrl = bdl.getString("stockUrl");
+		supplierId = bdl.getString("supplierId");
+		year = bdl.getString("year");
+		month = bdl.getString("month");
+		days = bdl.getString("days");
+		hour = bdl.getString("hour");
+		minute = bdl.getString("minute");
+		second = bdl.getString("second");
+		millisecond = bdl.getString("millisecond");
+		usr = bdl.getString("usr");
+		pwd = bdl.getString("pwd");
+		filter = bdl.getString("filter");
+		recordCount = bdl.getString("recordCount");
+		language = bdl.getString("language");
 		supplierId = bdl.getString("supplierId");
 	}
 	ObjectMapper mapper = new ObjectMapper();
@@ -36,40 +52,74 @@ public class ProductFetchUtil {
 
 
 
-	public  Map<String,String> getProductStock(Map<String,String> stockMap) {
-
+	/**
+	 * 程序配置：
+	 * 拉取的时间、用户名、用户密码、fiter、每页拉取数、语言为配置文件控制
+	 * 页码为程序中循环增加，
+	 * 程序的循环结束（while循环）标志：
+	 * 拉取的信息的 list size()为 0 的时候结束
+	 *
+	 */
+	public  Map<String,String> getProductStock(Collection<String> skuNos) {
+		//定义供应商 skuNo （key） Quantita(value) Map集合
+		Map<String, String> spStockMap = new HashMap<>(skuNos.size());
 		String result = null;
-		int page=0,pageSize = 100;
+		int page=0;
 		boolean loop= true;
-		String request ="";
-		List<StockResult> stockList = null;
-		while(loop){
-			page++;
-			StockQuery query = new StockQuery();
-			query.setPageIndex(page);
-			query.setPageSize(pageSize);
-			query.setSupplierId(supplierId);
-			try {
-				request = mapper.writeValueAsString(query);
-				result = HttpUtil45.operateData("post","json",stockUrl,timeConfig,null,request,"","");
-				stockList = 	mapper.readValue(result, new TypeReference<List<StockResult>>(){} );
-				if(null==stockList||stockList.size()<100){
+		try {
+			while(loop){
+				page++;
+				IVidraSvcOfArticoloFlatExtVOArticoloFlatVO http = new VidraSvc().getHTTP();
+				XMLGregorianCalendarImpl xmlGregorianCalendar = new XMLGregorianCalendarImpl();
+				xmlGregorianCalendar.setYear(Integer.parseInt(year));
+				xmlGregorianCalendar.setMonth(Integer.parseInt(month));
+				xmlGregorianCalendar.setDay(Integer.parseInt(days));
+				xmlGregorianCalendar.setHour(Integer.parseInt(hour));
+				xmlGregorianCalendar.setMinute(Integer.parseInt(minute));
+				xmlGregorianCalendar.setSecond(Integer.parseInt(second));
+				xmlGregorianCalendar.setMillisecond(Integer.parseInt(millisecond));
+				ArrayOfArticoloFlatExtLocaleVO articoliFlatExtLocaleByDate = http.getArticoliFlatExtLocaleByDate(usr, pwd, filter, xmlGregorianCalendar, Integer.parseInt(recordCount), page, language);
+
+				List<ArticoloFlatExtLocaleVO> articoloFlatExtLocaleVO = articoliFlatExtLocaleByDate.getArticoloFlatExtLocaleVO();
+
+				logger.info("thestyleside成功获取第"+page+"页库存数据，当前页有"+articoloFlatExtLocaleVO.size()+"条数据");
+				if(null==articoloFlatExtLocaleVO||articoloFlatExtLocaleVO.size()<Integer.parseInt(recordCount)){
 					loop = false;
 				}
-				if(null!=stockList&&stockList.size()>0){
-                     for(StockResult stockResult:stockList){
-                     	stockMap.put(stockResult.getSupplierSkuNo(),String.valueOf(stockResult.getStock()));
-					 }
+				if(null!=articoloFlatExtLocaleVO&&articoloFlatExtLocaleVO.size()>0){
+					for (ArticoloFlatExtLocaleVO vo :articoloFlatExtLocaleVO) {
+
+					/*System.out.println("brand="+vo.getBrand().getValue());
+					System.out.println("category(Brand ID)="+vo.getCategory().getValue());
+					System.out.println("categoryName(brandName)="+vo.getCategoryName().getValue());
+					System.out.println("colourName="+vo.getColour().getValue());
+					System.out.println("colourID="+vo.getColourID().getValue());
+					System.out.println("colourWeb="+vo.getColourWeb().getValue());
+					System.out.println("colourWebID="+vo.getColourWebID().getValue());
+					System.out.println("description="+vo.getDescription().getValue());*/
+						String barCode = vo.getBarCode().getValue();
+						String size = vo.getSize().getValue();
+						String skuNo = barCode + size;
+						/**
+						 * collect 中包含有该 skuNO 则放入到spStockMap中，没有的话不存放，
+						 * 供应商接口数据中可能没有提供全 skuNos 所包含的所有商品库存信息，该spStockMap中则不存放该key value，方法外处理置0
+						 */
+						if(skuNos.contains(skuNo)){
+							MgDispo mgDispo = vo.getQuantita().getValue();
+							BigDecimal quantitaDimm = mgDispo.getQuantitaDimm();
+							String stockNum = quantitaDimm+"";
+							spStockMap.put(skuNo,quantitaDimm+stockNum);
+						}
+					}
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
+			logger.info("  theStyleSide  循环获取库存数据结束==");
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info("  ==theStyleSide 获取第"+page+"页库存数据出现错误,错误如下："+e.getMessage());
 		}
-
-
-
-
-		return stockMap;
+		logger.info("成功获取到的map大小  spStockMap.size======"+spStockMap.size());
+		return spStockMap;
 	}
 
 
