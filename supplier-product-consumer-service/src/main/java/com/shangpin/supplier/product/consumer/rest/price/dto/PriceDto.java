1d1340c3cdc214d15e0a409eab65fd55407c371e
chaowang <chao.wang@shangpin.com>
2018-07-02 17:32:16 +0800

    zhicai 变价推送接口

---
+++ supplier-product-consumer-service/src/main/java/com/shangpin/supplier/product/consumer/rest/price/dto/PriceDto.java
@@ -0,0 +1,15 @@
+package com.shangpin.supplier.product.consumer.rest.price.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PriceDto {
+
+    private String supplierSkuNo;
+    private String marketPrice;
+    private String supplierId;
+    private String supplierNo;
+	
+}
