ee85c9205b26d363b4751d0be39bf36d4dbcd57a
wanner <wanner.guo@shangpinwang.com>
2018-08-10 10:07:18 +0800

    redigroup-memo brands HERMES 商品库存信息拉取及推送

---
+++ hermes-stock/src/main/java/com/shangpin/iog/hermes/schedule/Startup.java
@@ -0,0 +1,28 @@
+package com.shangpin.iog.hermes.schedule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+/**
+ * Created by wanner on 2018/8/8
+ * redigroup-memo brands 爬虫品牌：HERMES 库存价格推送程序 ，注意推送的是 昨天的库存数据
+ */
+public class Startup {
+
+    private static   Logger logger = LoggerFactory.getLogger(Startup.class);
+    private static ApplicationContext factory;
+    private static void loadSpringContext() {
+        factory = new AnnotationConfigApplicationContext(AppContext.class);
+    }
+
+	public static void main(String[] args)
+    {
+        //加载spring
+    	System.out.println("start");
+        loadSpringContext();
+        logger.info(" schedule start  ");
+    }
+}
