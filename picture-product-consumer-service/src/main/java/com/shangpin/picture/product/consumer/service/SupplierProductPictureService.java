73e65317bf19c439e4bb85024e81f9d63fd19e5c
Administrator <lizhongren@shangpin.com>
2018-10-09 20:37:37 +0800

    add pic tail log

--- picture-product-consumer-service/src/main/java/com/shangpin/picture/product/consumer/service/SupplierProductPictureService.java
+++ picture-product-consumer-service/src/main/java/com/shangpin/picture/product/consumer/service/SupplierProductPictureService.java
@@ -155,64 +155,64 @@ public class SupplierProductPictureService {
 			AuthenticationInformation authenticationInformation1 = new AuthenticationInformation();
 			return download.downloadPicture(picUrl,dto1,authenticationInformation1);
 		}else {
-		InputStream inputStream = null;
-		HttpURLConnection httpUrlConnection = null;
-		int flag = 0;
-		try {
-			if (authenticationInformation != null) {//需要认证
-				Authenticator.setDefault(new Authenticator() {
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(authenticationInformation.getUsername(),
-								new String(authenticationInformation.getPassword()).toCharArray());
-					}
-				});
+			InputStream inputStream = null;
+			HttpURLConnection httpUrlConnection = null;
+			int flag = 0;
+			try {
+				if (authenticationInformation != null) {//需要认证
+					Authenticator.setDefault(new Authenticator() {
+						protected PasswordAuthentication getPasswordAuthentication() {
+							return new PasswordAuthentication(authenticationInformation.getUsername(),
+									new String(authenticationInformation.getPassword()).toCharArray());
+						}
+					});
+				}
+
+				URL url = new URL(picUrl.replaceAll(" +", "%20"));
+				URLConnection openConnection = url.openConnection();
+				httpUrlConnection  =  (HttpURLConnection) openConnection;
+				httpUrlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0");
+				httpUrlConnection.setConnectTimeout(CONNECT_TIMEOUT);
+				httpUrlConnection.setReadTimeout(TIMEOUT);
+				httpUrlConnection.connect();
+				flag = httpUrlConnection.getResponseCode();
+				log.info("response code=" + flag);
+				if (flag == 404 || flag == 400) {
+					return flag;
+				}else if(flag == 301 || flag == 302){
+					log.info("id="+dto.getSpuPendingPicId()+"链接重定向，原始url="+picUrl);
+					String newPicUrl = picUrl.replaceFirst("http", "https");
+					return pullPicAndPushToPicServer(newPicUrl,dto,authenticationInformation);
+				}
+				inputStream = openConnection.getInputStream();
+				byte[] byteArray = IOUtils.toByteArray(inputStream);
+				if (byteArray == null || byteArray.length == 0) {
+					throw new RuntimeException("读取到的图片字节为空,无法获取图片");
+				}
+				String base64 = new BASE64Encoder().encode(byteArray);
+				log.info("id="+dto.getSpuPendingPicId()+"==第一步==>> "+"原始url="+picUrl+"， 上传图片前拉取的数据为"+base64.substring(0, 100)+"，长度 为 "+base64.length()+"， 下一步调用上传图片服务上传图片到图片服务器");
+				UploadPicDto uploadPicDto = new UploadPicDto();
+				uploadPicDto.setRequestId(String.valueOf(dto.getSpuPendingPicId()));
+				uploadPicDto.setBase64(base64);
+				uploadPicDto.setExtension(getExtension(picUrl));
+				String fdfsURL = supplierProductPictureManager.uploadPic(uploadPicDto);
+				log.info("id="+dto.getSpuPendingPicId()+"==第四步==>> 调用图片服务上传图片后返回的图片URL为"+fdfsURL+"， 下一步将更改数据库");
+				dto.setSpPicUrl(fdfsURL);
+				dto.setPicHandleState(PicHandleState.HANDLED.getIndex());
+				dto.setMemo("图片拉取成功");
+			}
+			catch (Throwable e) {
+					log.error("系统拉取图片时发生异常,url =" + picUrl, e);
+					e.printStackTrace();
+					dto.setPicHandleState(PicHandleState.HANDLE_ERROR.getIndex());
+					dto.setMemo("图片拉取失败:" + flag);
+
+			} finally {
+				close(inputStream, httpUrlConnection);
 			}
 
-			URL url = new URL(picUrl.replaceAll(" +", "%20"));
-			URLConnection openConnection = url.openConnection();
-			httpUrlConnection  =  (HttpURLConnection) openConnection;
-			httpUrlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0");
-			httpUrlConnection.setConnectTimeout(CONNECT_TIMEOUT);
-			httpUrlConnection.setReadTimeout(TIMEOUT);
-			httpUrlConnection.connect();
-			flag = httpUrlConnection.getResponseCode();
-			log.info("response code=" + flag);
-			if (flag == 404 || flag == 400) {
-				return flag;
-			}else if(flag == 301 || flag == 302){
-				log.info("id="+dto.getSpuPendingPicId()+"链接重定向，原始url="+picUrl);
-				String newPicUrl = picUrl.replaceFirst("http", "https");
-				return pullPicAndPushToPicServer(newPicUrl,dto,authenticationInformation);
-			}
-			inputStream = openConnection.getInputStream();
-			byte[] byteArray = IOUtils.toByteArray(inputStream);
-			if (byteArray == null || byteArray.length == 0) {
-				throw new RuntimeException("读取到的图片字节为空,无法获取图片");
-			}
-			String base64 = new BASE64Encoder().encode(byteArray);
-			log.info("id="+dto.getSpuPendingPicId()+"==第一步==>> "+"原始url="+picUrl+"， 上传图片前拉取的数据为"+base64.substring(0, 100)+"，长度 为 "+base64.length()+"， 下一步调用上传图片服务上传图片到图片服务器");
-			UploadPicDto uploadPicDto = new UploadPicDto();
-			uploadPicDto.setRequestId(String.valueOf(dto.getSpuPendingPicId()));
-			uploadPicDto.setBase64(base64);
-			uploadPicDto.setExtension(getExtension(picUrl));
-			String fdfsURL = supplierProductPictureManager.uploadPic(uploadPicDto);
-			log.info("id="+dto.getSpuPendingPicId()+"==第四步==>> 调用图片服务上传图片后返回的图片URL为"+fdfsURL+"， 下一步将更改数据库");
-			dto.setSpPicUrl(fdfsURL);
-			dto.setPicHandleState(PicHandleState.HANDLED.getIndex());
-			dto.setMemo("图片拉取成功");
-		}
-		catch (Throwable e) {
-				log.error("系统拉取图片时发生异常,url =" + picUrl, e);
-				e.printStackTrace();
-				dto.setPicHandleState(PicHandleState.HANDLE_ERROR.getIndex());
-				dto.setMemo("图片拉取失败:" + flag);
-
-		} finally {
-			close(inputStream, httpUrlConnection);
-		}
-
-		dto.setUpdateTime(new Date());
-		return flag;
+			dto.setUpdateTime(new Date());
+			return flag;
 		}
 	}
 
