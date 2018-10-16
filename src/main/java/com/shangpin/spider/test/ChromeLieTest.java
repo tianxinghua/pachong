f86ec63ba24d63692a6cf638d3e7cc8566832a8e
njt <njt>
2018-10-16 16:54:24 +0800

    列表组合翻页修复测试

--- src/main/java/com/shangpin/spider/test/ChromeLieTest.java
+++ src/main/java/com/shangpin/spider/test/ChromeLieTest.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -27,7 +29,7 @@ public class ChromeLieTest {
 		caps.setCapability("acceptSslCerts", true);
 		//css搜索支持
 		caps.setCapability("cssSelectorsEnabled", true);
-		
+		caps.setCapability("enableNativeEvents", true);
 		System.setProperty("webdriver.chrome.driver", "D:/software/driver/chromedriver.exe");
 		
 //		ChromeOptions options = new ChromeOptions();
@@ -38,13 +40,13 @@ public class ChromeLieTest {
 		
 		ChromeDriver driver = new ChromeDriver(caps);
 		String url = "";
-		url = "https://www.balenciaga.com/fr/femme/tout-pret-a-porter";
+		url = "https://www.balenciaga.com/fr/homme/chaussures";
 		driver.get(url);
 		driver.manage().window().maximize();
 		CrackDspiderUtil.crackMask(driver, null);
 		handleScroll(url,driver);
 		List<WebElement> elements = driver.findElements(By.cssSelector(".viewmore-btn"));
-		handleMore(url, ".viewmore-btn", driver, elements);
+		handleMore(url, ".viewmore-btn", driver, elements,elements.size(),0);
 		
 		System.out.println("完毕");
 		
@@ -86,17 +88,32 @@ public class ChromeLieTest {
 	 * 点击更多的处理
 	 * @param url
 	 * @param webDriver
+	 * @param i 
+	 * @param size 
 	 * @return 
 	 */
-	private static RemoteWebDriver handleMore(String url, String nextPageTagDe, RemoteWebDriver webDriver, List<WebElement> elements) {
+	private static RemoteWebDriver handleMore(String url, String nextPageTagDe, RemoteWebDriver webDriver, List<WebElement> elements, int size, int i) {
 		try {
-			if(elements!=null&&elements.size()>0) {
-				WebElement ele = elements.get(0);
-				WebDriverWait wait = new WebDriverWait(webDriver, 1);
-				wait.until(ExpectedConditions.elementToBeClickable(ele));
-				ele.click();
-				elements = webDriver.findElements(By.cssSelector(nextPageTagDe));
-				webDriver = handleMore(url,nextPageTagDe,webDriver,elements);
+			
+//			Actions actions = new Actions(webDriver);
+			if(i<size) {
+				try {
+					WebElement element = webDriver.findElement(By.cssSelector("#SearchboxReset > span.icn.icn-close"));
+//					actions.click(element).build().perform();
+//					element.click();
+					((JavascriptExecutor)webDriver).executeScript("arguments[0].click()", element);
+				} catch (Exception e) {
+					System.err.println("执行多余的X号！"+e.getMessage());
+				}
+				WebElement ele = elements.get(i);
+//				WebDriverWait wait = new WebDriverWait(webDriver, 1);
+//				wait.until(ExpectedConditions.elementToBeClickable(ele));
+//				ele.click();
+//				Action action = actions.clickAndHold(ele).release().build();
+//				action.perform();
+				((JavascriptExecutor)webDriver).executeScript("arguments[0].click()", ele);
+				i++;
+				webDriver = handleMore(url,nextPageTagDe,webDriver,elements,size,i);
 			}
 		} catch (Exception e) {
 			System.err.println("点击查看更多按钮有误！");
